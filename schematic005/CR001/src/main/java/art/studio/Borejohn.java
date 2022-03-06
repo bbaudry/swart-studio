9939406415415267967b25f78a73a526b823f1d3
bbaudry <baudry@kth.se>
2022-03-06 12:57:57 +0100

    colors and grid

--- schematic005/CR001/src/main/java/art/studio/Borejohn.java
+++ schematic005/CR001/src/main/java/art/studio/Borejohn.java
@@ -2,19 +2,27 @@ package art.studio;
 
 import processing.core.PApplet;
 import java.util.Random;
+import java.util.ArrayList;
 
 public class Borejohn  extends PApplet {
 
-    int w = 700;
-    int h = 500;
-    float x = 60;
+    int w = 1000;
+    int h = 1000;
+    float x = 30;
     float y = 60;
-    float radius = 51;
+    float r = random(256);
+    float g = random(256);
+    float b = random(256);
+    float cChange = 20;
+    float prevy;
+    boolean drawC = true; //if true draw circle, else a line
+    float radius = 57;
+    boolean first_row = true;
+    boolean end = false;
     float nextx;
     float nexty;
-    float[] posx = new float[100];
+    ArrayList<Float> posx = new ArrayList<Float>();
     int indexx = 0;
-    boolean ci = true;
     Random rd = new Random();
 
     @Override
@@ -24,46 +32,105 @@ public class Borejohn  extends PApplet {
 
     @Override
     public void setup() {
-        frameRate(1);
+        //frameRate(1);
         background(0);
-        posx[indexx]=x;
-        indexx++;
+        posx.add(x);
         //noLoop();
     }
 
     @Override
     public void draw() {
-        nextx = x + random(60);
-        if (ci){
-            System.out.println("circle");
-            strokeWeight(7);
-            stroke(255,255,255);
-            noFill();
-            ellipse(x,y,radius,radius);
-            ci=false;
-            x = x+radius/2;
-            posx[indexx]=x;
-            indexx++;
-            }
-        else{
-            if (nextx<w) {
-                System.out.println("line horizontal");
-                line(x,y,nextx,y);
-                ci=true;
-                x = nextx+radius/2;
-                posx[indexx]=x;
-                indexx++;
-                }                    
-            else{
-                float cy=y;
-                y+=random(60)+radius/2;
-                x=60;
-                line(x,cy,x,y);
-                y+=random(60)+radius/2;                
+        if (first_row) {
+            end=false;
+            if (drawC) {
+                x = x + radius / 2;                
+                draw_circle(x,y,radius);
+                drawC = false;
+                x = x + radius / 2;
+                nextx = x + random(20, 60);
+            } else {
+                if (nextx < w - radius) {
+                    draw_line(x, y, nextx, y);
+                    drawC = true;
+                    x = nextx;
+                    posx.add(x);
+                } else {
+                    indexx=0;
+                    drawC = true;
+                    first_row = false;
+                }
             }
         }
-    }    
+    else{
+        if (indexx==0){
+            prevy = y + radius/2;
+            y = prevy+random(30,80);
+            if (y<h-radius){
+                x=posx.get(indexx)+radius/2;
+                draw_line(x, prevy, x, y);
+                y += radius/2;
+                drawC=true;
+            }
+            else{
+                /*first_row=true;
+                drawC=true;
+                x=30;
+                y=60;*/
+                noLoop();
+                end=true;
+                save("bore.png");
+            }
+        }
+        if (indexx < posx.size() && !end){
+            if (drawC){
+                x=posx.get(indexx)+radius/2;
+                draw_circle(x, y, radius);
+                draw_line(x, prevy, x, y-radius/2);
+                x+=radius/2;
+                drawC=false;
+                indexx++;
+            }
+            else{
+                draw_line(x, y, posx.get(indexx), y);
+                drawC=true;
+            }
+        }
+        else{
+            indexx=0;
+        }
+    }
+    }   
+    
+    public void draw_circle(float x, float y, float rad){
+        strokeWeight(5);
+        r += random(-cChange, cChange);
+        r = constrain(r, 0, 256);
 
+        g += random(-cChange, cChange);
+        g = constrain(g, 0, 256);
+
+        b += random(-cChange, cChange);
+        b = constrain(b, 0, 256);
+        //stroke(r,g,b);
+        stroke(255,255,255);
+        fill(g,r,b);
+        //noFill();
+        ellipse(x,y,rad,rad);
+    }
+
+    public void draw_line(float origx, float origy, float destx, float desty){
+        strokeWeight(7);
+        r += random(-cChange, cChange);
+        r = constrain(r, 0, 256);
+
+        g += random(-cChange, cChange);
+        g = constrain(g, 0, 256);
+
+        b += random(-cChange, cChange);
+        b = constrain(b, 0, 256);
+        stroke(r,g,b);
+        line(origx, origy, destx, desty);
+    }
     public static void main(String[] args) {
         String[] processingArgs = { "Not Boring John" };
         Borejohn mySketch = new Borejohn();
