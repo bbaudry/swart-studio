db1a569215f2f6f4714376682d1c79ce23f42633
bbaudry <baudry@kth.se>
2022-03-23 22:35:39 +0100

    Good show. Sounds like a secondary

--- schematic005/CR001/src/main/java/art/studio/OddJob001.java
+++ schematic005/CR001/src/main/java/art/studio/OddJob001.java
@@ -9,9 +9,13 @@ public class OddJob001  extends PApplet{
 
     int h = 1000;
     int w = 1000;
+    float cx;
+    float cy;
     int[] palette = {5,45,340,240,200}; //80,90
     boolean c;
     Random rd = new Random();
+    int s;
+    int hu;
 
 
     @Override
@@ -27,18 +31,54 @@ public class OddJob001  extends PApplet{
 
     @Override
     public void draw() {
-        int ind = (int)(random(palette.length));
-        if (rd.nextBoolean()){
-            stroke(palette[ind],80,90);
-            strokeCap(SQUARE);
-            strokeWeight(random(41,111));
-            line(random(0,w/2), random(0,h), random(w/2,w), random(0,h));
-            noStroke();
-        }
-        else{
-            fill(palette[ind],80,90);
-            float radius = random(h/10, 3*h/10);
-            ellipse(random(0,w), random(0,h), radius, radius);
+        hu = (int) (random(palette.length));
+        s = rd.nextInt() % 5;
+        float radius;
+        float angle1; 
+        float rand_angle;
+    float angle2;
+        switch (s) {
+            case 0:
+                // if (rd.nextBoolean()){
+                stroke(palette[hu], 80, 90);
+                strokeCap(SQUARE);
+                strokeWeight(random(11, 42));
+                cx=random(0, w);
+                cy=random(0, h);
+//                line(cx, cy, cx+random(w/11,w/3), cy+random(h/11,h/3));
+                noStroke();
+                // }
+                // else{
+                break;
+            case 1:
+                noStroke();    
+                fill(palette[hu], 80, 90);
+                radius = random(h / 42, 3 * h / 42);
+                ellipse(random(0, w), random(0, h), radius, radius);
+                // }
+                break;
+            case 2:
+                stroke(palette[hu], 80, 90);
+                strokeWeight(random(11, 42));
+                noFill();
+                radius = random(h / 42, 3 * h / 42);
+                ellipse(random(0, w), random(0, h), radius, radius);
+                break;
+            case 3:
+                angle1 = random(0, 2 * PI);
+                rand_angle = random(PI / 50, 40 * PI / 50);
+                angle2 = angle1 + rand_angle;
+                radius = random(h / 42, 3 * h / 42);
+                stroke(palette[hu], 80, 90);
+                strokeWeight(random(11, 42));
+                strokeCap(SQUARE);
+                noFill();
+                cx = random(0, w);
+                cy = random(0, h);
+                arc(cx, cy, radius * 2, radius * 2, angle1, angle2);
+                break;
+            case 4:
+                break;
         }
     }
 
