65b2b1afab791f9a2279949f6e639b8d1cf06641
bbaudry <baudry@kth.se>
2022-03-30 22:50:49 +0200

    the blue one that I described from the lunar module window is colored because it is glass coated

---
+++ schematic005/CR001/src/main/java/art/studio/Deps003.java
@@ -0,0 +1,103 @@
+package art.studio;
+
+import processing.core.PApplet;
+import java.util.Random;
+import java.io.*;
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
+
+
+public class Deps003 extends PApplet{
+    int w = 1000;
+    int h = 1000;
+    float x;
+    float y;
+    int nb_deps;
+    int hue_factor;
+    float bright_factor;
+    JSONParser parser = new JSONParser();
+    JSONArray jsonArray;
+    String dir = "schematic005/CR001/src/main/java/art/studio";
+    Iterator<?> dep_iterator;
+    LinkedHashMap<String,Integer> suppliers_count;
+    LinkedHashMap<String,Integer> suppliers_color;
+    int john;
+    int baldessari;
+    float x_inc;
+    float y_inc;
+    boolean dark_mode;
+
+    @Override
+    public void settings() {
+        size(w, h);
+        /* Get the file content into the JSONObject */
+        Object obj;
+        try {
+            obj = parser.parse(new FileReader(dir+"/asynch-http-dependency-tree-dependencies.json"));
+            jsonArray = (JSONArray)obj;
+            nb_deps = jsonArray.size();
+            dep_iterator = jsonArray.iterator();
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void setup() {
+        colorMode(HSB, 360, 100, 100);
+        noStroke();
+        x=0;
+        y=0;
+        baldessari = Double.valueOf(Math.sqrt(nb_deps)).intValue()+1; //sqrt of nb of deps: nb of shapes per row
+        john = 1;
+        x_inc=w/baldessari;
+        y_inc=h/baldessari;
+        Random r = new Random();
+        dark_mode = r.nextBoolean(); //knob: dark_mode
+        suppliers_count = new LinkedHashMap<String,Integer>();
+        suppliers_color = new LinkedHashMap<String,Integer>();
+        frameRate=1;
+        if (dark_mode) {background(45,0,0);}
+        else {background(45,0,100);};
+    }
+
+    @Override
+    public void draw() {
+        if (dep_iterator.hasNext()) {
+            JSONObject o = (JSONObject)(dep_iterator.next());
+            float depth = (float)((Long) o.get("depth"));
+            String sup = o.get("supplier").toString();
+            Integer sup_occ = suppliers_count.putIfAbsent(sup, 1);
+            if (sup_occ!=null) {Integer occ = suppliers_count.get(sup); suppliers_count.put(sup, occ+1);}
+            if (suppliers_color.putIfAbsent(sup, 1)==null){suppliers_color.put(sup, hue_factor);hue_factor=hue_factor+17;}
+            if (dark_mode) {fill(45,0,100);}
+            else {fill(45,0,0);}
+            rect(x+(4*depth), y+(4*depth), x_inc-(8*depth), y_inc-(8*depth)); //knob : 4, 8 
+            if (john<baldessari){
+                x=x+x_inc;
+                john++;
+            }
+            else{
+                x=0;
+                y=y+y_inc;
+                john=1;
+            }
+        }
+        else{
+            noLoop();
+            save("chain_#001.png");
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        String[] processingArgs = { "Deps003"};
+        Deps003 mySketch = new Deps003();
+        PApplet.runSketch(processingArgs, mySketch);
+    }
+
+    
+}
