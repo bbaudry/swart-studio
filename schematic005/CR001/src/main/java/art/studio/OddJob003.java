63789d73b44cb87fdb971aaa35a03eb5d2d3e143
bbaudry <baudry@kth.se>
2022-04-04 22:52:58 +0200

    -°.}><{.°-

--- schematic005/CR001/src/main/java/art/studio/OddJob003.java
+++ schematic005/CR001/src/main/java/art/studio/OddJob003.java
@@ -3,10 +3,10 @@ package art.studio;
 import processing.core.PApplet;
 import java.util.Random;
 
-public class OddJob003 extends PApplet{
+public class OddJob003 extends PApplet {
     int w = 1000;
     int h = 1000;
-    int[] palette = {20,140,260};
+    int[] palette = { 20, 140, 260, 340, 200 };
     float x1;
     float y1;
     float x2;
@@ -21,49 +21,56 @@ public class OddJob003 extends PApplet{
     @Override
     public void setup() {
         colorMode(HSB, 360, 100, 100);
-        background(230,80,40);
+        background(230, 80, 40);
         rand = new Random();
     }
 
     @Override
     public void draw() {
-        x1 = random(w/2);
-        y1 = random(h/2,h);
-        x2 = x1+random(w/random(2,11));
+        x1 = random(w/4,w/2);
+        y1 = random(h/2, 3*h/4);
+        x2 = x1 + random(w / random(2, 5));
         boolean john = rand.nextBoolean();
-        if (john){y2 = y1-random(h/random(2,11));}
-        else {y2 = y1+random(h/random(2,11));}
-        float sw = random(11,37);
+        if (john) {
+            y2 = y1 - random(h / random(2, 5));
+        } else {
+            y2 = y1 + random(h / random(2, 5));
+        }
+        float sw = random(11, 37);
         strokeWeight(sw);
         strokeCap(SQUARE);
-        int c = (int)random(palette.length);
-        fill(palette[c],80,80);
+        int c = (int) random(palette.length);
+        fill(palette[c], 80, 80);
         noStroke();
-        if (john){quad(x1,y1,x2,y2,x2,y1,x1,y1+(y1-y2));}
-        else {quad(x1,y1,x2,y2,x2,y1,x1,y1+(y2-y1));}
-        if (john){x1=x2;y1=y2;}
-        else {x1=x2;}
-        c = (int)random(palette.length);
-        fill(palette[c],80,80);
+        if (john) {
+            quad(x1, y1, x2, y2, x2, y1, x1, y1 + (y1 - y2));
+        } else {
+            quad(x1, y1, x2, y2, x2, y1, x1, y1 + (y2 - y1));
+        }
+        if (john) {
+            x1 = x2;
+            y1 = y2;
+        } else {
+            x1 = x2;
+        }
+        c = (int) random(palette.length);
+        fill(palette[c], 80, 80);
         noStroke();
-        float rad = random(w/10,3*w/10);
-        ellipse(x1,y1-rad/2,rad,rad);
-        c = (int)random(palette.length);
-        fill(palette[c],80,80);
-        y1=y1-rad;
-        x2=x1-w/random(2,11);
-        float baldessari = x1;
-        x1=x1+w/random(2,11);
-        x2=x2+baldessari;
-        triangle(x1, y1, x2, y1+random(h/random(2,11)), random(x2,x1), y1);
-        fill(10,0,100);
-        ellipse(x1,y1,10,10);
+        float rad = random(w / 10, 3 * w / 10);
+        ellipse(x1, y1 - rad / 2, rad, rad);
+        c = (int) random(palette.length);
+        fill(palette[c], 80, 80);
+        y1 = y1 - rad;
+        x2 = x1 - w / random(7, 11);
+        triangle(x1, y1, x2, y1, random(x2, x1), y1 - random(h / random(2, 11)));
+        fill(10, 0, 100);
+        ellipse(x1, y1, 10, 10);
         noLoop();
+        save("OddJob03.png");
     }
 
-
     public static void main(String[] args) {
-        String[] processingArgs = { "OddJob003"};
+        String[] processingArgs = { "OddJob003" };
         OddJob003 mySketch = new OddJob003();
         PApplet.runSketch(processingArgs, mySketch);
     }
---
+++ schematic005/CR001/src/main/java/art/studio/OddJob004.java
@@ -0,0 +1,121 @@
+package art.studio;
+
+import processing.core.PApplet;
+import java.util.Random;
+
+public class OddJob004 extends PApplet {
+    int w = 1000;
+    int h = 1000;
+    int[] palette = { 20, 140, 260 };
+    float x1;
+    float y1;
+    float x2;
+    float y2;
+    Random rand;
+    int color;
+
+    @Override
+    public void settings() {
+        size(w, h);
+    }
+
+    @Override
+    public void setup() {
+        colorMode(HSB, 360, 100, 100);
+        background(230, 80, 40);
+        rand = new Random();
+    }
+
+    @Override
+    public void draw() {
+        int step = 10;
+        for (float x = 0; x < w; x = x + w / step) {
+            for (float y = 0; y < h; y = y + h / step) {
+                int john = (int) random(8);
+                System.out.println(john);
+                switch (john) {
+                    case 1:
+                        tri1(x,y,x+w/step,y+h/step);
+                        break;
+                    case 2:
+                        tri2(x,y,x+w/step,y+h/step);
+                        break;
+                    case 3:
+                        tri3(x,y,x+w/step,y+h/step);
+                        break;
+                    case 4:
+                        tri4(x,y,x+w/step,y+h/step);
+                        break;
+                    case 5:
+                        moon1(x,y,x+w/step,y+h/step);
+                        break;
+                    case 6:
+                        moon2(x,y,x+w/step,y+h/step);
+                        break;
+                    case 7:
+                        cell1(x,y,x+w/step,y+h/step);
+                        break;
+                    case 0:
+                        cell2(x,y,x+w/step,y+h/step);
+                        break;
+                }
+            }
+        }
+        noLoop();
+    }
+
+    public void tri1(float x1, float y1, float x2, float y2) {
+        color = (int) random(palette.length);
+        fill(palette[color], 80, 80);
+        triangle(x1,y1,x2,y1,x1,y2);
+    }
+
+    public void tri2(float x1, float y1, float x2, float y2) {
+        color = (int) random(palette.length);
+        fill(palette[color], 80, 80);
+        triangle(x1,y1,x2,y2,x1,y2);
+    }
+
+    public void tri3(float x1, float y1, float x2, float y2) {
+        color = (int) random(palette.length);
+        fill(palette[color], 80, 80);
+        triangle(x1,y2,x2,y1,x2,y2);
+    }
+
+    public void tri4(float x1, float y1, float x2, float y2) {
+        color = (int) random(palette.length);
+        fill(palette[color], 80, 80);
+        triangle(x1,y1,x2,y1,x2,y2);
+    }
+
+    public void moon1(float x1, float y1, float x2, float y2) {
+        color = (int) random(palette.length);
+        fill(palette[color], 80, 80);
+        arc(x1,y1+((y2-y1)/2),x2-x1,y2-y1,PI+HALF_PI,HALF_PI,PIE);
+    }
+
+    public void moon2(float x1, float y1, float x2, float y2) {
+        color = (int) random(palette.length);
+        fill(palette[color], 80, 80);
+        arc(x2,y1+((y2-y1)/2),x2-x1,y2-y1,HALF_PI,PI+HALF_PI,PIE);
+    }
+
+    public void cell1(float x1, float y1, float x2, float y2) {
+        color = (int) random(palette.length);
+        fill(palette[color], 80, 80);
+        rect(x1,y1,x2,y1,x2,y2,x1,y2);
+    }
+
+    public void cell2(float x1, float y1, float x2, float y2) {
+        color = (int) random(palette.length);
+        fill(palette[color], 80, 80);
+        ellipse(x1+((x2-x1)/2),y1+((y2-y1)/2),(x2-x1),(y2-y1));
+    }
+
+
+    public static void main(String[] args) {
+        String[] processingArgs = { "OddJob003" };
+        OddJob004 mySketch = new OddJob004();
+        PApplet.runSketch(processingArgs, mySketch);
+    }
+}
