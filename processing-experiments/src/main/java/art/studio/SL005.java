36e44fd471e31a681a960607857112316cc5ab61
bbaudry <baudry@kth.se>
2022-04-27 22:32:02 +0200

    }{+]><[+}{

--- processing-experiments/src/main/java/art/studio/SL005.java
+++ processing-experiments/src/main/java/art/studio/SL005.java
@@ -1,24 +1,16 @@
 package art.studio;
 
 import processing.core.PApplet;
-import java.util.Random;
-import art.Knob;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import org.json.simple.JSONObject;
 
 public class SL005 extends PApplet {
     int vis_w = 2000;
     int txt_w = 0;
     int w = vis_w+txt_w;
     int h = 1000;
-    int y_inc;
+    float y_inc;
+    float y_step;
     float x_inc;
+    float x_step;
 
     @Override
     public void settings() {
@@ -28,8 +20,10 @@ public class SL005 extends PApplet {
     @Override
     public void setup() {
         colorMode(HSB,360,100,100);
-        y_inc=22;
-        x_inc=62;
+        y_inc=11;
+        x_inc=42;
+        y_step=0.1f;
+        x_step=1;
     }
 
     @Override
@@ -38,7 +32,7 @@ public class SL005 extends PApplet {
         float not;
         float boring;
         background(0,0,0);
-        for(int baldessari=0; baldessari<h; baldessari=baldessari+y_inc){
+        for(float baldessari=0; baldessari<h; baldessari=baldessari+y_inc){
             john=0;
             while(john<w){
                 not=random(x_inc);
