b93f9e06c0f40d4c17a48c63aa47f40fc5537e10
bbaudry <baudry@kth.se>
2022-05-08 22:47:29 +0200

    .[+-><-+].

--- processing-experiments/src/main/java/art/studio/SL011.java
+++ processing-experiments/src/main/java/art/studio/SL011.java
@@ -5,10 +5,11 @@ import processing.core.PApplet;
 public class SL011 extends PApplet {
     int w = 1800;
     int h = 1000;
-    int x_step;
-    int y_step;
     int [][] miles;
     int davis;
+    float angle;
+    float angle_inc;
+    float ray;
 
     @Override
     public void settings() {
@@ -18,18 +19,27 @@ public class SL011 extends PApplet {
     @Override
     public void setup() {
         colorMode(HSB, 360, 100, 100);
-        x_step = 19;
-        y_step = 49;
-        davis = 1;
+        davis = 5;
         miles = new int[davis][];
+        angle=0;
+        angle_inc=PI/42;
+        frameRate(11);
+    }
+
+    @Override
+    public void draw() {
+        background(0,0,0);
+        ray=random(42);
         voodoo();
         down();
+        if (ray<7){feio();}
+        angle=angle+angle_inc;
     }
 
     private void voodoo(){
         for(int i=0;i<miles.length;i++){
-            int x = (int)random((float)0.4*w,(float)0.8*w);
-            int y = (int)random((float)0.3*h,(float)0.7*h);
+            int x = (int)random((float)0.1*w,(float)0.9*w);
+            int y = (int)random((float)0.1*h,(float)0.9*h);
             int[] mojo = {x,y};
             miles[i] = mojo;
         }
@@ -41,20 +51,26 @@ public class SL011 extends PApplet {
             int x = miles[i][0];
             int y = miles[i][1];
             float rad = random(w/10);
-            ellipse(x,y,rad,rad); System.out.println("draw");
+            ellipse(x,y,rad,rad);
         }
     }
 
     private void feio(){
         strokeWeight(7);
         stroke(0,0,100);
+        int ratio = 7;
+        float brew = PI/ratio;
+        float tx;
+        float ty;
         for(int i=0; i<miles.length; i++){
+            int cx = miles[i][0];
+            int cy = miles[i][1];
+            for(int j=0; j<ratio*2;j++){
+                tx = cx + w/2*cos(angle+j*brew);
+                ty = cy + w/2*sin(angle+j*brew);
+                line(cx,cy,tx,ty);    
+            }
         }
-    }
-
-    @Override
-    public void draw() {
-        background(0,0,0);
     }
 
    
