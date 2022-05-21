52244defd1cda8689049794605dee151434e8f44
bbaudry <baudry@kth.se>
2022-05-21 17:10:30 +0200

    .-+-><-+-.

--- processing-experiments/src/main/java/art/studio/Draugr004.java
+++ processing-experiments/src/main/java/art/studio/Draugr004.java
@@ -4,10 +4,9 @@ import processing.core.PApplet;
 import processing.core.PFont;
 import java.util.ArrayList;
 
-
 //Draugr is an exoplanet https://en.wikipedia.org/wiki/PSR_B1257%2B12_A
 //the Draugr series is about circular shapes and random black lines
-public class Draugr004  extends PApplet {
+public class Draugr004 extends PApplet {
     int w = 1000;
     int h = 1000;
     float cx;
@@ -17,11 +16,10 @@ public class Draugr004  extends PApplet {
     int knobs_index;
     int fSize;
     int yInc;
-    PFont f;  
-    float y;  
+    PFont f;
+    float y;
     boolean grow;
     int max;
-
 
     @Override
     public void settings() {
@@ -30,23 +28,23 @@ public class Draugr004  extends PApplet {
 
     @Override
     public void setup() {
-        cx=w/2;
-        cy=h/2;
-        radius = (float)(w*0.8);
-        background(242,242,217);
-        fill(233,233,211);
- 
+        cx = w / 2;
+        cy = h / 2;
+        radius = (float) (w * 0.8);
+        background(242, 242, 217);
+        fill(233, 233, 211);
+
         knobs = new ArrayList<Float>();
-        knobs_index=0;
+        knobs_index = 0;
         fSize = 16;
-        yInc = fSize+2;
-        f = createFont("Comfortaa",fSize,true);
-        y=h-yInc;
-        grow=true;
-        max=0;
-        textFont(f,fSize);
+        yInc = fSize + 2;
+        f = createFont("Comfortaa", fSize, true);
+        y = h - yInc;
+        grow = true;
+        max = 0;
+        textFont(f, fSize);
         textAlign(LEFT);
-        frameRate(1);
+        //frameRate(1);
 
     }
 
@@ -56,98 +54,100 @@ public class Draugr004  extends PApplet {
             if (random(3) < 1) {
                 stroke(0, 0, 0, random(51, 199));
             } else {
-                stroke(242,242,217);
+                stroke(242, 242, 217);
             }
             approx_chord(cx, cy, radius / 2);
         }
-        //erase numbers
+        // erase numbers
         noStroke();
-        fill(242,242,217);
+        fill(242, 242, 217);
         rect(0, 0, (float) (w * 0.1), h);
-        //write numbers
-        fill(0,0,0);
+        // write numbers
+        fill(0, 0, 0);
         if (grow) {
             int taille = knobs.size();
             if (taille * yInc < h) {
-                y = y - taille*yInc;
-                max=taille;
-//                for (int i = taille - 1; i > 0; i--) {
-//                    text(knobs.get(i).toString(), 10, y + (i * yInc));
-                  for(int i=0; i<max;i++){
-                      text(knobs.get(taille-i-1).toString(), 10, y + (i * yInc));
-                            System.out.println("still growing "+knobs.get(i).toString());
+                y = y - taille * yInc;
+                max = taille;
+                // for (int i = taille - 1; i > 0; i--) {
+                // text(knobs.get(i).toString(), 10, y + (i * yInc));
+                for (int i = 0; i < max; i++) {
+                    text(knobs.get(i).toString(), 10, y + (i * yInc));
+                    System.out.println("still growing " + i + " " + knobs.get(i));
                 }
+                
             } else {
                 grow = false;
             }
-        } 
-        else {
-            y=0;
+        } else {
+            y = 0;
             int taille = knobs.size();
-            for(int i=0; i<taille-max;i++){
-                text(knobs.get(taille-i-1).toString(), 10, y + (i * yInc));
-                System.out.println("not growing "+knobs.get(i).toString());
+            for (int i = 0; i < max+1; i++) {
+                text(knobs.get(taille - max -1 + i).toString(), 10, y + (i * yInc));
             }
         }
     }
 
-
-    // draw chords in the circle centered on (x,y) and radius. The chords are not straight
+    // draw chords in the circle centered on (x,y) and radius. The chords are not
+    // straight
     public void approx_chord(float x, float y, float radius) {
-        float angle = random((float)(Math.PI/2),(float)(3*Math.PI/2)); //random angle on the left half of the circle
-        float left_x = x + radius*cos(angle);
-        float left_y = y + radius*sin(angle);
-        knobs.add(knobs_index,angle);
+        float angle = random((float) (Math.PI / 2), (float) (3 * Math.PI / 2)); // random angle on the left half of the
+                                                                                // circle
+        float left_x = x + radius * cos(angle);
+        float left_y = y + radius * sin(angle);
+        knobs.add(knobs_index, angle);
         knobs_index++;
-        float angle_sym = (float)(2*3*Math.PI/2-angle);//angle to get the symmetric on the right half of the circle
-        float right_x = x + radius*cos(angle_sym);
-        float right_y = y + radius*sin(angle_sym);//sy2 should be equal to sy1 according to the way we compute angle_sym as the symmetric of angle on an horizontal axis
-        strokeWeight(random(2,8));
-        float chord_length=right_x-left_x;
-        float tmpx=left_x;
-        float tmpxdest=tmpx + random(chord_length/5);
+        float angle_sym = (float) (2 * 3 * Math.PI / 2 - angle);// angle to get the symmetric on the right half of the
+                                                                // circle
+        float right_x = x + radius * cos(angle_sym);
+        float right_y = y + radius * sin(angle_sym);// sy2 should be equal to sy1 according to the way we compute
+                                                    // angle_sym as the symmetric of angle on an horizontal axis
+        strokeWeight(random(2, 8));
+        float chord_length = right_x - left_x;
+        float tmpx = left_x;
+        float tmpxdest = tmpx + random(chord_length / 5);
         float tmpy = left_y;
-        float tmpydest = tmpy + random(-3,3);
-        while(tmpxdest<right_x){
-            line(tmpx,tmpy,tmpxdest,tmpydest);
-            tmpx=tmpxdest;
-            tmpy=tmpydest;
-            tmpxdest = tmpx + random(chord_length/5);
-            tmpydest = tmpy + random(-2,2);
+        float tmpydest = tmpy + random(-3, 3);
+        while (tmpxdest < right_x) {
+            line(tmpx, tmpy, tmpxdest, tmpydest);
+            tmpx = tmpxdest;
+            tmpy = tmpydest;
+            tmpxdest = tmpx + random(chord_length / 5);
+            tmpydest = tmpy + random(-2, 2);
         }
-        line(tmpx,tmpy,right_x,right_y);
+        line(tmpx, tmpy, right_x, right_y);
         noFill();
     }
 
-
-        // draws lines around a center
-    // x=h+r*cosθ; y=k+r*sinθ ; r is the radius of the circle; h,k are the coordinates of the center.
+    // draws lines around a center
+    // x=h+r*cosθ; y=k+r*sinθ ; r is the radius of the circle; h,k are the
+    // coordinates of the center.
     public void crater(float x, float y, float radius) {
-        strokeWeight(random((float)(0.5),3));
-        stroke(0,0,0,87);
+        strokeWeight(random((float) (0.5), 3));
+        stroke(0, 0, 0, 87);
         float ori_x = x + radius;
         float ori_y = y;
         float dest_x;
         float dest_y;
-        float rotate=0;
-        float angle=0;
-        float twopi = (float)(2*Math.PI);
-        while (rotate<twopi){
-            angle = random((float)(2*Math.PI / 21),(float)(2*Math.PI / 11));
-            if (rotate + angle>=twopi){
-                angle=twopi-rotate; 
+        float rotate = 0;
+        float angle = 0;
+        float twopi = (float) (2 * Math.PI);
+        while (rotate < twopi) {
+            angle = random((float) (2 * Math.PI / 21), (float) (2 * Math.PI / 11));
+            if (rotate + angle >= twopi) {
+                angle = twopi - rotate;
             }
-            dest_x = x + radius*cos(angle+rotate);
-            dest_y = y + radius*sin(angle+rotate);
+            dest_x = x + radius * cos(angle + rotate);
+            dest_y = y + radius * sin(angle + rotate);
             line(ori_x, ori_y, dest_x, dest_y);
             ori_x = dest_x;
             ori_y = dest_y;
-            rotate = rotate+angle;
+            rotate = rotate + angle;
         }
     }
 
     public static void main(String[] args) {
-        String[] processingArgs = { "Draugr004"};
+        String[] processingArgs = { "Draugr004" };
         Draugr004 mySketch = new Draugr004();
         PApplet.runSketch(processingArgs, mySketch);
     }
