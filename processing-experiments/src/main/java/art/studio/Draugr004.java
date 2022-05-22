4da6f9c3ca4599bf2985dd48ad0543cf203d8b06
bbaudry <baudry@kth.se>
2022-05-22 11:19:06 +0200

    ++{*><*}++

--- processing-experiments/src/main/java/art/studio/Draugr004.java
+++ processing-experiments/src/main/java/art/studio/Draugr004.java
@@ -12,10 +12,12 @@ public class Draugr004 extends PApplet {
     float cx;
     float cy;
     float radius;
+    int radigue; //number of chords at each step
     ArrayList<Float> knobs;
     int knobs_index;
     int fSize;
     int yInc;
+    float baldessari; // width of the rectangle on which numbers are written (its height is always h)
     PFont f;
     float y;
     boolean grow;
@@ -28,12 +30,13 @@ public class Draugr004 extends PApplet {
 
     @Override
     public void setup() {
-        cx = w / 2;
+        baldessari = (float)(0.1*w);
+        cx = w / 2 + baldessari / 2; // cx is shifted right of half of w to account for the text width on the left
         cy = h / 2;
         radius = (float) (w * 0.8);
         background(242, 242, 217);
         fill(233, 233, 211);
-
+        radigue=11;
         knobs = new ArrayList<Float>();
         knobs_index = 0;
         fSize = 16;
@@ -44,13 +47,16 @@ public class Draugr004 extends PApplet {
         max = 0;
         textFont(f, fSize);
         textAlign(LEFT);
-        //frameRate(1);
-
     }
 
     @Override
     public void draw() {
-        for (int i = 0; i < 11; i++) {
+        chords();
+        numbers();
+    }
+
+    public void chords (){
+        for (int i = 0; i < radigue; i++) {
             if (random(3) < 1) {
                 stroke(0, 0, 0, random(51, 199));
             } else {
@@ -58,50 +64,22 @@ public class Draugr004 extends PApplet {
             }
             approx_chord(cx, cy, radius / 2);
         }
-        // erase numbers
-        noStroke();
-        fill(242, 242, 217);
-        rect(0, 0, (float) (w * 0.1), h);
-        // write numbers
-        fill(0, 0, 0);
-        if (grow) {
-            int taille = knobs.size();
-            if (taille * yInc < h) {
-                y = y - taille * yInc;
-                max = taille;
-                // for (int i = taille - 1; i > 0; i--) {
-                // text(knobs.get(i).toString(), 10, y + (i * yInc));
-                for (int i = 0; i < max; i++) {
-                    text(knobs.get(i).toString(), 10, y + (i * yInc));
-                    System.out.println("still growing " + i + " " + knobs.get(i));
-                }
-                
-            } else {
-                grow = false;
-            }
-        } else {
-            y = 0;
-            int taille = knobs.size();
-            for (int i = 0; i < max+1; i++) {
-                text(knobs.get(taille - max -1 + i).toString(), 10, y + (i * yInc));
-            }
-        }
     }
 
     // draw chords in the circle centered on (x,y) and radius. The chords are not
     // straight
     public void approx_chord(float x, float y, float radius) {
-        float angle = random((float) (Math.PI / 2), (float) (3 * Math.PI / 2)); // random angle on the left half of the
-                                                                                // circle
+         // random angle on the left half of the circle; (x,y) on the edge of the circle at angle
+        float angle = random((float) (Math.PI / 2), (float) (3 * Math.PI / 2));
         float left_x = x + radius * cos(angle);
         float left_y = y + radius * sin(angle);
         knobs.add(knobs_index, angle);
         knobs_index++;
-        float angle_sym = (float) (2 * 3 * Math.PI / 2 - angle);// angle to get the symmetric on the right half of the
-                                                                // circle
+        // symmetric angle on the right half of the circle; (x,y) on the edge of the circle at angle
+        float angle_sym = (float) (2 * 3 * Math.PI / 2 - angle);
         float right_x = x + radius * cos(angle_sym);
-        float right_y = y + radius * sin(angle_sym);// sy2 should be equal to sy1 according to the way we compute
-                                                    // angle_sym as the symmetric of angle on an horizontal axis
+        float right_y = y + radius * sin(angle_sym);
+        //draw a chord from left to right, made of lines with a random slop between -3 and 3 
         strokeWeight(random(2, 8));
         float chord_length = right_x - left_x;
         float tmpx = left_x;
@@ -119,30 +97,31 @@ public class Draugr004 extends PApplet {
         noFill();
     }
 
-    // draws lines around a center
-    // x=h+r*cosθ; y=k+r*sinθ ; r is the radius of the circle; h,k are the
-    // coordinates of the center.
-    public void crater(float x, float y, float radius) {
-        strokeWeight(random((float) (0.5), 3));
-        stroke(0, 0, 0, 87);
-        float ori_x = x + radius;
-        float ori_y = y;
-        float dest_x;
-        float dest_y;
-        float rotate = 0;
-        float angle = 0;
-        float twopi = (float) (2 * Math.PI);
-        while (rotate < twopi) {
-            angle = random((float) (2 * Math.PI / 21), (float) (2 * Math.PI / 11));
-            if (rotate + angle >= twopi) {
-                angle = twopi - rotate;
+    public void numbers(){
+        // erase numbers
+        noStroke();
+        fill(242, 242, 217);
+        rect(0, 0, baldessari, h);
+        // write numbers
+        fill(0, 0, 0);
+        if (grow) {
+            int taille = knobs.size();
+            if (taille * yInc < h) {
+                y = y - taille * yInc;
+                max = taille;
+                for (int i = 0; i < max; i++) {
+                    text(knobs.get(i).toString(), 10, y + (i * yInc));
+                }
+                
+            } else {
+                grow = false;
             }
-            dest_x = x + radius * cos(angle + rotate);
-            dest_y = y + radius * sin(angle + rotate);
-            line(ori_x, ori_y, dest_x, dest_y);
-            ori_x = dest_x;
-            ori_y = dest_y;
-            rotate = rotate + angle;
+        } else {
+            y = 0;
+            int taille = knobs.size();
+            for (int i = 0; i < max+1; i++) {
+                text(knobs.get(taille - max -1 + i).toString(), 10, y + (i * yInc));
+            }
         }
     }
 
