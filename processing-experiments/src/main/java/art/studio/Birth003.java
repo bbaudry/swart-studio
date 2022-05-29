b451d6f819bb4361b54e3fb1b96eebe3e9355c94
bbaudry <baudry@kth.se>
2022-05-29 22:31:01 +0200

    {]{.><.}[}

--- processing-experiments/src/main/java/art/studio/Birth003.java
+++ processing-experiments/src/main/java/art/studio/Birth003.java
@@ -33,7 +33,7 @@ public class Birth003 extends PApplet {
     public void setup() {
         colorMode(HSB, 360, 100, 100);
         rand = new Random();
-        frameRate(1);
+        frameRate(5);
         force=100;
         ciel=100;
         noStroke();
@@ -45,20 +45,23 @@ public class Birth003 extends PApplet {
 
     @Override
     public void draw() {
-        if (grow && x1>w && y1>h) {grow=false;x1=0;y1=0;}
-       
-            poem = random(rimbaud);
-            x2 = x1 + poem;
-            y2 = y1 + poem;
-            bleu();
-            //paves();
-            x1 = x2;
-            y1 = y2;
-
-        if (!grow  && x1>w && y1>h) {
+        if (grow && x1 > w && y1 > h) {
+            grow = false;
             x1 = 0;
             y1 = 0;
-            noLoop();//background(palettej[1], force, ciel);
+        }
+        poem = random(rimbaud);
+        x2 = x1 + poem;
+        y2 = y1 + poem;
+        // bleu();
+        //perles();
+        paves();
+        x1 = x2;
+        y1 = y2;
+        if (!grow && x1 > w && y1 > h) {
+            x1 = 0;
+            y1 = 0;
+            noLoop();// background(palettej[1], force, ciel);
         }
     }
 
@@ -76,25 +79,38 @@ public class Birth003 extends PApplet {
 
     private void perles(){
         float t;
-        float x3 = random(x1,x2);
-        float y3 = random(y1,y2);
-        for(int i=0;i<rimbaud;i++){//draw rimbaud circles on the segment (0,y3),(x3,0)
-            // general rule:
-            // given 2 points (x1,y1) and (x2,y2) and t in [0,1]; 
-            // ((1-t)*x1+t*x2, (1-t)*y1+t*y2) are the coordinates of a point on the segment
-            t=random(1);
-            float cx = (1-t)*0+(t*x3);
-            float cy = (1-t)*y3+(t*0);
-            hu = (int)random(palettej.length);
-            fill(palettej[hu],force,ciel);
-            float rad = random(poem,3*poem);
-            ellipse(cx,cy,rad,rad);
+        float x3 = random(x1, x2);
+        float y3 = random(y1, y2);
+        if (grow) {
+            for (int i = 0; i < rimbaud; i++) {// draw rimbaud circles on the segment (0,y3),(x3,0)
+                // general rule:
+                // given 2 points (x1,y1) and (x2,y2) and t in [0,1];
+                // ((1-t)*x1+t*x2, (1-t)*y1+t*y2) are the coordinates of a point on the segment
+                t = random(1);
+                float cx = (1 - t) * 0 + (t * x3);
+                float cy = (1 - t) * y3 + (t * 0);
+                hu = (int) random(palettej.length);
+                fill(palettej[hu], force, ciel);
+                float rad = random(poem, 3 * poem);
+                ellipse(cx, cy, rad, rad);
+            }
+        } else {
+            for (int i = 0; i < rimbaud; i++) {// draw rimbaud circles on the segment (x3,h),(w,y3)
+                t = random(1);
+                float cx = (1 - t) * x3 + (t * w);
+                float cy = (1 - t) * h + (t * y3);
+                hu = (int) random(palettej.length);
+                fill(palettej[hu], force, ciel);
+                float rad = random(poem, 3 * poem);
+                ellipse(cx, cy, rad, rad);
+            }
         }
     }
 
     private void paves(){
         float t;
-        for(int i=0;i<rimbaud;i++){//draw rimbaud circles on the segment (0,y2),(x2,0)
+        if(grow){
+        for(int i=0;i<rimbaud;i++){//draw rimbaud quadrilaterales on the segment (0,y2),(x2,0)
             // general rule:
             // given 2 points (x1,y1) and (x2,y2) and t in [0,1]; 
             // ((1-t)*x1+t*x2, (1-t)*y1+t*y2) are the coordinates of a point on the segment
@@ -113,6 +129,25 @@ public class Birth003 extends PApplet {
             else{
                 quad(cx1,cy1,cx1,cy1-rad,cx2,cy2-rad,cx2,cy2);
             }
+        }}
+        else{
+            for(int i=0;i<rimbaud;i++){//draw rimbaud quadrilaterales on the segment (x2,h),(w,y2)
+            t=random(1);
+            float cx1 = (1-t)*x2+(t*w);
+            float cy1 = (1-t)*h+(t*y2);
+            t=random(1);
+            float cx2 = (1-t)*x2+(t*w);
+            float cy2 = (1-t)*h+(t*y2);
+            hu = (int)random(palettej.length);
+            fill(palettej[hu],force,ciel);
+            float rad = random(poem);
+            if(cx1>cx2){
+                quad(cx2,cy2,cx2,cy2-rad,cx1,cy1-rad,cx1,cy1);
+            }
+            else{
+                quad(cx1,cy1,cx1,cy1-rad,cx2,cy2-rad,cx2,cy2);
+            }
+        }
         }
     }
 
