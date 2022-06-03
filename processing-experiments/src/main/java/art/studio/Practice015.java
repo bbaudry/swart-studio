a3e19a17a6898b403a9a3f6c927312d359c01074
bbaudry <baudry@kth.se>
2022-06-03 22:40:21 +0200

    {]+.><.+[}

--- processing-experiments/src/main/java/art/studio/Practice015.java
+++ processing-experiments/src/main/java/art/studio/Practice015.java
@@ -10,7 +10,7 @@ public class Practice015  extends PApplet {
     float cy1 = 0;
     float cx2 = 300;
     float cy2 = h;
-    float eliane = 5;
+    float eliane;
     float len;
     float wid;
     int steps;
@@ -23,8 +23,7 @@ public class Practice015  extends PApplet {
     @Override
     public void setup() {
         colorMode(HSB,360,100,100);
-        background(1,0,0);
-        steps = 272; 
+        background(1,0,0); 
         frameRate(7);
     }
 
@@ -32,6 +31,8 @@ public class Practice015  extends PApplet {
     public void draw() {
         background(1,0,0);
         stroke(random(360),0,0);
+        eliane = 5; //width of a stripe
+        steps = 272; //number of stripes
         //coordinates of origin and destination of one stripe
         cx1 = 0; cy1 = 0; 
         cx2 = 300; cy2 = h;
@@ -42,8 +43,8 @@ public class Practice015  extends PApplet {
 
         float inc = (float)1/steps;
         fill(0,0,100);
-        for(int i=0; i<steps; i++){ //steps is the number of stripe
-            cx3=cx1+(i*eliane); //eliane is the number of shapes per stripe
+        for(int i=0; i<steps; i++){ 
+            cx3=cx1+(i*eliane); 
             cy3=cy1;
             for (int j=0; j<steps; j++){
                 t = (float)((j+1)*inc); 
@@ -57,7 +58,29 @@ public class Practice015  extends PApplet {
                 cy3=cy4+pad;    
             }
         }
-        noLoop();
+
+        eliane = 19; //width of a stripe
+        steps = 42; //number of stripes
+        //coordinates of origin and destination of one stripe
+        cx1 = 0; cy1 = h; 
+        cx2 = w; cy2 = (float)0.8*h;
+        inc = (float)1/steps;
+        for(int i=0; i<steps; i++){ 
+            cx3=cx1+(i*eliane); 
+            cy3=cy1;
+            for (int j=0; j<steps; j++){
+                t = (float)((j+1)*inc); 
+                float pad = random(eliane); //pad is a random offset for each shape
+                cx4 = (1 - t) * (cx1+(i*eliane)) + (t * cx2);
+                cy4 = (1 - t) * cy1 + (t * cy2);
+                fill(random(30,60),80,100);
+                quad(cx3-pad,cy3-pad,cx3+eliane,cy3-pad,cx4+eliane,cy4,cx4-pad,cy4);
+                //if(random(11)<2){inner(cx3-pad,cx4,cy3-pad,cy4);}              
+                cx3=cx4;
+                cy3=cy4+pad;    
+            }
+        }
+        //noLoop();
     } 
 
     private void inner(float cx1, float cx2,float cy1, float cy2){
