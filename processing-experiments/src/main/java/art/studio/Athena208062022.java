22c4abc54393ddca254b285b64fc922da62f218f
bbaudry <baudry@kth.se>
2022-06-08 22:38:31 +0200

    }-.]><[.-{

--- processing-experiments/src/main/java/art/studio/Athena208062022.java
+++ processing-experiments/src/main/java/art/studio/Athena208062022.java
@@ -4,8 +4,8 @@ package art.studio;
 import processing.core.PApplet;
 
 public class Athena208062022 extends PApplet {
-    int w = 1000;
-    int h = 1500;
+    int w = 3000;
+    int h = 3000;
     float x1;
     float x2;
     float y1;
@@ -19,46 +19,68 @@ public class Athena208062022 extends PApplet {
 
     @Override
     public void setup() {
-        colorMode(HSB,360,100,100);
-        background(230,100,80);
+        colorMode(HSB, 360, 100, 100);
+        background(230, 100, 80);
     }
 
     @Override
     public void draw() {
-        background(230,100,80);
-        noStroke();//stroke(0,0,100);
+        background(230, 100, 80);
+        noStroke();// stroke(0,0,100);
         chercheur();
         dor();
         noLoop();
+        save("Athena208062022.png");
     }
 
-    private void chercheur(){
-        float x=0;
-        float y=0;
-        float chanteuse = w/2;
-        float moqueuse = random(42);
-        while(y<h){
-            if (random(42)>2){
-                fill(random(45,55),100,100);
-                rect(x,y,chanteuse,moqueuse);
+    private void chercheur() {
+        float x = 0;
+        float y = 0;
+        float chanteuse = w / 2;
+        float moqueuse;
+        float rieuse = chanteuse / 42;
+        while (y < h) {
+            moqueuse = 42;//random(42);
+            while (x < chanteuse) {
+                if (random(42) > 2) {
+                    fill(random(45, 55), 100, 100);
+                    rect(x, y, rieuse, moqueuse);
+                }
+                if (random(42) > 40) {
+                    fill(random(45, 55), 100, 100);
+                    rect(x, y, rieuse*3, moqueuse);
+                    x=x+2*rieuse;
+                }
+                x=x+rieuse;
             }
-            y=y+moqueuse;
+            x=0;
+            y = y + moqueuse;
         }
     }
 
-    private void dor(){
-        float x=w/2;
-        float y=0;
-        float chanteuse = w/2;
-        float moqueuse = random(84);
-        while(y<h){
-            if (random(42)>2){
-                fill(random(45,55),100,100);
-                rect(x,y,chanteuse,moqueuse);
+    private void dor() {
+        float x = w / 2;
+        float y = 0;
+        float chanteuse = w / 2;
+        float moqueuse;
+        float rieuse = chanteuse / 21;
+        while (y < h) {
+            moqueuse = 84;//random(84);
+            while (x < w) {
+                if (random(42) > 2) {
+                    fill(random(45, 55), 100, 100);
+                    rect(x, y, rieuse, moqueuse);
+                }
+                if (random(42) > 40) {
+                    fill(random(45, 55), 100, 100);
+                    rect(x, y, rieuse*3, moqueuse);
+                    x=x+2*rieuse;
+                }
+                x=x+rieuse;
             }
-            y=y+moqueuse;
+            x=w/2;
+            y = y + moqueuse;
         }
-
     }
 
     public static void main(String[] args) {
