e235d210d4f22911690ffcdcf2af52fcff92a8bc
bbaudry <baudry@kth.se>
2022-06-09 17:39:20 +0200

    *-°-><-°-*

--- processing-experiments/src/main/java/art/studio/Athena209062022.java
+++ processing-experiments/src/main/java/art/studio/Athena209062022.java
@@ -2,7 +2,7 @@
 package art.studio;
 
 import processing.core.PApplet;
-
+import java.util.Random;
 import java.util.ArrayList;
 
 public class Athena209062022 extends PApplet {
@@ -12,6 +12,7 @@ public class Athena209062022 extends PApplet {
     float cy;
     float rad;
     ArrayList<Float[]> etoiles;
+    Random alea;
     
     @Override
     public void settings() {
@@ -22,18 +23,20 @@ public class Athena209062022 extends PApplet {
     public void setup() {
         colorMode(HSB, 360, 100, 100);
         background(230, 100, 80);
+        cx = random((float)0.45,(float)0.55)*w;
+        cy = random((float)0.45,(float)0.55)*h;
+        rad = random((float)0.3,(float)0.4)*w;
+        etoiles = new ArrayList<>();
+        alea = new Random();
     }
 
     @Override
     public void draw() {
         background(230, 100, 80);
         noStroke();
-        etoiles = new ArrayList<>();
-        cx = random((float)0.45,(float)0.55)*w;
-        cy = random((float)0.45,(float)0.55)*h;
-        rad = random((float)0.3,(float)0.4)*w;
         chercheur();
         dor();
+        frameRate(2);
         //noLoop();
 //        save("Athena209062022.png");
     }
@@ -52,13 +55,23 @@ public class Athena209062022 extends PApplet {
     }
 
     private void dor() {
-        for(int rays=0;rays<84;rays++){
-        int i = (int)random(etoiles.size());
-        int j = (int)random(etoiles.size());
-        stroke(50,100,100);
+        for(int rays=0;rays<168;rays++){
+            int i = alea.nextInt(etoiles.size()); // (int)random(etoiles.size());
+            int j = alea.nextInt(etoiles.size()); //(int)random(etoiles.size());
+            stroke(50,100,100);
+            strokeWeight(2);
             //line(etoiles.get(i)[0],etoiles.get(i)[1],etoiles.get(j)[0],etoiles.get(j)[1]);
             noFill();
-            bezier(etoiles.get(i)[0],etoiles.get(i)[1],etoiles.get(i)[0],etoiles.get(i)[1]-84,etoiles.get(j)[0]+42,etoiles.get(j)[1]+42,etoiles.get(j)[0],etoiles.get(j)[1]);
+            int danseuse = alea.nextInt(7)*42;
+            if(alea.nextBoolean()){
+                bezier(etoiles.get(i)[0],etoiles.get(i)[1],etoiles.get(i)[0],etoiles.get(i)[1]-danseuse,etoiles.get(j)[0]+danseuse,etoiles.get(j)[1]+danseuse,etoiles.get(j)[0],etoiles.get(j)[1]);
+            }
+            else{
+                bezier(etoiles.get(i)[0],etoiles.get(i)[1],etoiles.get(i)[0],etoiles.get(i)[1]+danseuse,etoiles.get(j)[0]-danseuse,etoiles.get(j)[1]-danseuse,etoiles.get(j)[0],etoiles.get(j)[1]);    
+            }
+            fill(50,100,100);
+            ellipse(etoiles.get(i)[0],etoiles.get(i)[1],3,3);
+            ellipse(etoiles.get(j)[0],etoiles.get(j)[1],3,3);
         }  
     }
 
