9620369665c74e3b58c8e644e1aada3c8c340351
bbaudry <baudry@kth.se>
2022-06-14 15:18:46 +0200

    +**.><.**+

--- processing-experiments/src/main/java/art/studio/Birth006.java
+++ processing-experiments/src/main/java/art/studio/Birth006.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 import processing.core.PApplet;
 
 public class Birth006 extends PApplet {
-    int w = 3000;
-    int h = 3000;
+    int w = 1000;
+    int h = 1000;
     int[] palettef = { 0, 20, 40, 60, 80 };
     int lou = 69;
     float x;
@@ -37,81 +37,42 @@ public class Birth006 extends PApplet {
         background(42, 42, 84);
         rd = new Random();
         stervilin = new ArrayList<>();
-        // frameRate(1);
+        init();
     }
 
     @Override
     public void draw() {
-        if (constellations < lou*2) {
-            leonard();
-            constellations++;
-        } else {
-            if (rays < lou*4){
-                //cohen();
-                rays++;
-            }
-            else{
-                noLoop();
-                save("gift002.png");    
-            }
+        if (constellations < stervilin.size()-1){
+            point();
+            constellations++;        
+        }
+        else{
+            noLoop();
         }
     }
 
-    private void leonard() {
-        float cx = random(w);//random((float) 0.1, (float) 0.9) * w;
-        float cy = random(h);//random((float) 0.1, (float) 0.9) * h;
-        float rad = random(1/lou, (float) 0.25) * w;
-        int stars = lou-(rd.nextInt(lou)+1);
-        for (int i = 0; i < stars; i++) {
-            stroke(palettef[3], 100, 100);
-            float a1 = (2 * PI) * random(1);
-            float a2 = a1;
-            while (a2 == a1) {
-                a2 = (2 * PI) * random(1);
+    private void init(){
+        int step = lou*2;
+        float x = step;
+        float y = step;
+        while (x<w){
+            while (y<h){
+                Float[] sunshine = new Float[2];
+                sunshine[0]=random(x-step,x);
+                sunshine[1]=random(y-step,y);
+                stervilin.add(sunshine);
+                y=y+step;
             }
-            hu = palettef[(int) random(palettef.length - 1)];
-            float x1 = cx + random(rad / 2) * cos(a1);
-            float y1 = cy + random(rad / 2) * sin(a1);
-            float x2 = cx + random(rad / 2) * cos(a2);
-            float y2 = cy + random(rad / 2) * sin(a2);
-            noStroke();
-            fill(hu, random(lou, 100), random(lou, 100));
-            ellipse(x2, y2, 4, 4);
-            noFill();
-            stroke(hu, random(lou, 100), random(lou, 100));
-            if (rd.nextBoolean()) {
-                ellipse(x1, y1, 4, 4);
-                bezier(x1, y1, x1 + 10, y2 - 10, x2 - 10, y2 - 10, x2, y2);
-            } else {
-                bezier(cx, cy, cx + 10, cy - 10, x2 - 10, y2 - 10, x2, y2);
-            }
-            if (random(lou)<2){
-                Float[] laita = new Float[2];
-                laita[0]=x1;
-                laita[1]=y1;
-                stervilin.add(laita);
-            }
+            y=step;
+            x=x+step;
         }
     }
 
-    private void cohen(){
-        int start;
-        int end;
-        start = rd.nextInt(stervilin.size()-1);
-        end = start;
-        while(end==start){
-            end = rd.nextInt(stervilin.size()-1);
-        }
-        float cx1=stervilin.get(start)[0];
-        float cy1=stervilin.get(start)[1];
-        float cx2=stervilin.get(end)[0];
-        float cy2=stervilin.get(end)[1];
-        hu = palettef[(int) random(palettef.length - 1)];
-        noFill();
-        stroke(hu, random(lou, 100), random(lou, 100));
-        strokeWeight(2);
-        if((Math.abs(cx1-cx2)<w/2)&&(Math.abs(cy1-cy2)<h/2)){
-        bezier(cx1, cy1, cx1 + (lou*2), cy1 - (lou*2), cx2 - (lou*2), cy2 - (lou*2), cx2, cy2);}
+    private void point(){
+        hu=palettef[rd.nextInt(palettef.length-1)];
+        fill(hu,100,100);
+        noStroke();
+        ellipse(stervilin.get(constellations)[0],stervilin.get(constellations)[1],10,10);
     }
 
     public static void main(String[] args) {
