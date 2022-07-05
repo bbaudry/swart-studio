3cfb8f8b95ba11e9f1f45159499b3ee850932108
bbaudry <baudry@kth.se>
2022-07-05 23:53:07 +0200

    .[}}><{{].

--- processing-experiments/src/main/java/art/studio/SL017.java
+++ processing-experiments/src/main/java/art/studio/SL017.java
@@ -3,16 +3,21 @@ package art.studio;
 
 import processing.core.PApplet;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class SL017 extends PApplet {
     int w = 1000;
     int h = 1000;
-    int streets;
+    int street;
     int block;
     ArrayList<ArrayList<Float[]>> coords;
     int[] cyberHue = {182,360};
     int[] cyberSat = {80,100};
     int[] cyberBri = {80,100};
+    float hu;
+    boolean huInc;
+    Random alea;
+
     @Override
     public void settings() {
         size(w, h);
@@ -25,35 +30,62 @@ public class SL017 extends PApplet {
         coords = new ArrayList<>();
         init();
         noFill();
-        streets = 0;
+        alea = new Random();
+        street = 0;
         block = 0;
+        hu = cyberHue[0];
+        huInc = true;
         background(0,0,0);
      }
 
     @Override
     public void draw() {
-        if (streets < coords.size()){
-            int s = coords.get(streets).size();
+        street = alea.nextInt(coords.size());
+        block = alea.nextInt(coords.get(street).size()-1);
+        Float[] rick = coords.get(street).get(block);
+        Float[] roll = coords.get(street).get(block+1);
+        fill(0,0,0);
+        ellipse(rick[0], rick[1], 7, 7);
+        float eps = random(2,17);
+        if (hu<cyberHue[1] && huInc){
+            fill(hu,random(50,100),random(50,100));
+            hu = hu+(float)0.1;
+        }
+        else {huInc = false;}
+        if (hu>cyberHue[0] && !huInc){
+            fill(hu,random(50,100),random(50,100));
+            hu = hu-(float)0.1;
+        }
+        else {huInc = true;}
+        if (rick[0]<roll[0]){
+            quad(rick[0],rick[1],rick[0]-eps,rick[1]-eps,roll[0]-eps,roll[1]-eps,roll[0],roll[1]);
+        }
+        else{
+            quad(rick[0],rick[1],rick[0]+eps,rick[1]-eps,roll[0]+eps,roll[1]-eps,roll[0],roll[1]);    
+        }
+
+        //ellipse(rick[0], rick[1], eps, eps);
+    }
+
+/*         if (street < coords.size()){
+            int s = coords.get(street).size();
             if (block<s-1){
-                Float[] cairo = coords.get(streets).get(block);
-                Float[] purple = coords.get(streets).get(block+1);
-                //fill(50 + (block * 3), 100, 100);
+                Float[] cairo = coords.get(street).get(block);
+                Float[] purple = coords.get(street).get(block+1);
                 fill(random(cyberHue[0],cyberHue[1]),random(50,100),random(cyberBri[0],cyberBri[1]));
-                /*if (random(5)<2){fill(2,90,97);}
-                else{fill(182,90,97);}*/
                 float eps = random(7,13);
-                /*if (cairo[0]<purple[0]){
+                if (cairo[0]<purple[0]){
                     quad(cairo[0],cairo[1],cairo[0]-eps,cairo[1]-eps,purple[0]-eps,purple[1]-eps,purple[0],purple[1]);
                 }
                 else{
                     quad(cairo[0],cairo[1],cairo[0]+eps,cairo[1]-eps,purple[0]+eps,purple[1]-eps,purple[0],purple[1]);    
-                }*/
+                }
                 ellipse(cairo[0], cairo[1], eps, eps);
                 ellipse(purple[0], purple[1], eps, eps);
                 block++;
             }
             else{
-                streets++;
+                street++;
                 block=0;
             }
         }
@@ -61,11 +93,10 @@ public class SL017 extends PApplet {
             save("SL017.png");
             noLoop();
         }
-        
-    }
+*/
 
     private void init(){
-        float moon = random((float)0.03, (float)0.05)*w;
+        float moon = random((float)0.01, (float)0.03)*w;
         while (moon<w){
             bang(0,moon,moon,0, 1);
             bang(w-moon,h,w,h-moon,2);
@@ -80,12 +111,12 @@ public class SL017 extends PApplet {
         ArrayList<Float[]> avenue = new ArrayList<>();
         float step=100; //default value, changed in the following switch
         switch (knob){
-            case 1 : step = dx/80; break;
-            case 2 : step = (w-ox)/80; break;
-            case 3 : step = ox/80; break;
-            case 4 : step = (w-dx)/80; break;
+            case 1 : step = dx/20; break;
+            case 2 : step = (w-ox)/20; break;
+            case 3 : step = ox/20; break;
+            case 4 : step = (w-dx)/20; break;
         }
-        float inc = random(1,3)/step;
+        float inc = 4/step;//random(1,3)/step;
         float x1=ox;
         float x2;
         float y2;
@@ -102,7 +133,7 @@ public class SL017 extends PApplet {
             avenue.add(destination);
             x1 = x2;
             y1 = y2;
-            i=i+random(inc);
+            i=i+random(1,4)*inc;
         }
         coords.add(avenue);
     }
