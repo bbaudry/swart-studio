ea81237e38d999e1e5ffd4100181c4e2e5000ef0
bbaudry <baudry@kth.se>
2022-07-07 14:38:43 +0200

    -.]*><*[.-

--- processing-experiments/src/main/java/art/studio/SL017.java
+++ processing-experiments/src/main/java/art/studio/SL017.java
@@ -8,6 +8,8 @@ import java.util.Random;
 public class SL017 extends PApplet {
     int w = 1000;
     int h = 1000;
+    float john = w/442;
+    float baldessari = w/97;
     int street;
     int block;
     ArrayList<ArrayList<Float[]>> coords;
@@ -45,8 +47,8 @@ public class SL017 extends PApplet {
         Float[] rick = coords.get(street).get(block);
         Float[] roll = coords.get(street).get(block+1);
         fill(0,0,0);
-        build(rick[0],rick[1],roll[0],roll[1],17);    
-        float eps = random(2,17);
+        build(rick[0],rick[1],roll[0],roll[1],baldessari);    
+        float eps = random(john,baldessari);
         if (hu<cyberHue[1] && huInc){
             fill(hu,random(50,100),random(50,100));
             hu = hu+(float)0.1;
@@ -113,12 +115,13 @@ public class SL017 extends PApplet {
     private void bang(float ox, float oy, float dx, float dy, int knob){
         ArrayList<Float[]> avenue = new ArrayList<>();
         float step=100; //default value, changed in the following switch
+        int den=w/50;
         switch (knob){
-            case 1 : step = dx/20; break;
-            case 2 : step = (w-ox)/20; break;
-            case 3 : step = ox/20; break;
-            case 4 : step = (w-dx)/20; break;
-        }
+            case 1 : step = dx/den; break;
+            case 2 : step = (w-ox)/den; break;
+            case 3 : step = ox/den; break;
+            case 4 : step = (w-dx)/den; break;
+        }        
         float inc = 4/step;//random(1,3)/step;
         float x1=ox;
         float x2;
