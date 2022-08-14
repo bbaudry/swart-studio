1c2db186d1280221339655c0ccb3c93cdacdf485
bbaudry <baudry@kth.se>
2022-08-14 18:32:25 +0200

    +]-]><[-[+

--- processing-experiments/src/main/java/art/studio/Stervilin003.java
+++ processing-experiments/src/main/java/art/studio/Stervilin003.java
@@ -6,6 +6,8 @@ import processing.core.PApplet;
 public class Stervilin003 extends PApplet {
   int w = 1000;
   int h = 1000;
+  int steps = 10;
+
 
   @Override
   public void settings() {
@@ -15,24 +17,32 @@ public class Stervilin003 extends PApplet {
   @Override
   public void setup() {
     colorMode(HSB,360,100,100);
-    frameRate(17);
+    frameRate(5);
   }
 
   @Override
   public void draw() {
     background(230,10,100);
-    float x1 = random(w/200);
-    float y1 = random(h/200);
-    leaf(x1,y1);
+    float x1=0;// = random(w/200);
+    float y1=0;// = random(h/200);
+    for (int i=0;i<steps;i++){
+      for (int j=0;j<steps;j++){
+        leaf(x1+random(w/20),y1+random(h/20));
+        y1=y1+h/steps;
+      }
+      x1=x1+w/steps;
+      y1=0;
+    }
+    
   }
   
   private void leaf(float x1,float y1){
-    float x2 = x1+random(w/10);
-    float y2 = y1-random(y1);
-    float x3 = x1+random(x1);
-    float y3 = y2+random(h/10);
+    float x2 = x1+random(2*w/steps);
+    float y2 = y1-random(h/20);
+    float x3 = x1+random(w/20);
+    float y3 = y2+random(2*h/steps);
     noStroke();
-    fill(0,0,random(40,80));
+    fill(0,0,random(60,90));
     triangle(x1, y1, x2, y2, x3, y3);
   }
 
