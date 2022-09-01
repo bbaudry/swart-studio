9fa890118e78df906193bd14c459aa4ea767d5a8
bbaudry <baudry@kth.se>
2022-09-01 16:09:10 +0200

    +°}*><*{°+

--- processing-experiments/src/main/java/art/studio/Sand004.java
+++ processing-experiments/src/main/java/art/studio/Sand004.java
@@ -5,6 +5,7 @@ package art.studio;
 import processing.core.PApplet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
 public class Sand004 extends PApplet {
@@ -13,12 +14,14 @@ public class Sand004 extends PApplet {
   int h = 1000;//1080;
   int steps = 10;
   int hu;
+  int kid;
   int sa=100;
-  int[] rain = {10,30,50,70,90,110};
+  int[] rain = {190,210,230,250,270,290};
   Random alea = new Random();
   float x;
   float y;
   ArrayList<Float[]> baldessari = new ArrayList<>();
+  Iterator<Float[]> tomwaits;
 
   @Override
   public void settings() {
@@ -29,45 +32,60 @@ public class Sand004 extends PApplet {
   public void setup() {
     colorMode(HSB,360,100,100);
     hu = rain[alea.nextInt(rain.length-1)];    
+    background(hu,100,35);
     float s=w/2;
-    Float[] no = { (float)0, s };
-    Float[] more = {  s, (float)0 };
-    Float[] boring = {2*s, s };
-    Float[] code = { s, 2*s };
+    Float[] no = { (float)0, s, s, (float)0,2*s, s,s, 2*s  };
     baldessari.add(no);
-    baldessari.add(more);
-    baldessari.add(boring);
-    baldessari.add(code); 
-    background(hu+180,100,35);
     frameRate(2);
+    fill(30,100,100);   
+    float x1 = baldessari.get(0)[0];
+    float y1 = baldessari.get(0)[1];
+    float x2 = baldessari.get(0)[2];
+    float y2 = baldessari.get(0)[3]; 
+    float x3 = baldessari.get(0)[4];
+    float y3 = baldessari.get(0)[5];
+    float x4 = baldessari.get(0)[6];
+    float y4 = baldessari.get(0)[7]; 
+    quad(x1, y1, x2, y2, x3, y3, x4, y4);
+    kid=1;
+    tomwaits = baldessari.iterator();
+    hu=0;
+    prep();
   }
 
   @Override
   public void draw() {
-    fill(30,100,100);   
-    float x1 = baldessari.get(0)[0];
-    float y1 = baldessari.get(0)[1];
-    float x2 = baldessari.get(1)[0];
-    float y2 = baldessari.get(1)[1]; 
-    quad(x1,y1,x2,y2,baldessari.get(2)[0],baldessari.get(2)[1],baldessari.get(3)[0],baldessari.get(3)[1]);
-    prep();
-    noLoop();
+    if (kid<baldessari.size()){
+      System.out.println("one square");
+      Float[] eyeball = baldessari.get(kid);
+      float x1 = eyeball[0];
+      float y1 = eyeball[1];
+      float x2 = eyeball[2];
+      float y2 = eyeball[3]; 
+      float x3 = eyeball[4];
+      float y3 = eyeball[5];
+      float x4 = eyeball[6];
+      float y4 = eyeball[7];   
+      fill(hu, 100, 100);
+      quad(x1, y1, x2, y2, x3, y3, x4, y4);
+      hu = (hu + 90)%360;
+      kid++;
+    }
+
     //  save("Sand004.png");
   //  exit();
   }
 
   private void prep(){
     float t = (float)0.5;
-    float x1,y1,x2,y2,x3,y3,x4,y4;
-    for (int i=0;i<baldessari.size();i++){
-      int ind1 = i%4; 
-      int ind2 = (i+1)%4;
-      x1=baldessari.get(ind1)[0];
-      y1=baldessari.get(ind1)[1];
-      x2=baldessari.get(ind2)[0];
-      y2=baldessari.get(ind2)[1];
-      float px = (1-t) * x1 + (t * x2);
-      float py = (1-t) * y1 + (t * y2);
+    float x1,y1,x2,y2,x3,y3,x4,y4,px,py;
+    
+      x1=baldessari.get(0)[0];
+      y1=baldessari.get(0)[1];
+      x2=baldessari.get(0)[2];
+      y2=baldessari.get(0)[3];
+      px = (1-t) * x1 + (t * x2);
+      py = (1-t) * y1 + (t * y2);
       if(px>x1){
         if(py<y1){x3=px+(px-x1);x4=px;y4=y1+(y1-py);y3=y1;}
         else {x4=x1-(px-x1);x3=x1;y3=py+(py-y1);y4=py;}
@@ -76,18 +94,26 @@ public class Sand004 extends PApplet {
         if(py>y1){x3=px-(x1-px);x4=px;y4=y1-(py-y1);y3=y1;}
         else{x4=x1+(x1-px);x3=x1;y3=py-(y1-py);y4=py;}
       }
-      //stroke(0,0,100);
-      //fill(50,100,100);
-      //quad(x1, y1, x2, y2, x3, y3, x4, y4);
-      fill(50,0,100);
-      //ellipse(px,py,30,30);
-      fill(i*80,100,100);
-      quad(x1, y1, px, py, x3, y3, x4, y4);
-      ellipse(x1,y1,20,20);
-      ellipse(px,py,20,20);
-      ellipse(x3,y3,20,20);
-      ellipse(x4,y4,20,20);
-    }
+      Float[] silver = {x1, y1, px, py, x3, y3, x4, y4};
+
+      baldessari.add(silver);
+      x1=baldessari.get(0)[2];
+      y1=baldessari.get(0)[3];
+      x2=baldessari.get(0)[4];
+      y2=baldessari.get(0)[5];
+      px = (1-t) * x1 + (t * x2);
+      py = (1-t) * y1 + (t * y2);
+      if(px>x1){
+        if(py<y1){x3=px+(px-x1);x4=px;y4=y1+(y1-py);y3=y1;}
+        else {x4=x1-(px-x1);x3=x1;y3=py+(py-y1);y4=py;}
+      }
+      else{
+        if(py>y1){x3=px-(x1-px);x4=px;y4=y1-(py-y1);y3=y1;}
+        else{x4=x1+(x1-px);x3=x1;y3=py-(y1-py);y4=py;}
+      }
+      Float[] gilded = {x1, y1, px, py, x3, y3, x4, y4};
+      baldessari.add(gilded);
+ 
   }
   
   /* Method receives an array that consists of coordinates (john[0],john[1]), and a width john[2]
