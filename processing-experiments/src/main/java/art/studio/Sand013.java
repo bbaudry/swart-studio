f368c8e424bc2e872358afcb6ba7ff811ea04672
bbaudry <baudry@kth.se>
2022-09-23 06:39:52 +0200

    -}}[><]{{-

--- processing-experiments/src/main/java/art/studio/Sand013.java
+++ processing-experiments/src/main/java/art/studio/Sand013.java
@@ -13,6 +13,9 @@ public class Sand013 extends PApplet {
   float backHu;
   float frontHu;
   Random alea;
+  float interval;
+  float x;
+  float y;
   
   @Override
   public void settings() {
@@ -24,23 +27,41 @@ public class Sand013 extends PApplet {
     alea=new Random();
     colorMode(HSB, 360, 100, 100, 250);
     backHu = 130;
-    fill(310,100,100,10);
+    interval = 20;
+    x = 0;
+    y = 0;
+    fill(310,100,100,200);
     noStroke();
     background(backHu, 100, 100);
   }
 
   @Override
   public void draw() {
-    float x1 = alea.nextFloat()*w/2;
-    float y1 = alea.nextFloat()*h/2;
-    float x2 = w/2+alea.nextFloat()*w/2;
-    float y2 = alea.nextFloat()*h/2;
-    float x3 = alea.nextFloat()*w;
-    float y3 = h/2+alea.nextFloat()*h/2;
-    triangle(x1, y1, x2, y2, x3, y3);
+    if (x<=w - interval){
+      tri();
+      x+=interval;
+    }
+    else{
+      x=0;
+      if (y<=h - interval){
+        y+=interval/3;
+      }
+      else{
+        save("Sand013.png");
+        noLoop();
+      }
+    }
   }
 
-  
+  private void tri(){
+    float x1 = x+alea.nextFloat()*interval/2;
+    float y1 = y+alea.nextFloat()*interval/2;
+    float x2 = x+interval/2+alea.nextFloat()*interval/2;
+    float y2 = y+alea.nextFloat()*interval/2;
+    float x3 = x+alea.nextFloat()*interval;
+    float y3 = y+interval/2+alea.nextFloat()*interval/2;
+    triangle(x1, y1, x2, y2, x3, y3);
+  }
 
   public static void main(String[] args) {
     String[] processingArgs = { "Sand013" };
