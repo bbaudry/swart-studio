57a940275c1e010556d045c6349f9093e3936587
bbaudry <baudry@kth.se>
2022-10-01 23:27:16 +0200

    []+}><{+[]

--- processing-experiments/src/main/java/art/studio/Sand015.java
+++ processing-experiments/src/main/java/art/studio/Sand015.java
@@ -13,8 +13,8 @@ public class Sand015 extends PApplet {
   int w = 1000;// 1920;
   int h = 1000;// 1080;
   Random alea = new Random();
-  float x1=20;float y1=440;
-  float x2=40;float y2=-100;
+  float x1;float y1;
+  float x2;float y2;
   float x3=770;float y3=770; 
   float x4=970; float y4=490;
 
@@ -29,17 +29,28 @@ public class Sand015 extends PApplet {
   public void setup() {
     colorMode(HSB, 360, 100, 100);
     background(0,0,100);
+    x1=random(5,18);
+    y1=random(444,499);
+    x4=random(888,999);
+    y4=y1+random(17,29);
+    x2=x1+random(444,555);//x1+random(37,77);
+    y2=y1-random(188,255);
+    x3=x4-random(333,377);//x4-random(7,17);
+    y3=y4+random(444,777);
+
     noFill();
   }
 
   @Override
   public void draw() {
+    if (frameCount < 1111){
     stroke(0,0,0);
-    if (x2<x1+w/2){x2+=1;}
+    if (x2>x1){x2-=1;}
     if (x3<x4){x3+=1;}
     y2-=1;
     y3+=1;
     wave(x2,y2,x3,y3);
+    }
   }
 
   private void wave (float x2, float y2, float x3, float y3){
