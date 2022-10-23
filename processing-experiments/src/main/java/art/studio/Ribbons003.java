a69f81cb113f95dea9f3e88d1cd5d1c4eaf9b877
bbaudry <baudry@kth.se>
2022-10-24 00:15:12 +0200

    °°.}><{.°°

--- processing-experiments/src/main/java/art/studio/Ribbons003.java
+++ processing-experiments/src/main/java/art/studio/Ribbons003.java
@@ -7,8 +7,8 @@ import java.util.Random;
 
 public class Ribbons003 extends PApplet {
   /* This piece assumes a square canvas */
-  int w = 1000;// 1920;
-  int h = 1000;// 1080;
+  int w = 3000;// 1920;
+  int h = 3000;// 1080;
   Random alea = new Random();
 
   @Override
@@ -19,32 +19,53 @@ public class Ribbons003 extends PApplet {
   @Override
   public void setup() {
     colorMode(HSB, 360, 100, 100);
-    background(0, 0, 100);
+    background(100, 100, 100);
   }
+
+
+  float x1 = 0;
+  float x2 = w;
+  float y1 = h/2;
+  float y2 = h/3;
+  float light = 100;
+  float hu = random(230,330);
+  float xoff = random(200,400);
+  float yoff = random(500,700);
+  boolean up = alea.nextBoolean();
+
+
 
   @Override
   public void draw() {
-    if(frameCount<100){
-      ray();
+    if(frameCount%100<99){
+      ray(hu, xoff, yoff,up);
     }
     else{
+      light=100;
+      y1=random(h);
+      if(alea.nextBoolean()){y2=y1+random(h-y1);}
+      else{y2=y1-random(y1);}
+      hu = random(230,330);
+      xoff = random(200,400);
+      yoff = random(500,700);
+      up = alea.nextBoolean();
+    }
+    if(frameCount==4099){
       save("Ribbons003.png");
       noLoop();
     }
   }
 
-  float x1 = 42;
-  float x2 = w-42;
-  float y1 = h/2;
-  float y2 = h/3;
-  float light = 100;
-
-  private void ray(){
+  private void ray(float hu,float xoff,float yoff, boolean up){
     noFill();
-    stroke(330,light,100);
-    bezier(x1, y1, x1+200, y1-400,x2-200, y2+600, x2, y2);
-    y1--;
-    y2--;
+    strokeWeight(5);
+    stroke(hu,light,100);
+    if(up){
+      bezier(x1, y1, x1+xoff, y1-yoff,x2-xoff, y2+yoff, x2, y2);}
+    else{
+      bezier(x1, y1, x1+xoff, y1+yoff,x2-xoff, y2-yoff, x2, y2);}
+    y1-=5;
+    y2-=5;
     light--;
   }
 
