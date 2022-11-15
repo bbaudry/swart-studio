d37db83954eae22b0c352245b52532ba2c691dd0
bbaudry <baudry@kth.se>
2022-11-15 17:45:38 +0100

    .}-*><*-{.

--- processing-experiments/src/main/java/art/studio/Sand016.java
+++ processing-experiments/src/main/java/art/studio/Sand016.java
@@ -27,7 +27,7 @@ public class Sand016 extends PApplet {
     background(0,0,0);
     grain = 100;
     noFill();
-    frameRate(11);
+    frameRate(5);
   }
 
   @Override
@@ -51,7 +51,7 @@ public class Sand016 extends PApplet {
     x=x-(float)(0.05*grain);
     y=y-(float)(0.05*grain);
     float localGrain = (float)(0.95*grain); 
-    if (alea.nextInt(168)<1){    stroke(230,100,100,250);
+    if (alea.nextInt(168)<1){    stroke(230,100,100,150);
     }
     else {    stroke(0,0,100,50);
     }
@@ -71,12 +71,17 @@ public class Sand016 extends PApplet {
             triangle(x+grain, y, x+localGrain, y+localGrain, x, y+localGrain); 
           }
           else{
-            if (chance<34){
+            if (chance<30){
               circle_hor(x, y);
             }
             else{
-              if(chance<41){
+              if(chance<34){
                 circle_vert(x, y);
+              }
+              else{
+                if(chance<38){
+                  circle_dots(x,y);
+                }
               }
             }
           }
@@ -100,6 +105,27 @@ public class Sand016 extends PApplet {
     ellipse(x+localGrain/2,y+localGrain/2,localGrain,localGrain);
     line(x,y+localGrain/2,x+localGrain,y+localGrain/2);
   }
+
+  private void circle_dots (float x, float y){
+    x=x-(float)(0.05*grain);
+    y=y-(float)(0.05*grain);
+    float localGrain = (float)(0.95*grain); 
+    ellipse(x+localGrain/2,y+localGrain/2,localGrain,localGrain);
+    for (int i = 0; i<10; i++){particle(x+localGrain/2, y+localGrain/2, localGrain, 5);}
+  }
+
+  /*draws a particle at a random location inside a circle centered on (cx,cy) and of radius rad*/
+  private void particle(float cx,float cy,float rad, float size){
+    float t=2*PI*alea.nextFloat();
+    float u=random(rad/4)+random(rad/4);
+    float r;
+    if (u>rad/4){r=2-u;}
+    else {r=u;}
+    float x=cx+r*cos(t);
+    float y=cy+r*sin(t);
+    ellipse(x,y,size,size);
+  }
+
 
   public static void main(String[] args) {
     String[] processingArgs = { "Sand016" };
