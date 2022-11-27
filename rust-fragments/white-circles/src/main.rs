553e14e9493d3582f35e77b43a116b5625c0b7a7
bbaudry <baudry@kth.se>
2022-11-27 23:12:36 +0100

    {*}{><}{*}

--- rust-fragments/white-circles/src/main.rs
+++ rust-fragments/white-circles/src/main.rs
@@ -15,10 +15,18 @@ fn model(_app: &App) -> Model {
     Model {white : true}
 }
 
-fn update(_app: &App, model: &mut Model, _update: Update) {
+fn update(_app: &App, _model: &mut Model, _update: Update) {
     
 }
 
-fn view(app: &App, model: &Model, frame: Frame){
+fn view(_app: &App, _model: &Model, _frame: Frame){
+    let mut n = 42;
+    while n!=0 {
+        draw.rect()
+    .color(WHITE)
+    .w(300.0)
+    .h(200.0);
+        n-=1;
+    }
     
 }
