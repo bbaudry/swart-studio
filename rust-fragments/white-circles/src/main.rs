3bb13a10c67f541f3f3d957a7f6ed7fb05e95a4b
bbaudry <baudry@kth.se>
2022-11-28 21:59:45 +0100

    +}}]><[{{+

--- rust-fragments/white-circles/src/main.rs
+++ rust-fragments/white-circles/src/main.rs
@@ -1,5 +1,5 @@
 use nannou::prelude::*;
-use nannou::rand::*;
+use nannou::rand::random_range;
 
 fn main() {
     nannou::app(model)
@@ -17,22 +17,25 @@ fn model(_app: &App) -> Model {
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {
-    
+ 
 }
 
 fn view(app: &App, _model: &Model, frame: Frame){
     let draw = app.draw();
     draw.background().color(BLACK);
-    let john = rand::random_range::<i32>(-300.0,300.0);
-    let baldessari = rand::random_range::<i32>(-200.0,200.0);
+    let john = random_range(-300.0,300.0);
+    let baldessari = random_range(-200.0,200.0);
+    let vera = random_range(3.0,11.0);
+    let molnar = random_range(2.0,7.0);
 
     let mut n = 42;
     while n!=0 {
         draw.rect()
-        .color(WHITE)
-        .w(john)
-        .h(baldessari);
+        .color(hsl(0.0,0.0,1.0))
+        .x_y(john,baldessari)
+        .w_h(vera,molnar);
         n-=1;
     }
-    
+    draw.to_frame(app,&frame).unwrap();
+
 }
