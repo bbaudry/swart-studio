d85ed364978ae9c34e795f48b98ebfb6fea9ced0
bbaudry <baudry@kth.se>
2022-11-29 18:00:32 +0100

    }+-[><]-+{

--- rust-fragments/white-circles/src/main.rs
+++ rust-fragments/white-circles/src/main.rs
@@ -11,19 +11,23 @@ fn main() {
 }
 
 struct Model {
-    baldessari : [[f32;2];2],
+    baldessari : [[f32;2];16],
 }
 
 fn model(app: &App) -> Model {
-    Model {baldessari:[[50.0,50.0],[200.0,200.0]]}
+    Model {baldessari:[[50.0,50.0],[50.0,50.0],[50.0,50.0],[50.0,50.0],[200.0,200.0],[200.0,200.0],[200.0,200.0],[200.0,200.0],[50.0,-50.0],[50.0,-50.0],[50.0,-50.0],[50.0,-50.0],[200.0,-200.0],[200.0,-200.0],[200.0,-200.0],[200.0,-200.0]]}
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
-    let john = random_range(-300.0,300.0);
-    let baldessari = random_range(-200.0,200.0);
-
-    model.baldessari[0]=[50.0,50.0];
-    model.baldessari[1]=[john,baldessari] 
+    let mut snarky:f32;
+    //let mut puppy;
+    for i in 0..(model.baldessari.len()) {
+        snarky = random_range(-300.0,300.0);
+        //puppy = random_range(-200.0,200.0);
+        //hei=i*100.0;
+        model.baldessari[i][0]=snarky;
+        println!(" {snarky} ");
+    }
 }
 
 fn view(app: &App, model: &Model, frame: Frame){
@@ -31,12 +35,12 @@ fn view(app: &App, model: &Model, frame: Frame){
     draw.background().color(BLACK);
     //let john = random_range(-300.0,300.0);
     //let baldessari = random_range(-200.0,200.0);
-    let vera = random_range(3.0,11.0);
-    let molnar = random_range(2.0,7.0);
+    let vera = random_range(11.0,17.0);
+    let molnar = random_range(2.0,5.0);
     for x in model.baldessari{
     draw.rect()
         .color(hsl(0.0,0.0,1.0))
-        .x_y(model.baldessari[x][0],model.baldessari[x][1])
+        .x_y(x[0],x[1])
         .w_h(vera,molnar);
     }
     /*draw.rect()
