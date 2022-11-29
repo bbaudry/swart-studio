e4ebf738b7edbfb81e18fbd7b2a5a6cc925c8908
bbaudry <baudry@kth.se>
2022-11-29 08:41:22 +0100

    ]°{-><-}°[

--- rust-fragments/white-circles/src/main.rs
+++ rust-fragments/white-circles/src/main.rs
@@ -1,3 +1,5 @@
+use std::array;
+
 use nannou::prelude::*;
 use nannou::rand::random_range;
 
@@ -9,33 +11,37 @@ fn main() {
 }
 
 struct Model {
-    white : bool,
+    baldessari : [[f32;2];2],
 }
 
-fn model(_app: &App) -> Model {
-    Model {white : true}
+fn model(app: &App) -> Model {
+    Model {baldessari:[[50.0,50.0],[200.0,200.0]]}
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {
- 
-}
-
-fn view(app: &App, _model: &Model, frame: Frame){
-    let draw = app.draw();
-    draw.background().color(BLACK);
+fn update(_app: &App, model: &mut Model, _update: Update) {
     let john = random_range(-300.0,300.0);
     let baldessari = random_range(-200.0,200.0);
+
+    model.baldessari[0]=[50.0,50.0];
+    model.baldessari[1]=[john,baldessari] 
+}
+
+fn view(app: &App, model: &Model, frame: Frame){
+    let draw = app.draw();
+    draw.background().color(BLACK);
+    //let john = random_range(-300.0,300.0);
+    //let baldessari = random_range(-200.0,200.0);
     let vera = random_range(3.0,11.0);
     let molnar = random_range(2.0,7.0);
 
-    //let mut n = 42;
-    for n in 1..42 {
-        draw.rect()
+    draw.rect()
         .color(hsl(0.0,0.0,1.0))
-        .x_y(john,baldessari)
+        .x_y(model.baldessari[0][0],model.baldessari[0][1])
         .w_h(vera,molnar);
-        //n-=1;
-    }
-    draw.to_frame(app,&frame).unwrap();
+    draw.rect()
+        .color(hsl(0.5,1.0,1.0))
+        .x_y(model.baldessari[1][0],model.baldessari[1][1])
+        .w_h(vera,molnar);
 
+    draw.to_frame(app,&frame).unwrap();
 }
