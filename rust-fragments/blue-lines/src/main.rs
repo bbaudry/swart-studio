0d74bf9fd345e27b87fadf014cf2f1f5bb0d49bc
bbaudry <baudry@kth.se>
2022-12-14 08:08:54 +0100

    {.*]><[*.}

--- rust-fragments/blue-lines/src/main.rs
+++ rust-fragments/blue-lines/src/main.rs
@@ -15,8 +15,7 @@ fn main() {
 }
 
 struct Model {
-    occam: Vec<f32>,
-    wasa:f32
+    occam: Vec<f32>
 }
 
 fn model(app: &App) -> Model {
@@ -55,18 +54,22 @@ fn view(app: &App, model: &Model, frame: Frame){
     draw.background().color(BLACK);
     let h = app.window_rect().h();
     let w = app.window_rect().w();
-    let nb = model.occam.len() as f32;
+    let nb = model.occam.len();
     let off = h/(nb+7.0);
-    for hornstull in &model.occam{
+    let mut rex:i32 = 0; //index to iterate on occam
+    while rex < nb{
         let mut x = -w/2.0 +random_range(-77.0,77.0);
+        let y1 = model.occam.get(rex);
+        let y2 = model.occam.get(rex+1);
         while x<w/2.0{
             draw.rect()
             .color(hsla(0.0,1.0,1.0,random_range(0.3,0.9)))//360.0/x.abs()
-            .x_y(x,*hornstull)
-            .w_h(111.0,off);
+            .x_y(x,y1)
+            .w_h(111.0,y2-y1);
             let will = x + 111.0 +random_range(-77.0,7.0);
             x = will;
         }
+        rex += 2;
     }
     draw.to_frame(app,&frame).unwrap();
 }
