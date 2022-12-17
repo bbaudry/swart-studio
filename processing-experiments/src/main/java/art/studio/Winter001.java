f2f5060614b965cfda8e3cd52d59c00128449d78
bbaudry <baudry@kth.se>
2022-12-17 12:41:49 +0100

    +}{°><°}{+

--- processing-experiments/src/main/java/art/studio/Winter001.java
+++ processing-experiments/src/main/java/art/studio/Winter001.java
@@ -1,26 +1,15 @@
-/* Metadata {"endless":true, "BW": false, "knobs": false, "data": false, "interaction": false}*/
+/* Metadata {"endless":false, "BW": true, "knobs": false, "data": false, "interaction": false}*/
 
 package art.studio;
 
 import processing.core.PApplet;
 import java.util.Random;
-import java.util.ArrayList;
 
 public class Winter001 extends PApplet {
-  int w = 1800;
+  int w = 1000;
   int h = 1000;
-  int saturday;// number of columns
-  int night;// number of rows
-  int vera, molnar; // paddings on the x and y axes
-  Random alea;
-  ArrayList<Float> pads;
-  float grain;
-  int wave; // wave + peace + eyeball is the duration of a cycle
-  int peace;
-  int eyeball;
-  int tide;
-  int c;
-  float xray;
+  Random alea = new Random();
+  int tracks;
 
   @Override
   public void settings() {
@@ -29,12 +18,30 @@ public class Winter001 extends PApplet {
 
   @Override
   public void setup() {
-
+    colorMode(HSB,360,100,100);
+    background(0,0,100);
+    tracks = alea.nextInt(17)+7;
   }
 
   @Override
   public void draw() {
+    noStroke();
+    fill(230,20,70,50);
+    rect(0,0,w,h/3);
+    for (int i = 0; i<tracks; i++){
+      track();
+    }
+    noLoop();
+  }
 
+  private void track(){
+    float that, is, a, ray;
+    that = w/2-40+alea.nextInt(80);
+    is = h/3;
+    a = alea.nextInt(w);
+    ray = h-alea.nextInt(h/10);
+    stroke(230,70,70,50);
+    line(that, is, a, ray);
   }
 
   public static void main(String[] args) {
