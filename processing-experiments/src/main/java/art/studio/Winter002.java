82ee3f037bd5dc9eabc3f324e28a286a44f61b72
bbaudry <baudry@kth.se>
2022-12-18 18:01:59 +0100

    °]+-><-+[°

--- processing-experiments/src/main/java/art/studio/Winter002.java
+++ processing-experiments/src/main/java/art/studio/Winter002.java
@@ -3,6 +3,8 @@
 package art.studio;
 
 import processing.core.PApplet;
+
+import java.security.Provider;
 import java.util.Random;
 
 public class Winter002 extends PApplet {
@@ -20,60 +22,56 @@ public class Winter002 extends PApplet {
   public void setup() {
     colorMode(HSB,360,100,100);
     background(0,0,100);
-    tracks = alea.nextInt(17)+13;
-    for (int i = 0; i<tracks; i++){
-      track();
-    }
+    tracks=alea.nextInt(122)+42;
+    poll();
+    road();
 
   }
 
   @Override
   public void draw() {
-    //flake();
-    if (frameCount==111){
+    if (frameCount<tracks){
+      sky();
+    }
+    else{
+      save("Winter002.png");
       noLoop();
-      save("Snow001.png");
     }
   }
 
-  private void track(){
-    float that, is, a, ray, alp;
-    that = alea.nextInt(w);// w/2-80+alea.nextInt(160);
-    is = 0;//h/4;
-    a = 2*w-alea.nextInt(3*w);
-    ray = h;//-alea.nextInt(h/10);
-    alp=80;
-    int o =4;
-    boolean right = alea.nextBoolean();
-    for (int i=0; i<alp/2; i++){
-      stroke(230,70,70,alp);
-      line(that, is, a, ray);
-      alp-=alea.nextInt(3);
-      if(right){that+=o; a+=o;}
-      else{that-=o; a-=o;}
-    } 
-  }
-
-  private void flake(){
-    noFill();
-    float moonchild, starfruit;
-    moonchild = alea.nextFloat()*w;
-    starfruit = alea.nextFloat()*h/2;
-    int alp=50;
-    float folks;
-    boolean grow = alea.nextBoolean();
-    if (grow) {folks=alea.nextFloat();}
-    else {folks=alp/2*alea.nextFloat();}
-    float hu = 250-alea.nextInt(40);
-    for (int i=0; i<alp/2; i++){
-      stroke(hu,70,70,alp);
+  private void sky(){
+    int john = h/5+alea.nextInt(h/4);
+    int baldessari = h/5+alea.nextInt(h/4);
+    int alp = 30;
+    for (int i=0; i<30; i++){
+      stroke(50,40,70,alp);
+      line(0,john,w,baldessari);
       alp-=2;
-      ellipse(moonchild, starfruit, folks, folks);
-      if(grow){folks++;}
-      else{folks--;}
-    } 
+      john++;
+      baldessari++;
+    }
   }
 
+  private void poll(){
+    float f = alea.nextInt(w);
+    float g = h/2+alea.nextInt(h/7);
+    stroke(0,0,0,100);
+    line(f, g, f, g-78);
+    stroke(0,0,50,70);
+    line(f, g, f+100, g-28);
+  }
+
+  private void road(){
+    float g = h/2+alea.nextInt(h/2);
+    float f = h/2+alea.nextInt(h/2);
+    stroke(0,0,50,50);
+    int west=37;
+    float far=7/west;
+    for(int i=0;i<west;i++){
+      line(0, g, w, f);
+      line(0, g+i, w, f+far);
+    }
+  }
 
   public static void main(String[] args) {
     String[] processingArgs = { "Winter002" };
