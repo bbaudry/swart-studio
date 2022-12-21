95cf68a20761916b21193963a1796c08db9754ad
bbaudry <baudry@kth.se>
2022-12-21 17:05:06 +0100

    -[}{><}{]-

---
+++ gen-ascii.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#this script generates a random, one-liner ascii art string and uses it as commit message
+#in commit-bra, bra stands for bash random ascii (art)
+
+#generate random string of nice ascii characters
+chars=°.[]-{+}*
+for ((i=0;i<4;i++)); do 
+	rand_str="${chars:RANDOM%${#chars}:1}"$rand_str
+done
+
+#get the string with characters in reverse order
+copy=$rand_str
+len=${#copy}
+for ((i=$len-1;i>=0;i--)); do 
+	if [ "${copy:$i:1}" = '[' ]; then rev_str="$rev_str]"; 
+	elif [ "${copy:$i:1}" = ']' ]; then rev_str="$rev_str["; 
+	elif [ "${copy:$i:1}" = '{' ]; then rev_str="$rev_str}"; 
+	elif [ "${copy:$i:1}" = '}' ]; then rev_str="$rev_str{"; 
+	else
+	rev_str="$rev_str${copy:$i:1}";
+	fi
+done
+
+#compose the final ascii art string
+ascii_art="$rand_str><$rev_str"
+echo $ascii_art
+#pass the string as commit message
+commit_output=`git commit -am "$ascii_art"`
+echo $commit_output
