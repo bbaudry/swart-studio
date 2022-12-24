a5b2ada3d8ad50357c09b9c1c13d86c28f492af1
bbaudry <baudry@kth.se>
2022-12-24 16:20:21 +0100

    *.}{><}{.*

--- rust-fragments/torg001/src/main.rs
+++ rust-fragments/torg001/src/main.rs
@@ -1,6 +1,7 @@
 use std::array;
 
 use nannou::color::Alpha;
+use nannou::color::GetHue;
 use nannou::draw::mesh::vertex::Color;
 use nannou::geom::Range;
 use nannou::geom::Rect;
@@ -18,22 +19,19 @@ fn main() {
 }
 
 struct Cell {
-    beam:Rect,
+    beam: Rect,
     speed: i32,
-    c:Hsl
+    c: Hsla,
 }
 
 struct Model {
-    occam: Vec<Cell>
+    occam: Vec<Cell>,
 }
 
 fn model(app: &App) -> Model {
-    app.new_window()
-    .size(1000,1000)
-    .build()
-    .unwrap();
-    Model{
-            occam:playground(app)
+    app.new_window().size(1000, 1000).build().unwrap();
+    Model {
+        occam: playground(app),
     }
 }
 
@@ -41,54 +39,64 @@ fn playground(app: &App) -> Vec<Cell> {
     let h = app.window_rect().h();
     let w = app.window_rect().w();
     let mut play = Vec::new();
-    let mut b= Rect{
-        x:Range{start:w/2.0-100.0,end:w/2.0},
-        y:Range{start:h/2.0,end:h/2.0-50.0}
-    };
-    let mut col: Hsl = Hsl::new(0.0,1.0,1.0);
-    play.push(Cell{beam:b,speed:15,c:col});
-    
-    b= Rect{
-        x:Range{start:w/2.0-200.0,end:w/2.0-120.0},
-        y:Range{start:h/2.0-55.0,end:h/2.0-80.0}
-    };
-    col = Hsl::new(0.0,1.0,1.0);
-    play.push(
-        Cell{
-        beam:Rect{
-            x:Range{start:w/2.0-200.0,end:w/2.0-120.0},
-            y:Range{start:h/2.0-55.0,end:h/2.0-80.0}
-        },
-        speed:12,
-        c:Hsl::new(50.0,1.0,1.0)
+    let mut dsb = h / 2.0;
+    while dsb > -h / 2.0 {
+        let off_y = random_range(11.0, 27.0);
+        let velo = random_range(311, 391);
+        let mut sj = 100.0 * w;
+        while sj > 0.0 {
+            let off_x = random_range(77.0, 111.0);
+            let cuba = random_range(0.0,1.0);
+            let zombie;
+            if cuba==0.0 {zombie=0.5;}
+            else {zombie=1.0;}
+            play.push(Cell {
+                beam: Rect {
+                    x: Range {
+                        start: sj - off_x,
+                        end: sj,
+                    },
+                    y: Range {
+                        start: dsb,
+                        end: dsb - off_y,
+                    },
+                },
+                speed: velo,
+                c: Hsla::new(
+                    230.0 / 360.0,
+                    0.5,
+                    zombie,
+                    1.0,//random_range(0.2, 0.5),
+                ),
+            });
+            sj = sj - off_x - random_range(1.0, 11.0);
         }
-    );
+    dsb = dsb - off_y - random_range(7.0, 11.0)
+    }
     return play;
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
-    for baldessari in &mut model.occam{
-        baldessari.beam.x.start-=baldessari.speed as f32;
-        baldessari.beam.x.end-=baldessari.speed as f32;
-        //baldessari.beam.y.start-=baldessari.speed as f32;
-        //baldessari.beam.y.end-=baldessari.speed as f32;
+    for baldessari in &mut model.occam {
+        baldessari.beam.x.start -= baldessari.speed as f32;
+        baldessari.beam.x.end -= baldessari.speed as f32;
     }
 }
 
-fn view(app: &App, model: &Model, frame: Frame){
+fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(BLACK);
     let h = app.window_rect().h();
     let w = app.window_rect().w();
-    for baldessari in &model.occam{
-        let r=baldessari.beam;
-        draw.rect()
-        .x_y(r.x(), r.y())
-        .w_h(r.w(), r.h())
-        .color(hsl(0.0,0.5,1.0));
+    for baldessari in &model.occam {
+        let r = baldessari.beam;
+        draw.rect().x_y(r.x(), r.y()).w_h(r.w(), r.h()).color(hsla(
+            baldessari.c.hue.to_degrees(),
+            baldessari.c.saturation,
+            baldessari.c.lightness,
+            baldessari.c.alpha,
+        ));
     }
 
-
-
-    draw.to_frame(app,&frame).unwrap();
+    draw.to_frame(app, &frame).unwrap();
 }
