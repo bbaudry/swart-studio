d89b005a1a280dd3c9e1a8952eabc276ce66317d
bbaudry <baudry@kth.se>
2023-03-14 11:47:34 +0100

    --*.><.*--

--- processing-experiments/src/main/java/art/studio/Winter003.java
+++ processing-experiments/src/main/java/art/studio/Winter003.java
@@ -12,7 +12,6 @@ public class Winter003 extends PApplet {
   int h = 1000;
   Random alea = new Random();
 
-
   @Override
   public void settings() {
     size(w, h);
@@ -20,44 +19,45 @@ public class Winter003 extends PApplet {
 
   @Override
   public void setup() {
-    colorMode(HSB,360,100,100);
+    colorMode(HSB, 360, 100, 100);
 
   }
 
   @Override
   public void draw() {
-    background(210,90,90);
+    background(210, 90, 90);
 
     oneIceLayer();
     oneIceLayer();
     save("Winter003.png");
     noLoop();
   }
-private void oneIceLayer(){
-  int step =168;
-  int off = 51;
-  float i,j;
-      noStroke();
-  fill(0, 0, 100, 151);
-  float vera,molnar;
-  i=0;j=0;
-  vera= random(step/10, step);
-  molnar=random(step/10, step);
-  
-  while (i<w){
-    while(j<h){
-      float al = random(111,199);
-      fill(0, 0, 100, al);
-       vera= random(off, step);
-       molnar=random(off, step);
-      quad(i,j,i+molnar+random(-off,off),j+random(-off,off),i+molnar+random(-off,off),j+vera+random(-off,off),i+random(-off,off),j+vera+random(-off,off));
-      j+=vera+random(-off,off);
-    }
-    i+=molnar+random(-off,off);
-    j=random(-off,off);
-  }
-}
 
+  private void oneIceLayer() {
+    noStroke();
+    float i = 0; 
+    float j = 0;
+
+    int step = 168; // basic value for width and height of eache quad
+    int off = 71; // randomly pick an offset in the [-off,off] range; inv: off<step
+    float vera = random(step / 10, step); // width
+    float molnar = random(step / 10, step); // height
+    while (i < w) {
+      while (j < h) {
+        float al = random(111, 199);
+        fill(0, 0, 100, al);
+        vera = random(step / 10, step); 
+        molnar = random(step / 10, step); 
+        quad(i,j, 
+            i + molnar + random(-off, off), j + random(-off, off), 
+            i + molnar + random(-off, off), j + vera + random(-off, off), 
+            i + random(-off, off), j + vera + random(-off, off));
+        j += vera + random(-off, off);
+      }
+      i += molnar + random(-off, off);
+      j = random(-off, off);
+    }
+  }
 
   public static void main(String[] args) {
     String[] processingArgs = { "Winter003" };
