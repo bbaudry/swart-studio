955ea375fcabaf8f95864028817ec383f6344fd8
bbaudry <baudry@kth.se>
2023-04-25 14:51:50 +0200

    .**{><}**.

--- rust-fragments/supply-chain-in-chrome/src/main.rs
+++ rust-fragments/supply-chain-in-chrome/src/main.rs
@@ -1,88 +1,22 @@
-use std::error::Error;
-use std::sync::Arc;
+//ws://localhost:9222/devtools/page/24C488F0B6682D43EA71B6911DEE77C1
+use tungstenite::*;
+use url::*;
 
-use headless_chrome::Browser;
-use headless_chrome::protocol::cdp::Page;
+pub fn main(){
 
-pub struct NetworkInterceptor;
+    let (mut socket, response) =
+        connect(Url::parse("ws://localhost:9222/devtools/page/24C488F0B6682D43EA71B6911DEE77C1").unwrap()).expect("Can't connect");
 
-impl headless_chrome::browser::tab::RequestInterceptor for NetworkInterceptor
-{
-    fn intercept(
-        &self,
-        transport: std::sync::Arc<headless_chrome::browser::transport::Transport>,
-        session_id: headless_chrome::browser::transport::SessionId,
-        event: headless_chrome::protocol::cdp::Fetch::events::RequestPausedEvent,
-    ) -> headless_chrome::browser::tab::RequestPausedDecision {
-        
-        println!("{:?}", event);
-
-        return headless_chrome::browser::tab::RequestPausedDecision::Continue(None)
+    println!("Connected to the server");
+    println!("Response HTTP code: {}", response.status());
+    println!("Response contains the following headers:");
+    for (ref header, _value) in response.headers() {
+        println!("* {}", header);
     }
-}
 
-fn browse_wikipedia() -> Result<(), Box<dyn Error>> {
-    let mut browser = Browser::new(
-        headless_chrome::LaunchOptions { 
-            headless: false, 
-            ..Default::default()}
-    )?;
-    
-
-    let tab = browser.new_tab()?;
-
-    /// Navigate to a site
-    tab.navigate_to("https://www.kth.se")?;
-    tab.wait_until_navigated()?;
-
-    tab.enable_request_interception(Arc::new(NetworkInterceptor)).unwrap();
-
-
-    
-    std::thread::sleep(std::time::Duration::from_secs(300));
-    /*
-    /// Wait for network/javascript/dom to make the search-box available
-    /// and click it.
-    tab.wait_for_element("input#searchInput")?.click()?;
-
-    /// Type in a query and press `Enter`
-    tab.type_str("WebKit")?.press_key("Enter")?;
-
-    /// We should end up on the WebKit-page once navigated
-    let elem = tab.wait_for_element("#firstHeading")?;
-    assert!(tab.get_url().ends_with("WebKit"));
-
-    /// Take a screenshot of the entire browser window
-    let _jpeg_data = tab.capture_screenshot(
-        Page::CaptureScreenshotFormatOption::Jpeg,
-        None,
-        None,
-        true)?;
-
-    /// Take a screenshot of just the WebKit-Infobox
-    let _png_data = tab
-        .wait_for_element("#mw-content-text > div > table.infobox.vevent")?
-        .capture_screenshot(Page::CaptureScreenshotFormatOption::Png)?;
-
-    // Run JavaScript in the page
-    let remote_object = elem.call_js_fn(r#"
-        function getIdTwice () {
-            // `this` is always the element that you called `call_js_fn` on
-            const id = this.id;
-            return id + id;
-        }
-    "#, vec![], false)?;
-    match remote_object.value {
-        Some(returned_string) => {
-            dbg!(&returned_string);
-            assert_eq!(returned_string, "firstHeadingfirstHeading".to_string());
-        }
-        _ => unreachable!()
-    };*/
-
-    Ok(())
-}
-
-pub fn main() {
-    browse_wikipedia().unwrap();
+    socket.write_message(Message::Text("Hello WebSocket".into())).unwrap();
+    loop {
+        let msg = socket.read_message().expect("Error reading message");
+        println!("Received: {}", msg);
+    }
 }
