5447c489fc9bf7d9266ea8970a2b1e59289e6806
bbaudry <baudry@kth.se>
2023-04-28 11:21:32 +0200

    .{+-><-+}.

--- credits/credits.py
+++ credits/credits.py
@@ -3,10 +3,11 @@ import os
 import sys
 
 crewfile = open(sys.argv[1],'r')
+asciiart = ":::::"
 
 for line in crewfile:
     l = line.strip()
     x=l.rpartition(",")
-    print(x[0].rjust(42)+"::::"+x[2].ljust(42))
+    print(x[0].rjust(42)+asciiart+x[2].ljust(42))
 
 
