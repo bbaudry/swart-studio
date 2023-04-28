927d77664f65c5fbd7b0dbc51ed3d33c14bef040
bbaudry <baudry@kth.se>
2023-04-28 12:01:06 +0200

    °°.°><°.°°

--- credits/credits.py
+++ credits/credits.py
@@ -1,13 +1,20 @@
 import glob
 import os
 import sys
+import random
 
 crewfile = open(sys.argv[1],'r')
-asciiart = ":::::"
+asciiart =  [':',':',':',':',':']
+asciichars = ['°','+','-','_','*','.']
+
+def mutateart():
+    i = random.randint(0,len(asciiart)-1)
+    j = random.randint(0,len(asciichars)-1)
+    asciiart[i] = asciichars[j]
+    ''.join(asciiart)
 
 for line in crewfile:
     l = line.strip()
     x=l.rpartition(",")
-    print(x[0].rjust(42)+asciiart+x[2].ljust(42))
-
-
+    print(x[0].rjust(42)+''.join(asciiart)+x[2].ljust(42))
+    mutateart()
