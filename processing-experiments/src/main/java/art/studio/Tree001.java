174dc16917d3a57ae4b00d8afc4160b3fb7a3fd6
bbaudry <baudry@kth.se>
2023-05-03 22:41:11 +0200

    -.--><--.-

--- processing-experiments/src/main/java/art/studio/Tree001.java
+++ processing-experiments/src/main/java/art/studio/Tree001.java
@@ -5,7 +5,7 @@ import processing.core.PApplet;
 import java.util.Random;
 
 public class Tree001 extends PApplet {
-    int w = 2000;
+    int w = 1800;
     int h = 1000;
     Random alea;
     @Override
@@ -23,19 +23,21 @@ public class Tree001 extends PApplet {
 
     @Override
     public void draw() {
-        branch(1,84,w/2,h);
-noLoop();
+        if(frameCount<6){
+            float rx = random((float)0.4*w, (float)0.6*w);
+            branch(1,84,rx,h);}
+//noLoop();
     }
 
     private void branch(int depth, float length, float x, float y){
         if (depth<11){
-        stroke(0,0,100,100);
+        stroke(0,0,100,80);
         float y2 = y-length;
         float x2 = x+random(-142, 142);
         line(x,y,x2,y2);
-        int spin = 1 + alea.nextInt(4);
+        int spin = alea.nextInt(3)+1;
         for (int i = 0; i<spin;i++){
-            branch(depth+1,length+random(-17,7),x2,y2);
+            branch(depth+1,length+random(-17,9),x2,y2);
         }
         }
     }
