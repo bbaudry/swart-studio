bc72049da592c17ba3511fb0dc24e40feaef928d
bbaudry <baudry@kth.se>
2023-06-03 15:26:56 +0200

    [--*><*--]

--- processing-experiments/src/main/java/art/studio/Tshirt003.java
+++ processing-experiments/src/main/java/art/studio/Tshirt003.java
@@ -4,6 +4,7 @@ package art.studio;
 
 import processing.core.PApplet;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Tshirt003 extends PApplet {
     int ratio = 1;
@@ -11,6 +12,7 @@ public class Tshirt003 extends PApplet {
     int h = 1000*ratio;
     int resolution = 4;
     ArrayList<Integer> hues;
+    Random alea;
 
     @Override
     public void settings() {
@@ -25,6 +27,7 @@ public class Tshirt003 extends PApplet {
         stroke(0,0,100);
         hues = new ArrayList<>();
         hues.add(50); hues.add(330);hues.add(140);hues.add(230);
+        alea = new Random();
     }
 
     @Override
@@ -32,19 +35,19 @@ public class Tshirt003 extends PApplet {
         noStroke();
         float diam = (float)0.5*w;
         float x,y;
-        fill(hues.get(0),100,100);
+        fill(hues.get(alea.nextInt(hues.size())),100,100);
         x=(float)0.25*w;
         y=(float)0.25*h;
         ellipse(x,y,diam,diam);
-        fill(hues.get(1),100,100);
+        fill(hues.get(alea.nextInt(hues.size())),100,100);
         x=(float)0.75*w;
         y=(float)0.25*h;
         ellipse(x,y,diam,diam);
-        fill(hues.get(2),100,100);
+        fill(hues.get(alea.nextInt(hues.size())),100,100);
         x=(float)0.25*w;
         y=(float)0.75*h;
         ellipse(x,y,diam,diam);
-        fill(hues.get(3),100,100);
+        fill(hues.get(alea.nextInt(hues.size())),100,100);
         x=(float)0.75*w;
         y=(float)0.75*h;
         ellipse(x,y,diam,diam);
