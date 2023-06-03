cb8551a5a8bfdf644b18b1ab5bfd4468e8c4822d
bbaudry <baudry@kth.se>
2023-06-03 15:24:28 +0200

    *{-.><.-}*

---
+++ processing-experiments/src/main/java/art/studio/Tshirt003.java
@@ -0,0 +1,60 @@
+/* Metadata {"endless":false, "BW": false, "knobs": "false", "data": false, "interaction": false}*/
+package art.studio;
+
+
+import processing.core.PApplet;
+import java.util.ArrayList;
+
+public class Tshirt003 extends PApplet {
+    int ratio = 1;
+    int w = 1000*ratio;
+    int h = 1000*ratio;
+    int resolution = 4;
+    ArrayList<Integer> hues;
+
+    @Override
+    public void settings() {
+        size(w, h);
+    }
+
+    @Override
+    public void setup() {
+        colorMode(HSB,360,100,100,250);
+        background(0,0,100);   
+        noFill();
+        stroke(0,0,100);
+        hues = new ArrayList<>();
+        hues.add(50); hues.add(330);hues.add(140);hues.add(230);
+    }
+
+    @Override
+    public void draw() {
+        noStroke();
+        float diam = (float)0.5*w;
+        float x,y;
+        fill(hues.get(0),100,100);
+        x=(float)0.25*w;
+        y=(float)0.25*h;
+        ellipse(x,y,diam,diam);
+        fill(hues.get(1),100,100);
+        x=(float)0.75*w;
+        y=(float)0.25*h;
+        ellipse(x,y,diam,diam);
+        fill(hues.get(2),100,100);
+        x=(float)0.25*w;
+        y=(float)0.75*h;
+        ellipse(x,y,diam,diam);
+        fill(hues.get(3),100,100);
+        x=(float)0.75*w;
+        y=(float)0.75*h;
+        ellipse(x,y,diam,diam);
+
+    }
+
+    
+    public static void main(String[] args) {
+        String[] processingArgs = { "Tshirt003" };
+        Tshirt003 mySketch = new Tshirt003();
+        PApplet.runSketch(processingArgs, mySketch);
+    }
+}
