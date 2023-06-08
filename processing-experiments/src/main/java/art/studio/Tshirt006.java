29137219ccb32948a0b8166b3ee425ddac779fce
bbaudry <baudry@kth.se>
2023-06-08 21:11:49 +0200

    *]{{><}}[*

--- processing-experiments/src/main/java/art/studio/Tshirt006.java
+++ processing-experiments/src/main/java/art/studio/Tshirt006.java
@@ -9,6 +9,8 @@ public class Tshirt006 extends PApplet {
     int w = 1000*ratio;
     int h = 1000*ratio;
     Random alea;
+    float x1,y1,x2,y2,x3,y3;
+    float min, max;
 
     @Override
     public void settings() {
@@ -22,17 +24,32 @@ public class Tshirt006 extends PApplet {
         noFill();
         stroke(0,0,100);
         alea = new Random();
+        x1=(float)(0.1*w);
+        x2=(float)(0.5*w);
+        x3=(float)(0.9*w);
+        y1=(float)(0.8*h);
+        y2=(float)(0.2*h);
+        y3=(float)(0.8*h);
+        min=-7;max=7;
     }
 
     @Override
     public void draw() {
-        float x1=random(w/3);
-        float x2=x1+random(w/3);
-        float x3=x2+random(w/3);
-        float y1=h-random(h/7);
-        float y2=random(h/7);
-        float y3=h-random(h/7);
+        if(alea.nextInt(168)<2){stroke(330,100,100);}
+        else{stroke(0,0,100);}
+        if(frameCount<168){
         triangle(x1,y1,x2,y2,x3,y3);
+        x1+=random(min,max);
+        x2+=random(min,max);
+        x3+=random(min,max);
+        y1+=random(min,max);
+        y2+=random(min,max);
+        y3+=random(min,max);
+        }
+        else{
+            noLoop();;
+            save("tshirt006.png");
+        }
     }
 
     public static void main(String[] args) {
