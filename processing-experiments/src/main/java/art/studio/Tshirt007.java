8c0f682dae21ec883b2f7d3e6994342652d580a7
bbaudry <baudry@kth.se>
2023-06-08 21:22:17 +0200

    *-.-><-.-*

--- processing-experiments/src/main/java/art/studio/Tshirt007.java
+++ processing-experiments/src/main/java/art/studio/Tshirt007.java
@@ -18,12 +18,24 @@ public class Tshirt007 extends PApplet {
     @Override
     public void setup() {
         colorMode(HSB,360,100,100,250);
-        background(0,0,0);   
+        background(0,0,0); 
+        alea=new Random();
+        noStroke(); 
     }
 
     @Override
     public void draw() {
-
+        if(frameCount<84){
+        if(alea.nextInt(3)<1){fill(0,0,0);}
+        else{fill(0,0,100);}
+        float cx=alea.nextFloat()*w;
+        float cy=alea.nextFloat()*h;
+        ellipse(cx,cy,alea.nextFloat()*w,alea.nextFloat()*h);
+    }
+        else{
+            noLoop();
+            save("tshirt007.png");
+        }
     }
 
     public static void main(String[] args) {
