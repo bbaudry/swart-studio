209fc4fadbab4c7ac261b248914c656fde5f4284
bbaudry <baudry@kth.se>
2023-06-09 16:13:29 +0200

    }[}]><[{]{

--- processing-experiments/src/main/java/art/studio/Tshirt008.java
+++ processing-experiments/src/main/java/art/studio/Tshirt008.java
@@ -11,8 +11,7 @@ public class Tshirt008 extends PApplet {
     int h = 1000*ratio;
     Random alea;
     ArrayList<Float> wid;
-    float x1,x2;
-    float cx;
+    float cx,cy;
 
     @Override
     public void settings() {
@@ -24,30 +23,44 @@ public class Tshirt008 extends PApplet {
         colorMode(HSB,360,100,100,250);
         background(0,0,0); 
         alea=new Random();
-        fill(0,0,100);stroke(0,0,0);
+        fill(0,0,100);noStroke();// stroke(0,0,0);
         wid = new ArrayList<>();
-        cx=w/2;
-        initW();
-    }
-
-    private void initW(){
-
+        cx=w/2;cy=0;
+        frameRate(1);
     }
 
     @Override
     public void draw() {
-        background(0,0,0);
+        row(h/2,7+alea.nextInt(7));
+        noLoop();
+    }
+
+    private void row(float y, float grain){
+        float x1,x2;
         x1=0;x2=1;
         while(cx+x2<w){
-            rect(cx+x1,h/2-4,x2-x1,8);
+            int t = alea.nextInt(42);
+            if (t<1){stroke(0,0,100);fill(330,100,100);}
+            else{
+                if (t<2){stroke(0,0,0);fill(180,100,100);}
+                else{stroke(0,0,0);fill(330,0,100);}
+            }
+            rect(cx+x1,y-grain/2,x2-x1,grain);
             x1=x2;
             x2+=0.3*x2+random(-x2,x2);
         }
         x1=0;x2=-1;
         while(cx+x2>0){
-            rect(cx+x1,h/2-4,x2-x1,8);
+
+            int t = alea.nextInt(42);
+            if (t<1){stroke(0,0,100);fill(330,100,100);}
+            else{
+                if (t<2){stroke(0,0,0);fill(180,100,100);}
+                else{stroke(0,0,0);fill(330,0,100,100);}
+            }
+            rect(cx+x1,y-grain/2,x2-x1,grain);
             x1=x2;
-            x2+=0.3*x2+random(x2,-x2);
+            x2+=0.2*x2-random(x2,-x2);
         }
     }
 
