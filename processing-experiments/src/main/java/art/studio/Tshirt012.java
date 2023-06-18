631db5f6d6c5f470d5485a580effa05261debc65
bbaudry <baudry@kth.se>
2023-06-18 18:47:31 +0200

    }]°.><.°[{

--- processing-experiments/src/main/java/art/studio/Tshirt012.java
+++ processing-experiments/src/main/java/art/studio/Tshirt012.java
@@ -19,45 +19,52 @@ public class Tshirt012 extends PApplet {
     public void setup() {
         colorMode(HSB, 360, 100, 100, 250);
         background(0, 0, 0);
-        stroke(0,0,100); fill(0,0,100,7);// noFill();
+        stroke(0, 0, 100);
+        fill(0, 0, 100, 7);// noFill();
         alea = new Random();
     }
 
     @Override
     public void draw() {
-        if(frameCount<199){
+        if (frameCount < 42) {
             ray();
-        }
-        else{
+        } else {
             noLoop();
             save("tshirt12.png");
         }
     }
 
-    private void ray(){
-        stroke(0,0,100);
-        fill(0,0,100,42);
-        float cx=(float)(0.84*w)+alea.nextFloat()*11;
-        float cy=(float)(0.21*w)+alea.nextFloat()*11;
-        float angle1 = alea.nextFloat()*(2*PI);
-        float angle2 = angle1+alea.nextFloat()*PI/10;
-        float l = alea.nextFloat()*h;
-        float dx1=cx+l*cos(angle1);
-        float dy1=cy+l*sin(angle1);
-        float dx2=cx+l*cos(angle2);
-        float dy2=cy+l*sin(angle2);
-        triangle(cx,cy,dx1,dy1,dx2,dy2);
-
-        float t;
-        float px1,py1,px2,py2;
-        for (int i=0;i<17;i++){
-        t=alea.nextFloat();
-        px1 = (1 - t) * cx + (t * dx1);
-        py1 = (1 - t) * cy + (t * dy1);
-        t=alea.nextFloat();
-        px2 = (1 - t) * cx + (t * dx2);
-        py2 = (1 - t) * cy + (t * dy2);
-        line(px1,py1,px2,py2);}
+    private void ray() {
+        stroke(0, 0, 100);
+        fill(0, 0, 100, 42);
+        float cx = (float) (0.84 * w) + alea.nextFloat() * 11;
+        float cy = (float) (0.21 * w) + alea.nextFloat() * 11;
+        float angle1 = alea.nextFloat() * (2 * PI);
+        float angle2 = angle1 + alea.nextFloat() * PI / 10;
+        float l = h/2;//alea.nextFloat() * h;
+        float t=(float)0.1;
+        float cx1, cy1, cx2, cy2;
+        float dx1 = cx + l * cos(angle1);
+        float dy1 = cy + l * sin(angle1);
+        float dx2 = cx + l * cos(angle2);
+        float dy2 = cy + l * sin(angle2);
+            cx1 = (1 - t) * cx + (t * dx1);
+            cy1 = (1 - t) * cy + (t * dy1);
+            cx2 = (1 - t) * cx + (t * dx2);
+            cy2 = (1 - t) * cy + (t * dy2);
+//        triangle(cx, cy, dx1, dy1, dx2, dy2);
+        quad(cx1,cy1,dx1,dy1,dx2,dy2,cx2,cy2);
+        float px1, py1, px2, py2;
+        
+        for (int i = 0; i < 42; i++) {
+            t = alea.nextFloat();
+            px1 = (1 - t) * cx1 + (t * dx1);
+            py1 = (1 - t) * cy1 + (t * dy1);
+            t = alea.nextFloat();
+            px2 = (1 - t) * cx2 + (t * dx2);
+            py2 = (1 - t) * cy2 + (t * dy2);
+            line(px1, py1, px2, py2);
+        }
 
     }
 
