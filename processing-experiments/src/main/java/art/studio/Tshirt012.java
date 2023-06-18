2b79b3facb98aa1dd2e307139290ba71b02be32d
bbaudry <baudry@kth.se>
2023-06-18 22:51:31 +0200

    -[°}><{°]-

--- processing-experiments/src/main/java/art/studio/Tshirt012.java
+++ processing-experiments/src/main/java/art/studio/Tshirt012.java
@@ -53,7 +53,7 @@ public class Tshirt012 extends PApplet {
             cx2 = (1 - t) * cx + (t * dx2);
             cy2 = (1 - t) * cy + (t * dy2);
         quad(cx1,cy1,dx1,dy1,dx2,dy2,cx2,cy2);
-
+        stroke(330,100,100);
         float px1, py1, px2, py2;        
         for (int i = 0; i < 42; i++) {
             t = alea.nextFloat();
