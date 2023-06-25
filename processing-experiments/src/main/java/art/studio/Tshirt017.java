0e7515e3d36a38b0066fa46a7346959cc4c7e763
bbaudry <baudry@kth.se>
2023-06-25 23:06:13 +0200

    *{+{><}+}*

--- processing-experiments/src/main/java/art/studio/Tshirt017.java
+++ processing-experiments/src/main/java/art/studio/Tshirt017.java
@@ -54,6 +54,12 @@ public class Tshirt017 extends PApplet {
             y2=alea.nextFloat()*h;
             line(x1,y1,x2,y2);
         }
+        if(alea.nextFloat()<0.3){
+            float x3=alea.nextFloat()*w;
+            float y3=alea.nextFloat()*h;
+            noFill();
+            triangle(x1,y1,x2,y2,x3,y3);
+        }
 
         x1=x2;
         y1=y2;
