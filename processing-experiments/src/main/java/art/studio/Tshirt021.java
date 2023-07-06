df96591b6bb6c26d24fe6e850a2efc4bf2e9371f
bbaudry <baudry@kth.se>
2023-07-06 23:22:39 +0200

    °°-{><}-°°

--- processing-experiments/src/main/java/art/studio/Tshirt021.java
+++ processing-experiments/src/main/java/art/studio/Tshirt021.java
@@ -15,6 +15,7 @@ public class Tshirt021 extends PApplet {
     int cellsCount;
     float minx,miny,maxx,maxy;
     float xoff,grain;
+    float hu;
 
     float cx,cy,radius,angle;
     int nbsections,section;
@@ -33,42 +34,31 @@ public class Tshirt021 extends PApplet {
         alea = new Random();
         totalCells = alea.nextInt(5) + 5;
         cellsCount = 0;
+        hu=140;
         xoff = (float)0.0;
-        grain = (float)0.09;
+        grain = (float)0.1;
         minx = (float)(0.12*w); maxx = (float)(0.76*w);
         miny = (float)(0.12*h); maxy = (float)(0.76*h);
         drawing=false;
-        frameRate(1);
     }
 
     @Override
     public void draw() {
-        background(0, 0, 0);
-        if(drawing){
+        if(frameCount<42){
             //paintCell();
             //testB();
             oneCell();//noLoop();
         }
-        else{
-            if(cellsCount<totalCells){
-                cx = randomInWidth();
-                cy = randomInHeight();
-                angle = 0;
-                section = 0;
-                radius = noise(xoff)*168; xoff+=grain;
-                nbsections = 42 + alea.nextInt(42);
-                drawing=true;
-            }
             else{
                 noLoop();
                 save("tshirt021.png");
-            }
         }
     }
 
     private void oneCell(){
         noFill();
-        stroke(50,0,100,84);
+        noStroke();
+        //stroke(50,0,100,84);
         int cake = 42+alea.nextInt(42); 
         angle = 0;
         float a_inc = (2*PI)/cake;
@@ -80,6 +70,8 @@ public class Tshirt021 extends PApplet {
         float iy = yinit;
         float dx=0;
         float dy=0;
+        fill(hu,100,100,21);
+        hu+=4;
         beginShape();
         curveVertex(xinit, yinit);
         curveVertex(xinit, yinit);
@@ -102,57 +94,6 @@ public class Tshirt021 extends PApplet {
 //        line(ix,iy,xinit,yinit);
 //        triangle(cx,cy, ix, iy, dx, dy);
     }
-
-    private void testB(){
-        noFill();
-        stroke(180,100,100);
-beginShape();
-vertex(120, 80);
-bezierVertex(320, 0, 320, 300, 120, 300);
-endShape();
-noStroke();
-fill(0,100,100);
-ellipse(120,80,9,9);
-ellipse(320,0,9,9);
-ellipse(320,300,9,9);
-ellipse(120,300,9,9);
-    }
-
-    private void paintCell(){
-        if (section<nbsections){
-            stroke(50,100,100);//,42+alea.nextFloat()
-            radius = noise(xoff)*168; xoff+=grain;
-            float dx = cx + radius*cos(angle);
-            float dy = cy + radius*sin(angle);
-            //line(cx,cy,dx,dy);
-            edge(radius,dx,dy);
-            angle += (2*PI)/nbsections;
-            section++;
-        }
-        else{
-            drawing=false;
-            cellsCount++;
-        }
-    }
-
-    private void edge(float r, float dx, float dy){
-        float t,px,py;
-        stroke(50,100,100);
-        fill(50,100,100);
-        for (int i=1; i<11; i++){
-            t = (float)(i*0.1);
-            px = (1-t) * cx + (t * dx);
-            py = (1-t) * cy + (t * dy);
-            ellipse(px,py,i,i);
-        }
-    }
-
-    private float randomInWidth(){
-        return minx + alea.nextFloat()*maxx;
-    }   
-    private float randomInHeight(){
-        return miny + alea.nextFloat()*maxy;
-    }   
 
     public static void main(String[] args) {
         String[] processingArgs = { "Tshirt021" };
