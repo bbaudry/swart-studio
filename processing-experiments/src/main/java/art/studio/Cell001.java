effc5f48934b3b4425b1e864b63cd2f21bbaff06
bbaudry <baudry@kth.se>
2023-07-12 18:49:38 +0200

    *°-.><.-°*

--- processing-experiments/src/main/java/art/studio/Cell001.java
+++ processing-experiments/src/main/java/art/studio/Cell001.java
@@ -9,16 +9,17 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Cell001 extends PApplet {
-    int ratio = 1;
-    int w = 1000 * ratio;
-    int h = 1000 * ratio;
+    int ratio = 3;
+    int wi = 1000 * ratio;
+    int he = 1000 * ratio;
+    int w;
     Random alea = new Random();
     float xoffglobal = (float) 0.0;
     float grainglobal = (float) 0.05;
 
     @Override
     public void settings() {
-        size(w, h);
+        size(wi, he);
     }
 
     @Override
@@ -26,29 +27,29 @@ public class Cell001 extends PApplet {
         colorMode(HSB, 360, 100, 100, 250);
         background(0, 0, 0);
         stroke(0, 0, 0);
-        //frameRate(1);
+        // frameRate(1);
     }
 
     @Override
     public void draw() {
         if (frameCount < 2) {
             noFill();
-            stroke(185+alea.nextFloat()*10,100,100,84);
-            background(0,0,0);
-            oneCell(11,84, w/2, h/2);
+            stroke(185 + alea.nextFloat() * 10, 100, 100, 84);
+            background(0, 0, 0);
+            w = wi;
+            oneCell(11, 84, wi / 2, he / 2);
         } else {
             noLoop();
             save("cell001.png");
         }
     }
 
-    private void oneCell(int cake, int layers, float cx, float cy){
-        ArrayList<ArrayList<Float>> radii = setRadii(cake,layers);
+    private void oneCell(int cake, int layers, float cx, float cy) {
+        ArrayList<ArrayList<Float>> radii = setRadii(cake, layers);
         core(cake, layers, radii, cx, cy);
         petals(cake, layers, radii, cx, cy);
-        oneEnvelop(cake, layers, radii, cx,cy);
+        oneEnvelop(cake, layers, radii, cx, cy);
     }
-
 
     private void oneEnvelop(int cake, int layers, ArrayList<ArrayList<Float>> radii, float cx, float cy) {
         noFill();
@@ -78,30 +79,30 @@ public class Cell001 extends PApplet {
         }
     }
 
-    private void changeStrokeColor(){
-            stroke(185+alea.nextFloat()*30,100,100,84);
+    private void changeStrokeColor() {
+        stroke(25 + alea.nextFloat() * 30, 100, 100);
     }
-
 
     private void petals(int cake, int layers, ArrayList<ArrayList<Float>> radii, float cx, float cy) {
         noFill();
-        float angle, radius, py,t, angle_inc;
+        float angle, radius, py, t, angle_inc;
         angle_inc = radians(360 / cake);
-        angle=angle_inc;
-        for (int k = 0; k < layers/2; k++) {
-            angle = angle_inc/2;
+        angle = angle_inc;
+        for (int k = 0; k < layers / 2; k++) {
+            angle = angle_inc / 2;
             for (int i = 0; i < cake; i++) {
-            pushMatrix();
-            translate(cx, cy);
-            radius = radii.get(i).get(k);
-            rotate(angle+radians(-4+alea.nextFloat()*8));
-            t = (float) (0.6);
-            py = (1 - t) * 0 + (t * radius);
-            strokeWeight(1+alea.nextFloat()*2);
-            changeStrokeColor();
-            ellipse(0, py, radius / 4 + alea.nextFloat() * radius / 9, radius / 3 + alea.nextFloat() * radius / 4);
-            angle += angle_inc;
-            popMatrix();            }
+                pushMatrix();
+                translate(cx, cy);
+                radius = radii.get(i).get(k);
+                rotate(angle + radians(-4 + alea.nextFloat() * 8));
+                t = (float) (0.6);
+                py = (1 - t) * 0 + (t * radius);
+                //strokeWeight(1 + alea.nextFloat() * 2);
+                changeStrokeColor();
+                ellipse(0, py, radius / 4 + alea.nextFloat() * radius / 9, radius / 3 + alea.nextFloat() * radius / 4);
+                angle += angle_inc;
+                popMatrix();
+            }
         }
     }
 
@@ -111,7 +112,7 @@ public class Cell001 extends PApplet {
         for (int i = 0; i < cake; i++) {
             ArrayList<Float> radiiVec = new ArrayList<>();
             for (int j = 0; j < layers; j++) {
-                radius = w / 4 + noise(xoffglobal) * w/4;
+                radius = w / 4 + noise(xoffglobal) * w / 4;
                 xoffglobal += grainglobal;
                 radiiVec.add(radius);
             }
@@ -120,58 +121,24 @@ public class Cell001 extends PApplet {
         return radii;
     }
 
-    private void core(int cake, int layers, ArrayList<ArrayList<Float>> radii, float cx, float cy){
+    private void core(int cake, int layers, ArrayList<ArrayList<Float>> radii, float cx, float cy) {
         changeStrokeColor();
         noFill();
-        float s = w/80; //diameter for each particle
-        int rings = 11; //controls the number of rings that are drawn
-        int density = 30; //60 is the most regular distribution, lower is more dense
-        stroke(180,100,100,100);
-        ellipse(cx,cy,s,s);
-        float dx,dy,t;
-        for(int k=1;k<rings;k++){
-            t=s*k;//distance from center (cy,cy)
-                for(int i=0;i<360;i+=density/k){
-                    dx = cx+(t+alea.nextFloat()*11)*cos(radians(i+alea.nextFloat()*7));
-                    dy = cy+(t+alea.nextFloat()*11)*sin(radians(i+alea.nextFloat()*7));
-                    changeStrokeColor();
-                    //stroke(130+alea.nextFloat()*60,100,100,100);
-                    ellipse(dx,dy,s+alea.nextFloat()*7,s+alea.nextFloat()*7);
-                }
+        float s = w / 80; // diameter for each particle
+        int rings = 11; // controls the number of rings that are drawn
+        int density = 30; // 60 is the most regular distribution, lower is more dense
+        ellipse(cx, cy, s, s);
+        float dx, dy, t;
+        for (int k = 1; k < rings; k++) {
+            t = s * k;// distance from center (cy,cy)
+            for (int i = 0; i < 360; i += density / k) {
+                dx = cx + (t + alea.nextFloat() * w / 100) * cos(radians(i + alea.nextFloat() * s));
+                dy = cy + (t + alea.nextFloat() * w / 100) * sin(radians(i + alea.nextFloat() * s));
+                changeStrokeColor();
+                ellipse(dx, dy, s + alea.nextFloat() * s / 2, s + alea.nextFloat() * s / 2);
+            }
         }
-/*         for(int i=0;i<360;i+=60){
-            float dx,dy;
-            t = s;
-            dx = cx+t*cos(radians(i));
-            dy = cy+t*sin(radians(i));
-            ellipse(dx,dy,s,s);
-
-            t=2*s;//2*sqrt(s*s-(s/2)*(s/2));
-            dx = cx+t*cos(radians(i));
-            dy = cy+t*sin(radians(i));
-            ellipse(dx,dy,w/20,w/20);
-            dx = cx+t*cos(radians(i+30));
-            dy = cy+t*sin(radians(i+30));
-            ellipse(dx,dy,s,s);
-
-            t=3*s;
-            dx = cx+t*cos(radians(i));
-            dy = cy+t*sin(radians(i));
-            ellipse(dx,dy,w/20,w/20);
-            dx = cx+t*cos(radians(i+15));
-            dy = cy+t*sin(radians(i+15));
-            ellipse(dx,dy,s,s);
-            dx = cx+t*cos(radians(i+30));
-            dy = cy+t*sin(radians(i+30));
-            ellipse(dx,dy,s,s);
-            dx = cx+t*cos(radians(i+45));
-            dy = cy+t*sin(radians(i+45));
-            ellipse(dx,dy,s,s);
-        }
-*/
-        
     }
-
 
     public static void main(String[] args) {
         String[] processingArgs = { "Cell001" };
