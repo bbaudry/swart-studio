d8dd6f33cc616b9f92ff542f68da17f8dda75d19
bbaudry <baudry@kth.se>
2023-07-17 22:57:35 +0200

    ]]+°><°+[[

--- processing-experiments/src/main/java/art/studio/Practice023.java
+++ processing-experiments/src/main/java/art/studio/Practice023.java
@@ -43,56 +43,45 @@ public class Practice023  extends PApplet {
         px2=cx+rad*cos(radians(60));
         py2=cy+rad*sin(radians(60));
         //line(px1,py1,px2,py2);
+        ellipse(px2,py2,11,11);
+        cpx1=px1+200;//alea.nextInt(100);
+        cpy1=py1+100;//alea.nextInt(100);
+        cpx2=px1-200;//(float)(0.5*px1+(0.5*cpx1));
+        cpy2=py1-100;//(float)(0.5*py1+(0.5*cpy1));
+        bezierVertex(cpx2, cpy2, cpx1, cpy1, px2, py2);
+        line(cpx1,cpy1,cpx2,cpy2);
+
         px1=px2;
         py1=py2;
-        ellipse(px1,py1,11,11);
-        cpx1=px1+200;//alea.nextInt(100);
-        cpy1=py1+200;//alea.nextInt(100);
-        cpx2=px1-200;//(float)(0.5*px1+(0.5*cpx1));
-        cpy2=py1-200;//(float)(0.5*py1+(0.5*cpy1));
-        bezierVertex(cpx2, cpy2, cpx1, cpy1, px1, py1);
-
         px2=cx+rad*cos(radians(120));
         py2=cy+rad*sin(radians(120));
         //line(px1,py1,px2,py2);
-        px1=px2;
-        py1=py2;
         ellipse(px1,py1,11,11);
         cpx1=px1-200;//alea.nextInt(100);
         cpy1=py1+200;//alea.nextInt(100);
-        cpx2=px1+200;//(float)(0.5*px1+(0.5*cpx1));
-        cpy2=py1-200;//(float)(0.5*py1+(0.5*cpy1));
-        bezierVertex(cpx2, cpy2, cpx1, cpy1, px1, py1);
+        cpx2=(float)(0.5*px1+(0.5*cpx1));
+        cpy2=(float)(0.5*py1+(0.5*cpy1));
+        bezierVertex(cpx2, cpy2, cpx1, cpy1, px2, py2);
 
+        px1=px2;
+        py1=py2;
         px2=cx+rad*cos(radians(200));
         py2=cy+rad*sin(radians(200));
         //line(px1,py1,px2,py2);
-        px1=px2;
-        py1=py2;
         ellipse(px1,py1,11,11);
         cpx1=px1+200;//alea.nextInt(100);
         cpy1=py1-200;//alea.nextInt(100);
-        cpx2=px1-200;//(float)(0.5*px1+(0.5*cpx1));
-        cpy2=py1+200;//(float)(0.5*py1+(0.5*cpy1));
-        bezierVertex(cpx2, cpy2, cpx1, cpy1, px1, py1);
+        cpx2=(float)(0.5*px1+(0.5*cpx1));
+        cpy2=(float)(0.5*py1+(0.5*cpy1));
+        bezierVertex(cpx2, cpy2, cpx1, cpy1, px2, py2);
 
-        px2=cx+rad*cos(radians(300));
-        py2=cy+rad*sin(radians(300));
-        //line(px1,py1,px2,py2);
         px1=px2;
         py1=py2;
-        ellipse(px1,py1,11,11);
-        cpx1=px1-200;//alea.nextInt(100);
-        cpy1=py1-200;//alea.nextInt(100);
-        cpx2=px1+200;//(float)(0.5*px1+(0.5*cpx1));
-        cpy2=py1+200;//(float)(0.5*py1+(0.5*cpy1));
-        bezierVertex(cpx2, cpy2, cpx1, cpy1, px1, py1);
-
         //line(px1,py1,px,py);
         cpx1=px-200;//alea.nextInt(100);
         cpy1=py-200;//alea.nextInt(100);
-        cpx2=px+200;//(float)(0.5*px1+(0.5*cpx1));
-        cpy2=py+200;//(float)(0.5*py1+(0.5*cpy1));
+        cpx2=(float)(0.5*px1+(0.5*cpx1));
+        cpy2=(float)(0.5*py1+(0.5*cpy1));
         bezierVertex(cpx2, cpy2, cpx1, cpy1, px, py);
 
         endShape();
