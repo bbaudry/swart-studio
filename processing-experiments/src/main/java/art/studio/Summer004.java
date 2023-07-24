3f7dc2061ba2b681708d7a156c83bdd93ba5c001
bbaudry <baudry@kth.se>
2023-07-24 11:56:16 +0200

    .°}+><+{°.

--- processing-experiments/src/main/java/art/studio/Summer004.java
+++ processing-experiments/src/main/java/art/studio/Summer004.java
@@ -11,9 +11,8 @@ public class Summer004  extends PApplet {
     int w = 1000;
     int h = 1000;
     Random alea;
-    ArrayList<ArrayList<Float>> coords;
     float xoff=(float)0.0;
-    float grain=(float)0.05;
+    float grain=(float)0.09;
     int nbAngles;
     int nbLayers;
 
@@ -28,35 +27,39 @@ public class Summer004  extends PApplet {
         colorMode(HSB,360,100,100,250);
         alea=new Random();
         background(0,0,0);
-        nbLayers=5;
-        initCoords();
+        nbLayers=85;
+        frameRate(1);
     }
 
-    private void initCoords(){
-        coords=new ArrayList<>();
-        float angle=0;
+    private ArrayList<ArrayList<Float[]>> initCoords(){
+        ArrayList<ArrayList<Float[]>> coords=new ArrayList<>();
+        float angle,radius;
         for (int i=0;i<nbLayers;i++){
-            ArrayList<Float> layer=new ArrayList<>();
+            angle=0;
+            ArrayList<Float[]> layer=new ArrayList<>();
             while(angle<360){
-                layer.add(angle);
+                radius=(float)(0.3*w+w*0.2*noise(xoff));
+                xoff+=grain;
                 angle+=21+21*noise(xoff);
                 xoff+=grain;
+                Float[] vec={angle,radius};
+                layer.add(vec);
             }
             coords.add(layer);
+        System.out.println(layer.size());
         }
+        return(coords);
     }
 
     @Override
     public void draw() {
-        float rad_seed = (float)0.1*w;
-        float rad;
-        if(frameCount<2){
+        if(frameCount<7){
             background(0,0,0,42);
             noFill();
             stroke(193,100,100);
+            ArrayList<ArrayList<Float[]>> coords = initCoords();
             for (int i=0; i<nbLayers; i++){
-                rad = (float)(0.3*w+rad_seed*noise(xoff));xoff+=grain;
-                oneLayerCompact(w/2, h/2, rad, coords.get(i));
+                oneLayerCompact(w/2, h/2, coords.get(i));
             }
         }
         else{
@@ -65,7 +68,7 @@ public class Summer004  extends PApplet {
         }
     }
 
-    private void showAngles(ArrayList<Float> angles){
+    private void showAngles(ArrayList<Float> angles, ArrayList<ArrayList<Float[]>> coords){
         float hu = 20;
         float rad_seed = (float)0.1*w;
         float rad;
@@ -77,37 +80,39 @@ public class Summer004  extends PApplet {
                 float x = w/2 + rad*cos(radians(angles.get(j)));
                 float y = h/2 + rad*sin(radians(angles.get(j)));
                 ellipse(x,y,17,17);
+            }
         }
     }
-    }
 
-    private void oneLayerCompact(float cx, float cy, float rad, ArrayList<Float> angles){
-        
-        float px, py, px1, py1, cpx1, cpy1, cpx2, cpy2;
+    private void oneLayerCompact(float cx, float cy, ArrayList<Float[]> angles){
+        float px, py, px1, py1, cpx1, cpy1, cpx2, cpy2,rad;
         Float[] controls;
         beginShape();
-        px=cx+rad*cos(radians(angles.get(0)));
-        py=cy+rad*sin(radians(angles.get(0)));
+        rad=angles.get(0)[1];
+        px=cx+rad*cos(radians(angles.get(0)[0]));
+        py=cy+rad*sin(radians(angles.get(0)[0]));
         vertex(px, py);
-        controls = drawTang(angles.get(0),cx,cy,rad);
+        controls = drawTang(angles.get(0)[0],cx,cy,rad);
         cpx2 = controls[2];
         cpy2 = controls[3];
         for (int i=1; i<angles.size(); i++){
-            px1 = cx + rad * cos(radians(angles.get(i)));
-            py1 = cy + rad * sin(radians(angles.get(i)));
-            controls = drawTang(angles.get(i),cx,cy,rad);
+            rad=angles.get(i)[1];
+            px1 = cx + rad * cos(radians(angles.get(i)[0]));
+            py1 = cy + rad * sin(radians(angles.get(i)[0]));
+            controls = drawTang(angles.get(i)[0],cx,cy,rad);
             cpx1 = controls[0];
             cpy1 = controls[1];
             bezierVertex(cpx2, cpy2, cpx1, cpy1, px1, py1);
             cpx2 = controls[2];
             cpy2 = controls[3];
         }
-        controls=drawTang(angles.get(0),cx,cy,rad);
+        controls=drawTang(angles.get(0)[0],cx,cy,rad);
         cpx1=controls[0];
         cpy1=controls[1];
         bezierVertex(cpx2, cpy2, cpx1, cpy1, px, py);
         endShape();
     }
+
 
     private Float[] drawTang(float deg, float cx, float cy, float rad){
         float tx = cx+rad*cos(radians(deg));
