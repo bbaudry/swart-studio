2798fc6f838af54cf3e84607057d0ccd8736f18b
bbaudry <baudry@kth.se>
2023-08-01 23:59:36 +0200

    [°}-><-{°]

--- processing-experiments/src/main/java/art/studio/Galaxy002.java
+++ processing-experiments/src/main/java/art/studio/Galaxy002.java
@@ -9,8 +9,9 @@ public class Galaxy002 extends PApplet {
     int w = 720;
     int h = 1600;
     Random alea;
-    int wcell = w/10;
-    int hcell = h/10;
+    int ratio=10;
+    int wcell = w/ratio;
+    int hcell = h/ratio;
 
     @Override
     public void settings() {
@@ -39,24 +40,29 @@ public class Galaxy002 extends PApplet {
         int i=hcell;
         while(i<h-hcell){
             int a = alea.nextInt(3);
+            int amount = alea.nextInt(ratio-2);
+
             switch(a){
             case 0:
-                sliders(i);
+                sliders(i,wcell,amount*wcell);
+                plug(i,amount*wcell,w-wcell);
                 break;
             case 1:
-                plug(i);
+                plug(i,wcell,amount*wcell);
+                knob(i,amount*wcell,w-wcell);
                 break;
             case 2:
-                knob(i);
+                knob(i,wcell,amount*wcell);
+                sliders(i,amount*wcell,w-wcell);
                 break;
             }
             i+=hcell;
         }
     }
 
-    private void sliders(int hindex){
-        int j=wcell;
-        while (j<w-wcell){
+    private void sliders(int hindex,int wstart, int wend){
+        int j=wstart;
+        while (j<wend){
             noStroke();
             float x = (float)0.42*wcell;
             rect(j+x,hindex,(float)0.16*wcell,hcell,5);
@@ -66,11 +72,11 @@ public class Galaxy002 extends PApplet {
         }
     }
 
-    private void plug(int hindex){
+    private void plug(int hindex,int wstart, int wend){
         float w4 = wcell/4;
-        float h8 = hcell/8;
-        int j=wcell;
-        while (j<w-wcell){
+        float h8 = hcell/6;
+        int j=wstart;
+        while (j<wend){
             for (float x=w4; x<wcell; x+=w4*2){
                 for (float y=h8; y < hcell; y+=h8*2){
                     ellipse(j+x, hindex+y, (float)1.5*w4, (float)1.5*w4);
@@ -85,18 +91,18 @@ public class Galaxy002 extends PApplet {
         }
     }
 
-    private void knob(int hindex){
+    private void knob(int hindex,int wstart, int wend){
         float w4 = wcell/4;
-        float h8 = hcell/8;
-        int j=wcell;
-        while (j<w-wcell){
+        float h8 = hcell/6;
+        int j=wstart;
+        while (j<wend){
             for (float x=w4; x<wcell; x+=w4*2){
                 for (float y=h8; y < hcell; y+=h8*2){
                     float cx = j+x;
                     float cy = hindex+y;
-                    ellipse(cx,cy, (float)1.2*w4, (float)1.2*w4);
+                    ellipse(cx,cy, (float)1.4*w4, (float)1.4*w4);
                     int ang = alea.nextInt(360);
-                    float rad = (float)0.9*w4;
+                    float rad = (float)1.05*w4;
                     float dx = cx + rad*cos(radians(ang));
                     float dy = cy + rad*sin(radians(ang));
                     pushStyle();
