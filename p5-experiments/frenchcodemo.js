d5a9cbcd09ecdaf49e6fe1ca08bd2fb3bead738f
bbaudry <baudry@kth.se>
2023-11-06 21:32:43 +0100

    *{.°><°.}*

--- p5-experiments/frenchcodemo.js
+++ p5-experiments/frenchcodemo.js
@@ -18,5 +18,19 @@ function centerCanvas() {
 }
 
 function draw() {
+    background(0, 0, 0)
+    noStroke();
+    strokeWeight(1)
+    fill(0,0,0)
+    var cx=w/2
+    var cy=h/2
+    var r = 20
+    var density = 20
+    for(let off=200; off>0; off-=density){
+        if(off%(density*2)==0){    fill(0,0,0)        }
+        else{    fill(50,100,100)        }
+        quad(cx-(random(off-r,off+r)),cy-(random(off-r,off+r)),cx+(random(off-r,off+r)),cy-(random(off-r,off+r)),cx+(random(off-r,off+r)),cy+off,cx-(random(off-r,off+r)),cy+(random(off-r,off+r)))
+    }
 
+    noLoop()
 }
