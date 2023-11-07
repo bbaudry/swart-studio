91516ad682f3efe198c59d8de90560e0300ac379
bbaudry <baudry@kth.se>
2023-11-07 09:10:18 +0100

    }]]]><[[[{

--- p5-experiments/frenchcodemo.js
+++ p5-experiments/frenchcodemo.js
@@ -9,8 +9,9 @@ function setup() {
     centerCanvas();
     colorMode(HSB, 360, 100, 100, 250);
     background(0, 0, 0)
+    noLoop()
 }
-
+ 
 function centerCanvas() {
     var x = (windowWidth - 800) / 2;
     var y = (windowHeight - 800) / 2;
@@ -18,27 +19,19 @@ function centerCanvas() {
 }
 
 function draw() {
-    //background(0, 0, 0)
-    noStroke();
-    strokeWeight(1)
-    fill(0, 0, 0)
-    var cx = w / 2
-    var cy = h / 2
-    var r = 20
-    var density = w/40
-    var step = w/4
-    for (cx=step/2;cx<w;cx+=step){
-        for (cy=step/2;cy<h;cy+=step){
-            for (let off = w/8; off > 0; off -= density) {
-                if (off % (density * 2) == 0) { fill(0, 0, 0) }
-                else { fill(50, 100, 100) }
-                quad(cx - (random(off - r, off + r)), cy - (random(off - r, off + r)),
-                    cx + (random(off - r, off + r)), cy - (random(off - r, off + r)),
-                    cx + (random(off - r, off + r)), cy + off,
-                    cx - (random(off - r, off + r)), cy + (random(off - r, off + r)))
-            }        
-        }
-    }
-
-    noLoop()
+    var cx = w/2
+    var cy = h/2
+    var vera = w/4
+    var molnar = 15
+    var step = 40
+    noStroke()//stroke(0,0,0)
+    for(vera=w/2;vera>0;vera-=step){
+        if(vera%(step*2)==0){fill(50,100,100)}
+        else{fill(0,0,0)}
+    quad(cx-random(vera-molnar,vera+molnar),cy-random(vera-molnar,vera+molnar),
+        cx+random(vera-molnar,vera+molnar), cy-random(vera-molnar,vera+molnar),
+        cx+random(vera-molnar,vera+molnar), cy+random(vera-molnar,vera+molnar),
+        cx-random(vera-molnar,vera+molnar), cy+random(vera-molnar,vera+molnar)
+    )
+}
 }
