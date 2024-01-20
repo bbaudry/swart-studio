99b343c883ab82a605b29bc7d7bf147a3a7b7630
bbaudry <baudry@kth.se>
2024-01-20 18:15:36 -0500

    +-]-><-[-+

--- p5-experiments/cegep/cegepdemo1.js
+++ p5-experiments/cegep/cegepdemo1.js
@@ -10,7 +10,7 @@ function setup() {
     colorMode(HSB, 360, 100, 100, 250);
     noLoop()
 }
- 
+
 function centerCanvas() {
     var x = (windowWidth - 800) / 2;
     var y = (windowHeight - 800) / 2;
@@ -18,19 +18,20 @@ function centerCanvas() {
 }
 
 function draw() {
-    var cx,cy,vera, molnar, density,couleur
-    cx=w*0.5
-    cy=h*0.5
-    vera=w*0.2
-    molnar=21
-    couleur=true
-    density=63
-    for(vera=w*0.5;vera>0;vera-=density){
-        if(couleur){fill(50,100,100);couleur=false}
-    else{fill(0,0,0);couleur=true}
-    quad(cx-random(vera-molnar,vera+molnar),cy-random(vera-molnar,vera+molnar),
-        cx+random(vera-molnar,vera+molnar),cy-random(vera-molnar,vera+molnar),
-        cx+random(vera-molnar,vera+molnar),cy+random(vera-molnar,vera+molnar),
-        cx-random(vera-molnar,vera+molnar),cy+random(vera-molnar,vera+molnar))
-    }
+    var cx, cy, vera, molnar, density, color
+    cx = 0.5 * w
+    cy = 0.5 * h
+    vera = 0.5 * w
+    molnar = 42
+    density = 42
+    color=true
+    for(vera=0.2*w;vera>0;vera-=density){
+        if(color){ fill(50, 100, 100); color=false}
+        else{ fill(0, 0, 0); color=true}
+   
+    quad(cx - random(vera-molnar,vera+molnar), cy - random(vera-molnar,vera+molnar),
+        cx + random(vera-molnar,vera+molnar), cy - random(vera-molnar,vera+molnar),
+        cx + random(vera-molnar,vera+molnar), cy + random(vera-molnar,vera+molnar),
+        cx - random(vera-molnar,vera+molnar), cy + random(vera-molnar,vera+molnar))
+}
 }
