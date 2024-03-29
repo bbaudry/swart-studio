02988c7678c940a371fe3a42056ae1129ecdd1bb
bbaudry <baudry@kth.se>
2024-03-29 18:05:53 -0400

    {*[+><+]*}

--- p5-experiments/plottable/squigglelines001.js
+++ p5-experiments/plottable/squigglelines001.js
@@ -34,32 +34,33 @@ function frames() {
 
 function squiggleline() {
     var px, py, cpx1, cpy1, cpx2, cpy2, px1, py1
-    var wavewidth = rightmargin - leftmargin
-    var ratio = 0.2
-    var c = 20
+    var sqiwid = rightmargin - leftmargin
+    var ratio = 0.07
+    var sqighi = 0.03*h
+    var sqiglo = 0.02*h
     var y = h * 0.5
     beginShape();
     px = leftmargin
     py = y
-    vertex(px, py);//y * 0.0001
+    vertex(px, py);
     for (var x = ratio; x <= 1; x += ratio) {
         ellipse(px,py,5,5)
-        cpx2 = px //+ wavewidth * (ratio * 0.5) 
-        cpy2 = y - 0.1 * h 
-        px += wavewidth * ratio
-        py = y //+ random(-actualheight*0.2, actualheight*0.2)
-        cpx1 = px //- wavewidth * (ratio * 0.5) 
-        cpy1 = y - 0.1 * h 
+        cpx2 = px 
+        cpy2 = y - sqighi
+        px += sqiwid * ratio
+        py = y 
+        cpx1 = px  
+        cpy1 = y - sqighi
         bezierVertex(cpx2, cpy2, cpx1, cpy1, px, py);
         ellipse(cpx1,cpy1,15,15)
         ellipse(cpx2,cpy2,25,25)
 
         ellipse(px,py,5,5)
-        cpx2 = px //- wavewidth * (ratio * 0.1) 
-        cpy2 = y //+ 0.01 * h 
-        px -= wavewidth * ratio*0.2
-        py = y+h*0.03//+h*0.05 //+ random(-actualheight*0.2, actualheight*0.2)
-        cpx1 = px+ wavewidth * (ratio * 0.2) 
+        cpx2 = px 
+        cpy2 = y 
+        px -= sqiwid * ratio*0.2
+        py = y+sqiglo
+        cpx1 = px+ sqiwid * (ratio * 0.2) 
         cpy1 = py  
         bezierVertex(cpx2, cpy2, cpx1, cpy1, px, py);
         ellipse(cpx1,cpy1,15,15)
@@ -67,12 +68,12 @@ function squiggleline() {
 
 
         ellipse(px,py,5,5)
-        cpx2 = px- wavewidth * (ratio * 0.2) 
+        cpx2 = px- sqiwid * (ratio * 0.2) 
         cpy2 = py  
-        px -= wavewidth * ratio*0.2
-        py = y//+h*0.05 //+ random(-actualheight*0.2, actualheight*0.2)
-        cpx1 = px// + wavewidth * (ratio * 0.1) 
-        cpy1 = y //+ 0.01 * h 
+        px -= sqiwid * ratio*0.2
+        py = y
+        cpx1 = px
+        cpy1 = y 
         bezierVertex(cpx2, cpy2, cpx1, cpy1, px, py);
         ellipse(cpx1,cpy1,15,15)
         ellipse(cpx2,cpy2,25,25)
