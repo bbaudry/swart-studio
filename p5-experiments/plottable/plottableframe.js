114370677142196a0c1f3435bfa3ac44a3f14473
bbaudry <baudry@kth.se>
2024-04-26 15:54:25 -0400

    }-+°><°+-{

--- p5-experiments/plottable/plottableframe.js
+++ p5-experiments/plottable/plottableframe.js
@@ -18,8 +18,8 @@ var actualheight = bottommargin - topmargin
 var cnv, imgbtn
 
 function setup() {
-    getsvg()
-    //getpng()
+    //getsvg()
+    getpng()
     centerCanvas();
     colorMode(HSB, 360, 100, 100, 250);
     strokeCap(SQUARE)
