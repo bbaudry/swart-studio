e163647a232a6c2ce1f1e02e4c127b16619da259
bbaudry <baudry@kth.se>
2024-04-29 11:51:55 -0400

    [[{.><.}]]

---
+++ p5-experiments/plottable/showcode.js
@@ -0,0 +1,43 @@
+function showknobs(){
+    posx=leftmargin
+    posy=topmargin+actualheight
+    for(i=0;i<knobs.length;i++){
+        var s = knobs[i].name+" "+knobs[i].value
+        tw = textWidth(s)
+        if (posx + tw > rightmargin) {
+            posx = leftmargin
+            posy += fSize + 1
+        }
+        text(s, posx, posy)
+        posx+=tw+fSize
+    }
+    posx = leftmargin
+    posy += 2*fSize + 1
+}
+
+function showcode() {
+    var allcode, c, tw 
+    allcode = ''
+    for (var i = 0; i < sourcecode.length; i++) {
+        var token = sourcecode[i]
+        var notab = token.toString().replace(/\s/g, '').split('\r\n')[0]
+        allcode += notab
+    }
+    for (let i = 0; i < allcode.length; i++) {
+        c = allcode.charAt(i)
+        tw = textWidth(c)
+        if (posx + tw > rightmargin) {
+            posx = leftmargin
+            posy += fSize + 1
+        }
+        text(c, posx, posy)
+        posx += tw
+    }
+    posx = leftmargin
+    posy += 2*fSize + 1
+}
+
+function showcredits(){
+    var c="al.my.re :: p5.js :: CamBam Stick [destriangles(deslignes(unrond())) 001). April 2024]"
+    text(c,posx,posy)
+}
