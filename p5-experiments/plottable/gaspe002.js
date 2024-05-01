7549b303ba5df60bc50b550fff48e6a9a64c81cc
bbaudry <baudry@kth.se>
2024-04-30 22:56:14 -0400

    [}][><][{]

--- p5-experiments/plottable/gaspe002.js
+++ p5-experiments/plottable/gaspe002.js
@@ -1,9 +1,9 @@
 function savesvg() {
-    save("gaspe001.svg");
+    save("gaspe002.svg");
 }
 
 function savepng() {
-    save("gaspe001.png");
+    save("gaspe002.png");
 }
 
 var font, posx, posy, knobs = [], grid = []
@@ -13,7 +13,7 @@ var nbhorizontalsteps = Math.floor(actualwidth / stepsize)
 var nbvertcicalsteps = Math.floor(actualheight / stepsize)
 function preload() {
     font = loadFont("../fonts/1CAMBam_Stick_9.ttf");
-    sourcecode = loadStrings('gaspe001.js');
+    sourcecode = loadStrings('gaspe002.js');
 
 }
 
@@ -62,21 +62,21 @@ function drawgrid() {
         for (i = 0; i < nbhorizontalsteps; i++) {
             var index=i+j*nbhorizontalsteps
             var v = grid[index]
-            var x=leftmargin+i*stepsize
-            var y=topmargin+j*stepsize
+            var x=leftmargin+i*stepsize+stepsize*0.5
+            var y=topmargin+j*stepsize+stepsize*0.5
             var ratio
-            if(v<0.1){ratio=0.1}
-            if(v>=0.1&&v<0.2){ratio=0.14}
-            if(v>=0.2&&v<0.3){ratio=0.17}
-            if(v>=0.3&&v<0.4){ratio=0.2}
-            if(v>=0.4&&v<0.5){ratio=0.23}
-            if(v>=0.5&&v<0.6){ratio=0.26}
-            if(v>=0.6&&v<0.7){ratio=0.29}
-            if(v>=0.7&&v<0.8){ratio=0.32}
-            if(v>=0.8&&v<0.9){ratio=0.35}
+            if(v<0.1){ratio=0.05}
+            if(v>=0.1&&v<0.2){ratio=0.08}
+            if(v>=0.2&&v<0.3){ratio=0.11}
+            if(v>=0.3&&v<0.4){ratio=0.14}
+            if(v>=0.4&&v<0.5){ratio=0.17}
+            if(v>=0.5&&v<0.6){ratio=0.2}
+            if(v>=0.6&&v<0.7){ratio=0.23}
+            if(v>=0.7&&v<0.8){ratio=0.26}
+            if(v>=0.8&&v<0.9){ratio=0.29}
             if(v>=0.9){ratio=0.38}
-            rect(x+stepsize*ratio,y+stepsize*ratio,stepsize*(1-ratio*2),stepsize*(1-ratio*2))
-            rect(x+2+stepsize*ratio,y+2+stepsize*ratio,stepsize*(1-ratio*2),stepsize*(1-ratio*2))
+            ellipse(x,y,stepsize*ratio,stepsize*ratio)
+            ellipse(x,y,stepsize*ratio+2,stepsize*ratio+2)
         }
     }
 }
