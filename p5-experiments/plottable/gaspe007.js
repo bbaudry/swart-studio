441d226ac2a479b3bb6f61e6d5ac0dc49f3a02ff
bbaudry <baudry@kth.se>
2024-05-23 17:34:01 -0400

    -°-°><°-°-

--- p5-experiments/plottable/gaspe007.js
+++ p5-experiments/plottable/gaspe007.js
@@ -8,7 +8,7 @@ function savepng() {
 
 var font, posx, posy, knobs = []
 var fSize = 15
-var stepsize,nbhorizontalsteps, nbvertcicalsteps,grid
+var stepsize, nbhorizontalsteps, nbvertcicalsteps, grid
 function preload() {
     font = loadFont("../fonts/1CAMBam_Stick_9.ttf");
     sourcecode = loadStrings('gaspe007.js');
@@ -17,8 +17,7 @@ function preload() {
 
 function draw() {
     background(0, 0, 100)
-    stroke(300, 100, 100)
-    stepsize = Math.floor(actualwidth * 0.5)
+    stepsize = Math.floor(actualwidth * 0.003)
     nbhorizontalsteps = Math.floor(actualwidth / stepsize)
     nbvertcicalsteps = Math.floor(actualheight / stepsize)
     initgrid()
@@ -37,7 +36,7 @@ function saveknob(name, value) {
 }
 
 function initgrid() {
-    grid=[]
+    grid = []
     var yoff = 0.0
     var xoff
     var inc = 0.004
@@ -46,7 +45,7 @@ function initgrid() {
         xoff = 0.0
         for (i = 0; i < nbhorizontalsteps; i++) {
             xoff += inc
-            grid.push(noise(xoff,yoff))
+            grid.push(noise(xoff, yoff))
         }
     }
 }
@@ -54,20 +53,32 @@ function initgrid() {
 function drawgrid() {
     for (j = 0; j < nbvertcicalsteps; j++) {
         for (i = 0; i < nbhorizontalsteps; i++) {
-            var index=i+j*nbhorizontalsteps
-            var v=grid[index]
+            var index = i + j * nbhorizontalsteps
+            var v = grid[index]
             x = leftmargin + i * stepsize
             y = topmargin + j * stepsize
-            /*if (v < 0.1) { arc(x, y, stepsize * 2, stepsize * 2, 0, PI * 0.5) }
-            if (v >= 0.1 && v < 0.2) { arc(x + stepsize, y, stepsize * 2, stepsize * 2, PI * 0.5, PI) }*/
-            if (v >= 0.2 && v < 0.3) { arc(x + stepsize, y + stepsize, stepsize * 2, stepsize * 2, PI, PI * 1.5) }
-            if (v >= 0.3 && v < 0.4) { arc(x, y + stepsize, stepsize * 2, stepsize * 2, PI * 1.5, PI * 2) }
-            if (v >= 0.4 && v < 0.5) { arc(x, y + stepsize * 0.5, stepsize, stepsize, PI * 1.5, PI * 2.5) }
-            if (v >= 0.5 && v < 0.6) { arc(x + stepsize, y + stepsize * 0.5, stepsize, stepsize, PI * 0.5, PI * 1.5) }
-            /*if (v >= 0.6 && v < 0.7) { arc(x + stepsize * 0.5, y, stepsize, stepsize, 0, PI) }
-            if (v >= 0.7 && v < 0.8) { arc(x + stepsize * 0.5, y + stepsize, stepsize, stepsize, PI, PI * 2) }
-            if (v >= 0.8 && v < 0.9) { ellipse(x + stepsize * 0.5, y + stepsize * 0.5, stepsize, stepsize) }
-            if (v >= 0.9) { rect(x, y, stepsize, stepsize, v, v, v, v) }*/
+            if (v >= 0.4 && v < 0.44) {
+                stroke(300, 100, 100)
+                ellipse(x + stepsize * 0.5, y + stepsize * 0.5, stepsize, stepsize)
+            }
+            else {
+                if (v >= 0.5 && v < 0.52) {
+                    stroke(310, 100, 100)
+                    ellipse(x + stepsize * 0.5, y + stepsize * 0.5, stepsize, stepsize)
+                }
+                else{
+                    if (v >= 0.64 && v < 0.66) {
+                        stroke(330, 100, 100)
+                        ellipse(x + stepsize * 0.5, y + stepsize * 0.5, stepsize, stepsize)
+                    }   
+                    else{
+                        stroke(0, 0, 0)
+                        ellipse(x + stepsize * 0.5, y + stepsize * 0.5, stepsize, stepsize)
+
+                    }     
+                }
+            }
+            // if (v >= 0.64 && v < 0.68) { ellipse(x + stepsize*0.5, y + stepsize*0.5, stepsize, stepsize) }
         }
     }
 
