8de165e66297b0d3ac5e89f56f692553208c9360
bbaudry <baudry@kth.se>
2024-05-29 10:12:22 -0400

    {.]}><{[.}

---
+++ p5-experiments/plottable/gaspe008.js
@@ -0,0 +1,67 @@
+function savesvg() {
+    save("gaspe008.svg");
+}
+
+function savepng() {
+    save("gaspe008.png");
+}
+
+var font, posx, posy, knobs = []
+var fSize = 15
+var stepsize, nbhorizontalsteps, nbvertcicalsteps, grid
+function preload() {
+    font = loadFont("../fonts/1CAMBam_Stick_9.ttf");
+    sourcecode = loadStrings('gaspe008.js');
+
+}
+
+function draw() {
+    background(0, 0, 100)
+    stepsize = Math.floor(actualwidth * 0.011)
+    nbhorizontalsteps = Math.floor(actualwidth / stepsize)
+    nbvertcicalsteps = Math.floor(actualheight / stepsize)
+    grid()
+    stroke(0, 0, 0)
+    textFont(font)
+    textSize(fSize)
+    showknobs()
+    showcode()
+    showcredits()
+    noLoop()
+}
+
+function saveknob(name, value) {
+    knobs.push({ name: name, value: value.toFixed(2) })
+}
+
+function grid() {
+    grid = []
+    var yoff = 0.0
+    var xoff, v, x, y
+    var inc = 0.004
+    for (j = 0; j < nbvertcicalsteps; j++) {
+        yoff += inc
+        xoff = 0.0
+        for (i = 0; i < nbhorizontalsteps; i++) {
+            xoff += inc
+            v = noise(xoff, yoff)
+            x = leftmargin + i * stepsize
+            y = topmargin + j * stepsize
+
+            grid.push(v)
+            if (v >= 0.34 && v < 0.44 || v >= 0.5 && v < 0.6) {
+                stroke(200, 100, 100)
+            }
+            else {
+                stroke(300, 100, 100)
+            }
+            ellipse(x + stepsize * 0.5, y + stepsize * 0.5, stepsize*2, stepsize*2)
+        }
+    }
+}
+
+
+function showcredits() {
+    var c = "al.my.re :: p5.js :: CamBam Stick :: vyper [gaspe 008). May 2024]"
+    text(c, posx, posy)
+}
