8e70dd0b3726978e828e5346f8f61cb58ab33958
bbaudry <baudry@kth.se>
2024-06-03 11:18:55 -0400

    +}*]><[*{+

--- p5-experiments/plottable/gaspe008.js
+++ p5-experiments/plottable/gaspe008.js
@@ -6,9 +6,9 @@ function savepng() {
     save("gaspe008.png");
 }
 
-var font, posx, posy, stepsize, nbhorizontalsteps, nbvertcicalsteps
-var fSize = 15
-var palette = [{ h1: 280, h2: 300, h3: 330 }, { h1: 180, h2: 30, h3: 200 }, { h1: 280, h2: 30, h3: 250 }]
+var font, xoff,yoff,inc,shake, h1,h2,h3,stepsize, nbhorizontalsteps, nbvertcicalsteps
+var fSize = 14
+var palette = [{ h1: 280, h2: 300, h3: 330 }, { h1: 180, h2: 30, h3: 200 }, { h1: 280, h2: 30, h3: 250 }, { h1: 270, h2: 0, h3: 90 }]
 function preload() {
     font = loadFont("../fonts/1CAMBam_Stick_9.ttf");
     sourcecode = loadStrings('gaspe008.js');
@@ -16,7 +16,12 @@ function preload() {
 
 function draw() {
     background(0, 0, 100)
-    stepsize = Math.floor(actualwidth * 0.009)
+    //inc,shake,stepsize,palettecolor are the main knobs that steer the artwork 
+    inc = 0.004
+    if(random()<0.5){shake=true;}else{shake=false}
+    stepsize = Math.floor(actualwidth * 0.011)
+    palettecolor = palette[Math.floor(random(palette.length))]
+    h1=palettecolor.h1;h2=palettecolor.h2;h3=palettecolor.h3;
     var vratio = random(0.21, 0.63)
     var hratio = random(0.21, 0.63)
     var oratio = random(0.009, 0.03)
@@ -31,12 +36,12 @@ function draw() {
     noLoop()
 }
 
+
 function grid(x1, y1, x2, y2, stepsize) {
     nbhorizontalsteps = Math.floor((x2 - x1) / stepsize)
     nbvertcicalsteps = Math.floor((y2 - y1) / stepsize)
-    var yoff = 0.0
-    var xoff, v, x, y
-    var inc = 0.004
+    var v, x, y
+    yoff = 0.0
     for (j = 0; j < nbvertcicalsteps; j++) {
         yoff += inc
         xoff = 0.0
@@ -46,22 +51,23 @@ function grid(x1, y1, x2, y2, stepsize) {
             x = x1 + i * stepsize
             y = y1 + j * stepsize
             if (v >= 0.34 && v < 0.44) {
-                stroke(0, 100, 100)//300
+                stroke(h1, 100, 100)//300
             }
             else {
                 if (v >= 0.5 && v < 0.6) {
-                    stroke(90, 100, 100)//330
+                    stroke(h2, 100, 100)//330
                 }
                 else {
                     if (v >= 0.63 && v < 0.84) {
                         noStroke()
                     }
                     else {
-                        stroke(270, 100, 100)//280
+                        stroke(h3, 100, 100)//280
                     }
                 }
             }
-            var loupe =  random(1, 1.2)
+            var loupe 
+            if(shake){loupe=random(1, 1.42)}else{loupe=2}
             ellipse(x + stepsize * 0.5, y + stepsize * 0.5, stepsize * loupe, stepsize * loupe)
         }
     }
@@ -69,6 +75,6 @@ function grid(x1, y1, x2, y2, stepsize) {
 
 
 function showcredits(posx, posy) {
-    var c = "al.my.re :: p5.js :: CamBam Stick :: noise :: vpype [gaspe 008). May 2024]"
+    var c = "al.my.re :: p5.js :: CamBam Stick :: noise :: vpype [gaspe 008). June 2024]"
     text(c, posx, posy)
 }
