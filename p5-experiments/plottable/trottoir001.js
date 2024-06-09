a18ac383012f8b652430c77713b1c1a94e9b45e4
bbaudry <baudry@kth.se>
2024-06-09 14:22:50 -0400

    ].]{><}[.[

--- p5-experiments/plottable/trottoir001.js
+++ p5-experiments/plottable/trottoir001.js
@@ -24,15 +24,17 @@ function draw() {
 function grid() {
     var cx = leftmargin + actualwidth * 0.5
     var cy = topmargin + actualheight * 0.5
-    var x1, y1, x2, y2, pas
-    for (i = 0; i < 17; i++){
-            pas = 90-(i*4)
-            amplitude = 21+(i*17)
-            for (var a = 0; a < 360; a += pas) {
+    var x1, y1, x2, y2, pas, debut, deux
+    for (i = 0; i < 17; i++) {
+        pas = 90 - (i * 4)
+        amplitude = 21 + (i * 17)
+        debut=random(360)
+        for (var a = debut; a < debut+360; a += pas) {
             x1 = cx + amplitude * cos(radians(a))
             y1 = cy + amplitude * sin(radians(a))
-            x2 = cx + amplitude * cos(radians(a + pas))
-            y2 = cy + amplitude * sin(radians(a + pas))
+            if(a+pas>debut+360){deux=debut}else{deux=a+pas}
+            x2 = cx + amplitude * cos(radians(deux))
+            y2 = cy + amplitude * sin(radians(deux))
             line(x1, y1, x2, y2)
         }
     }
