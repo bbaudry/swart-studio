4b183d115a81f88441d310fd934135b87a6c2049
bbaudry <baudry@kth.se>
2024-06-10 22:21:56 -0400

    .}[+><+]{.

--- p5-experiments/plottable/trottoir002.js
+++ p5-experiments/plottable/trottoir002.js
@@ -26,20 +26,27 @@ function draw() {
 function grid() {
     var cx = leftmargin + actualwidth * 0.5
     var cy = topmargin + actualheight * 0.5
-    var x1, y1, x2, y2, amplitude, pas, debut
-    for (i = 0; i < 17; i++) {
-        amplitude = 21 + (i * 19)
+    var x1, y1, x2, y2, amplitude1, amplitude2, pas, debut1, debut2
+    debut1 = noise(xoff)*360
+    for (i = 0; i < 7; i++) {
+        amplitude1 = 21 + (i * 19)
+        amplitude2 = 21 + ((i+1) * 19)
         pas = 60
-        debut = noise(xoff)*360
+        debut2 = noise(xoff)*360
         xoff+=inc
-        for (var a = debut; a < debut+360; a += pas) {
-            x1 = cx + amplitude * cos(radians(a))
-            y1 = cy + amplitude * sin(radians(a))
-            if(a+pas>debut+360){deux=debut}else{deux=a+pas}
-            x2 = cx + amplitude * cos(radians(deux))
-            y2 = cy + amplitude * sin(radians(deux))
+        for(var j=0;j<6;j++){
+            var a = debut1+j*pas
+            x1 = cx + amplitude1 * cos(radians(a))
+            y1 = cy + amplitude1 * sin(radians(a))
+            x2 = cx + amplitude1 * cos(radians(a+pas))
+            y2 = cy + amplitude1 * sin(radians(a+pas))
+            line(x1, y1, x2, y2)
+            var deux=debut2+j*pas
+            x2 = cx + amplitude2 * cos(radians(deux))
+            y2 = cy + amplitude2 * sin(radians(deux))
             line(x1, y1, x2, y2)
         }
+        debut1=debut2
     }
 }
 
