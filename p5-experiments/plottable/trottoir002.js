6ba92d3ee3ad392f5a179b9cac2cc4dad9d7f46a
bbaudry <baudry@kth.se>
2024-06-21 23:16:57 -0400

    +].}><{.[+

--- p5-experiments/plottable/trottoir002.js
+++ p5-experiments/plottable/trottoir002.js
@@ -9,7 +9,7 @@ function savepng() {
 var font
 var fSize = 14
 var xoff = 0.0
-var inc = 0.1
+var inc = 0.2
 function preload() {
     font = loadFont("../fonts/1CAMBam_Stick_9.ttf");
     sourcecode = loadStrings('trottoir002.js');
@@ -18,9 +18,10 @@ function preload() {
 function draw() {
     background(0, 0, 100)
     stroke(0, 0, 0)
+    inc = random(1,2)
     //initgrid()
     //drawgrid()
-    test()
+    test2()
     noLoop()
 }
 
@@ -59,6 +60,7 @@ function test(){
     var y = topmargin+actualheight*0.5
     while(x>leftmargin){
         x-=Math.exp(i)
+        line(x,topmargin,x,bottommargin)
         ellipse(x,y,5,5)
         i+=inc
     }
@@ -66,21 +68,36 @@ function test(){
     while(x<rightmargin){
         x+=Math.exp(i)
         ellipse(x,y,5,5)
+        line(x,topmargin,x,bottommargin)
         i+=inc
     }
     x=w*0.5;i=0
     while(y<bottommargin){
         y+=Math.exp(i)
         ellipse(x,y,5,5)
+        line(leftmargin,y,rightmargin,y)
         i+=inc
     }
     y=topmargin+actualheight*0.5;i=0
     while(y>topmargin){
         y-=Math.exp(i)
         ellipse(x,y,5,5)
+        line(leftmargin,y,rightmargin,y)
         i+=inc
     }
+}
 
+function test2(){
+    translate(leftmargin+actualwidth*0.5,topmargin+actualheight*0.5)
+    var a=0
+    for(i=0;i<actualwidth*0.5;i+=Math.exp(a)){
+        var b=0
+        for(j=0;j<actualheight*0.5;j+=Math.exp(b)){
+            ellipse(i,j,3,3)
+            b+=inc
+        }
+        a+=inc
+    }
 }
 
 function showcredits(posx, posy) {
