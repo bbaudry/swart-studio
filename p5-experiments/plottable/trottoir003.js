f91e9d5839c096b3299e5a2e0ee9701c88568d97
bbaudry <baudry@kth.se>
2024-06-26 08:54:09 -0400

    +*{+><+}*+

--- p5-experiments/plottable/trottoir003.js
+++ p5-experiments/plottable/trottoir003.js
@@ -7,11 +7,11 @@ function savepng() {
 }
 
 var font
-var fSize = 14
+var fSize = 11
 var xoff = 0.0
 var yoff = 0.0
 var xinc = 0.1
-var yinc = 0.001
+var yinc = 0.01
 function preload() {
     font = loadFont("../fonts/1CAMBam_Stick_9.ttf");
     sourcecode = loadStrings('trottoir002.js');
@@ -23,7 +23,11 @@ function draw() {
     noFill()
     rect(0,0,w,h)
     gridexp()
-    drawsection()
+    //drawsection()
+    testbeziervertex()
+    textFont(font)
+    textSize(fSize)
+    showcredits(leftmargin,bottommargin+fSize)
     noLoop()
 }
 
@@ -39,7 +43,7 @@ function gridexp() {
         col=[]
         for (y = topmargin; y < actualheight; y += Math.exp(stepy)) {
             //random(-Math.exp(stepx)*0.2,Math.exp(stepx)*0.2)
-            col.push({x:x+(noise(xoff)*Math.exp(stepx)-2*Math.exp(stepx)),y:y+random(-Math.exp(stepy)*0.2,Math.exp(stepy)*0.2)})
+            col.push({x:x+(noise(xoff)*Math.exp(stepx)-2*Math.exp(stepx)),y:y+(noise(yoff)*Math.exp(stepy)-2*Math.exp(stepy))})
             stepy += expstep//+noise(yoff)
             yoff+=yinc
             xoff+=xinc
@@ -69,7 +73,87 @@ function drawsection(){
     }
 }
 
+function testbezier(){
+    fill(0,0,100)
+    var cx,cy,x1,y1,x2,y2
+    cx=leftmargin+actualwidth*0.5
+    cy=bottommargin
+    x1=leftmargin+actualwidth*0.1
+    y1=topmargin+actualheight*0.7
+    x2=leftmargin+actualwidth*0.9
+    y2=topmargin+actualheight*0.7
+    bezier(cx,cy,x1,y1,x2,y2,cx,cy)
+    noFill()
+    ellipse(x1,y1,5,5)
+    ellipse(x2,y2,5,5)
+}
+
+function testbeziervertex(){
+    var apx1,apy1,apx2,apy2,cpx1,cpy1,cpx2,cpy2
+    var x1,y1,x2,y2,x3,y3,x4,y4
+    x1=leftmargin+actualwidth*0.3
+    y1=bottommargin
+    x2=leftmargin
+    y2=topmargin+actualheight*0.5
+    x3=leftmargin+actualwidth*0.8
+    y3=topmargin
+    x4=rightmargin
+    y4=topmargin+actualheight*0.3
+    beginShape()
+    apx1=x1
+    apy1=y1
+    vertex(apx1,apy1)
+
+    cpx1=x2
+    cpy1=y1
+    cpx2=cpx1
+    cpy2=y2
+    apx2=cpx1
+    apy2=cpy2
+    bezierVertex(cpx1,cpy1,cpx2,cpy2,apx2,apy2)
+    stroke(0,100,100);ellipse(cpx1,cpy1,5,5)
+    stroke(50,100,100);ellipse(cpx2,cpy2,5,5)
+    stroke(100,100,100);ellipse(apx2,apy2,5,5)
+
+    cpx1=x2
+    cpy1=y3
+    cpx2=x3
+    cpy2=cpy1
+    apx2=cpx2
+    apy2=cpy1
+    bezierVertex(cpx1,cpy1,cpx2,cpy2,apx2,apy2)
+    stroke(0,100,100);ellipse(cpx1,cpy1,5,5)
+    stroke(50,100,100);ellipse(cpx2,cpy2,5,5)
+    stroke(100,100,100);ellipse(apx2,apy2,5,5)
+
+    cpx1=x4
+    cpy1=y3
+    cpx2=cpx1
+    cpy2=y4
+    apx2=cpx1
+    apy2=cpy2
+    bezierVertex(cpx1,cpy1,cpx2,cpy2,apx2,apy2)
+    stroke(0,100,100);ellipse(cpx1,cpy1,5,5)
+    stroke(50,100,100);ellipse(cpx2,cpy2,5,5)
+    stroke(100,100,100);ellipse(apx2,apy2,5,5)
+
+    cpx1=x4
+    cpy1=y1
+    cpx2=x1
+    cpy2=cpy1
+    apx2=cpx2
+    apy2=cpy1
+    bezierVertex(cpx1,cpy1,cpx2,cpy2,apx2,apy2)
+    stroke(0,100,100);ellipse(cpx1,cpy1,5,5)
+    stroke(50,100,100);ellipse(cpx2,cpy2,5,5)
+    stroke(100,100,100);ellipse(apx2,apy2,5,5)
+
+    fill(0,0,100)
+    stroke(0,0,100);
+    endShape()
+}
+
 function showcredits(posx, posy) {
-    var c = "al.my.re :: p5.js :: CamBam Stick :: noise :: vpype [trottoir 002). June 2024]"
+    var c = "al.my.re :: p5.js :: CamBam Stick :: noise :: vpype [trottoir 003). June 2024]"
     text(c, posx, posy)
 }
