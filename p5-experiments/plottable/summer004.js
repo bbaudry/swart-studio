b8e37200960424ae1b2079feaac816b4c9eb61d9
bbaudry <baudry@kth.se>
2024-07-15 22:04:09 -0400

    -°}]><[{°-

--- p5-experiments/plottable/summer004.js
+++ p5-experiments/plottable/summer004.js
@@ -35,28 +35,23 @@ function rows(x1, y1, x2, y2, x3, y3, x4, y4, dep) {
     t1 = 0
     t2 = 0
     d = dep + 1
-    ox1 = (1 - t1) * x1 + (t1 * x4)
-    oy1 = (1 - t1) * y1 + (t1 * y4)
-    dx1 = (1 - t2) * x2 + (t2 * x3)
-    dy1 = (1 - t2) * y2 + (t2 * y3)
     while (t1 < 1 && t2 < 1) {
-        line(ox1, oy1, dx1, dy1);
+        ox1 = (1 - t1) * x1 + (t1 * x4)
+        oy1 = (1 - t1) * y1 + (t1 * y4)
+        dx1 = (1 - t2) * x2 + (t2 * x3)
+        dy1 = (1 - t2) * y2 + (t2 * y3)
+            line(ox1, oy1, dx1, dy1);
         t1 += random() * 0.17;
         t2 += random() * 0.17;
-        ox2 = (1 - t1) * x1 + (t1 * x4)
+        if (d < 2) {
+            ox2 = (1 - t1) * x1 + (t1 * x4)
         oy2 = (1 - t1) * y1 + (t1 * y4)
         dx2 = (1 - t2) * x2 + (t2 * x3)
         dy2 = (1 - t2) * y2 + (t2 * y3)
-        if (d < 2) {
             rows(ox1, oy1, ox2, oy2, dx2, dy2, dx1, dy1, d)
-            line(ox2,oy2,dx2,dy2)
-        }
-        ox1 = ox2
-        oy1 = oy2
-        dx1 = dx2
-        dy1 = dy2
-}
+        } 
     }
+}
 
 var randomnumbers = []
 function randstore(min, max) {
