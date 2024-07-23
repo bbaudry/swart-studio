ef5162407659eff98d97d43c23f7802eef152859
bbaudry <baudry@kth.se>
2024-07-22 22:52:29 -0400

    {-*.><.*-}

--- p5-experiments/plottable/summer005.js
+++ p5-experiments/plottable/summer005.js
@@ -17,9 +17,9 @@ function preload() {
 function draw() {
     background(0, 0, 100)
     drawpostcard(globalmargin, globalmargin)
-    drawpostcard(globalmargin+wpostcard, globalmargin)
-    drawpostcard(globalmargin, globalmargin+hpostcard)
-    drawpostcard(globalmargin+wpostcard, globalmargin+hpostcard)
+    drawpostcard(globalmargin + wpostcard, globalmargin)
+    drawpostcard(globalmargin, globalmargin + hpostcard)
+    drawpostcard(globalmargin + wpostcard, globalmargin + hpostcard)
     noLoop()
 }
 
@@ -30,100 +30,128 @@ function drawpostcard(x, y) {
     rect(x, y, wpostcard, hpostcard)
     sign()
 }
-function onepiece(){
+function onepiece() {
     alea = []
-    punch()
+    punch3()
 }
 
-function punch(){
+function punch() {
     stroke(0, 0, 0)
-    var xoff,yoff,i,j,yinc
-    yinc=1+random()
-    xoff=floor(actualwidth*0.01)
-    i=leftmargin
-    while(i<rightmargin-xoff){
-        j=bottommargin
-        yoff=floor(actualheight*0.001)
-        while(j>topmargin-yoff){
-            var black=random(); alea.push(black)
-            if(black<0.21){
-                colorie(i,j,i+xoff,j,i+xoff,j+yoff,i,j+yoff)
+    var xoff, yoff, i, j, yinc
+    yinc = 1 + random()
+    xoff = floor(actualwidth * 0.01)
+    i = leftmargin
+    while (i < rightmargin - xoff) {
+        j = bottommargin
+        yoff = floor(actualheight * 0.001)
+        while (j > topmargin - yoff) {
+            var black = random(); alea.push(black)
+            if (black < 0.21) {
+                colorie(i, j, i + xoff, j, i + xoff, j + yoff, i, j + yoff)
             }
-            else{
-                var bound=random(); alea.push(bound)
-                if(bound>0.42){
-                    line(i+xoff,j,i,j+yoff)
+            else {
+                var bound = random(); alea.push(bound)
+                if (bound > 0.42) {
+                    line(i + xoff, j, i, j + yoff)
                 }
-                if(bound<0.1){
-                    console.log("losange")
-                    colorie(i,j,i+xoff,j,i,j+yoff,i-xoff,j+yoff)
+                if (bound < 0.1) {
+                    colorie(i, j, i + xoff, j, i, j + yoff, i - xoff, j + yoff)
                 }
             }
             yoff++;
-            j-=yoff
+            j -= yoff
         }
         xoff++
-        i+=xoff
+        i += xoff
     }
 }
 
-var offset=0.0
-var xinc=0.01
-function punch2(){
+var offset = 0.0
+var xinc = 0.01
+function punch2() {
     stroke(0, 0, 0)
-    var xoff,yoff,i,j,plein
-    xoff=floor(actualwidth*0.01)
-    i=leftmargin
-    while(i<rightmargin-xoff){
-        j=bottommargin
-        yoff=noise(offset)*11;offset+=xinc; alea.push(yoff)
-        while(j>topmargin-yoff){
-            plein=random();alea.push(plein)
-            if(plein<0.11){
-                colorie(i,j,i+xoff,j,i+xoff,j+yoff,i,j+yoff)
+    var xoff, yoff, i, j, plein
+    xoff = floor(actualwidth * 0.01)
+    i = leftmargin
+    while (i < rightmargin - xoff) {
+        j = bottommargin
+        yoff = noise(offset) * 11; offset += xinc; alea.push(yoff)
+        while (j > topmargin - yoff) {
+            plein = random(); alea.push(plein)
+            if (plein < 0.11) {
+                colorie(i, j, i + xoff, j, i + xoff, j + yoff, i, j + yoff)
             }
-            else{
-                var bound=random(); alea.push(bound)
-                if(bound>0.42){
-                    line(i,j,i,j+yoff)
+            else {
+                var bound = random(); alea.push(bound)
+                if (bound > 0.42) {
+                    line(i, j, i, j + yoff)
                 }
-                else{
-                    line(i+xoff,j,i,j+yoff)
+                else {
+                    line(i + xoff, j, i, j + yoff)
                 }
             }
-            j-=yoff
-            yoff=noise(offset)*11;offset+=xinc; alea.push(yoff)
+            j -= yoff
+            yoff = noise(offset) * 11; offset += xinc; alea.push(yoff)
         }
         xoff++
-        i+=xoff
+        i += xoff
     }
 }
 
+function punch3() {
+    var ox1, oy1, dx1, dy1, ox2, oy2, dx2, dy2, t1
+    for (t1 = 0; t1 < 0.9; t1 += 0.1) {
+        ox1 = leftmargin
+        oy1 = bottommargin
+        dx1 = rightmargin
+        dy1 = (1 - t1) * bottommargin + (t1 * topmargin)
+        ox2 = leftmargin
+        oy2 = bottommargin
+        dx2 = rightmargin
+        dy2 = (1 - (t1+0.05)) * bottommargin + ((t1+0.05) * topmargin)
+        line(ox1, oy1, dx1, dy1)
+        line(ox2, oy2, dx2, dy2)
+    }
+    for (t1 = 0; t1 < 1; t1 += 0.1) {
+        ox1 = leftmargin
+        oy1 = bottommargin
+        dx1 = (1 - t1) * leftmargin + (t1 * rightmargin)
+        dy1 = topmargin
+        line(ox1, oy1, dx1, dy1)
+    }
+    //line(leftmargin,bottommargin,rightmargin,topmargin)
+    ellipse(rightmargin,topmargin,7,7)
+}
 
-function colorie(x1,y1, x2,y2,x3,y3,x4,y4){
-    var ox,oy,dx,dy
+function section(x1, y1, x2, y2, x3, y3){
+    
+}
+
+
+function colorie(x1, y1, x2, y2, x3, y3, x4, y4) {
+    var ox, oy, dx, dy
     push()
     var red = random(); alea.push(red)
-    if(red<0.1){stroke(0,100,100)}
-    for(var t1 = 0;t1<1;t1+=0.01){
+    if (red < 0.1) { stroke(0, 100, 100) }
+    for (var t1 = 0; t1 < 1; t1 += 0.01) {
         ox = (1 - t1) * x1 + (t1 * x2)
         oy = (1 - t1) * y1 + (t1 * y2)
         dx = (1 - t1) * x4 + (t1 * x3)
         dy = (1 - t1) * y4 + (t1 * y3)
-        line(ox,oy,dx,dy)
+        line(ox, oy, dx, dy)
     }
     pop()
 }
 
 
-function sign(){
-    var title="[summer005]"
-    var credits="al.my.re :: July 2024 (p5.js ~ CamBam Stick ~ gcode ~ juicy ~ " + alea.length + " random numbers)"
+function sign() {
+    var title = "[summer005]"
+    var credits = "al.my.re :: July 2024 (p5.js ~ CamBam Stick ~ gcode ~ juicy ~ " + alea.length + " random numbers)"
     textFont(font)
     textSize(fSize)
     push()
-    translate(rightmargin,bottommargin); 
+    translate(rightmargin, bottommargin);
     rotate(radians(270))
-    showcredits(0,fSize, title, credits)
+    showcredits(0, fSize, title, credits)
     pop()
 }
