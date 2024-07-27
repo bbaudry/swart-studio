24a870033402c6f7994415190e94bc3f40bda334
bbaudry <baudry@kth.se>
2024-07-27 15:50:16 -0400

    ++*+><+*++

---
+++ p5-experiments/plottable/etemontreal002-core.js
@@ -0,0 +1,56 @@
+var mainhu = 270
+var rarehu = 90
+
+function cartepostale(x, y) {
+    noFill()
+    setmargins(x, y)
+    stroke(mainhu, 100, 100)
+    alea = []
+    grille()
+    rect(x, y, wpostcard, hpostcard)
+    signature()
+}
+
+function grille(){
+    var x,y,stepx,stepy,cx,cy,rad,grain
+    grain=5
+    stepx = floor(actualwidth/grain)
+    stepy = floor(actualheight/grain)
+    for(x=leftmargin;x<grain*stepx;x+=stepx){
+        for(y=topmargin;y<grain*stepy;y+=stepy){
+            //rect(x,y,stepx,stepy)
+            cx=random(stepx);alea.push(cx)
+            cy=random(stepx);alea.push(cy);
+            rad=random(stepx*0.1);alea.push(rad)
+            ellipse(x+cx,y+cy,rad,rad)
+        }
+    }
+}
+
+function colorie(x1, y1, x2, y2, x3, y3, x4, y4) {
+    var ox, oy, dx, dy
+    push()
+    var red = random(); alea.push(red)
+    if (red < 0.42) { stroke(rarehu, 100, 100) }
+    for (var t1 = 0; t1 < 1; t1 += 0.05) {
+        ox = (1 - t1) * x1 + (t1 * x2)
+        oy = (1 - t1) * y1 + (t1 * y2)
+        dx = (1 - t1) * x4 + (t1 * x3)
+        dy = (1 - t1) * y4 + (t1 * y3)
+        line(ox, oy, dx, dy)
+    }
+    pop()
+}
+
+
+function signature() {
+    var title = "~ été à montréal #2  ~"
+    var credits =  "al.my.re :: July 2024 (p5.js ~ CamBam Stick ~ vpype ~ juicy ~ gcode ~ " + alea.length + " random numbers)"
+    textFont(font)
+    textSize(fSize)
+    push()
+    translate(rightmargin, bottommargin);
+    rotate(radians(270))
+    showcredits(0, fSize, title, credits)
+    pop()
+}
