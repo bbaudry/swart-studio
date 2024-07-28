8c39aaf44655b4bea40e862aa05168fe84695e73
bbaudry <baudry@kth.se>
2024-07-28 18:39:18 -0400

    }]}°><°{[{

--- p5-experiments/plottable/etemontreal002-core.js
+++ p5-experiments/plottable/etemontreal002-core.js
@@ -5,10 +5,10 @@ function cartepostale(x, y) {
     noFill()
     setmargins(x, y)
     alea = []
-    stroke(mainhu, 100, 100)
+    stroke(0,0,0)//stroke(mainhu, 100, 100)
     grille()
-    stroke(rarehu, 100, 100)
-    grille()
+    stroke(0,0,0)//stroke(rarehu, 100, 100)
+    //grille()
     rect(x, y, wpostcard, hpostcard)
     signature()
 }
@@ -18,9 +18,8 @@ var xoff = 0.0
 var xinc = 0.1
 function grille() {
     grid = []
-    var x, y, i, j, stepx, stepy, cx, cy, rad, grain, p1, p2
-    grain = 42
-    rad = 5
+    var x, y, i, j, stepx, stepy, cx, cy, grain
+    grain = 31
     stepx = floor(actualwidth / grain)
     stepy = floor(actualheight / grain)
     for (i = 0; i < grain; i++) {
@@ -28,27 +27,19 @@ function grille() {
             cx = leftmargin + (i * stepx) + noise(xoff) * stepx; alea.push(cx); xoff += xinc
             cy = topmargin + (j * stepy) + noise(xoff) * stepy; alea.push(cy); xoff += xinc
             grid.push(createVector(cx, cy))
-            //ellipse(cx, cy, rad, rad)
-            cx = x + random(stepx)
-            cy = y + random(stepy)
-            //ellipse(cx,cy,rad,rad)
         }
     }
-    var red = true
     for (i = 0; i < grid.length - 1; i++) {
-        //if (i % grain != grain - 1) { line(grid[i].x, grid[i].y, grid[i + 1].x, grid[i + 1].y) }
-        if (i < grid.length - grain && i % grain != grain - 1 && random()<0.42) {
-            if(random()>0.21){
+        if (i < grid.length - grain && i % grain != grain - 1 && random() < 0.42) {
             quad(grid[i].x, grid[i].y,
                 grid[i + grain].x, grid[i + grain].y,
                 grid[i + grain + 1].x, grid[i + grain + 1].y,
                 grid[i + 1].x, grid[i + 1].y)
-            }
-            else{
+            if (random() < 0.21) {
                 colorie(grid[i].x, grid[i].y,
                     grid[i + grain].x, grid[i + grain].y,
                     grid[i + grain + 1].x, grid[i + grain + 1].y,
-                    grid[i + 1].x, grid[i + 1].y)    
+                    grid[i + 1].x, grid[i + 1].y)
             }
         }
     }
@@ -58,7 +49,7 @@ function colorie(x1, y1, x2, y2, x3, y3, x4, y4) {
     var ox, oy, dx, dy
     push()
     var red = random(); alea.push(red)
-    if (red < 0.42) { stroke(rarehu, 100, 100) }
+    if (red < 0.1) { stroke(rarehu, 100, 100) }
     for (var t1 = 0; t1 < 1; t1 += 0.25) {
         ox = (1 - t1) * x1 + (t1 * x2)
         oy = (1 - t1) * y1 + (t1 * y2)
