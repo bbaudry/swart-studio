99074f5f9a806ea73756fca5df4e83881406e14e
bbaudry <baudry@kth.se>
2024-07-28 18:34:26 -0400

    -[+.><.+]-

--- p5-experiments/plottable/etemontreal002-core.js
+++ p5-experiments/plottable/etemontreal002-core.js
@@ -1,44 +1,56 @@
-var mainhu = 270
-var rarehu = 90
+var mainhu = 0
+var rarehu = 270
 
 function cartepostale(x, y) {
     noFill()
     setmargins(x, y)
-    stroke(mainhu, 100, 100)
     alea = []
+    stroke(mainhu, 100, 100)
+    grille()
+    stroke(rarehu, 100, 100)
     grille()
     rect(x, y, wpostcard, hpostcard)
     signature()
 }
 
-var grid=[]
-var xoff=0.0
-var xinc=0.1
-function grille(){
-    var x,y,stepx,stepy,cx,cy,rad,grain,p1,p2
-    grain=11
-    rad=5
-    stepx = floor(actualwidth/grain)
-    stepy = floor(actualheight/grain)
-    for(x=leftmargin;x<grain*stepx;x+=stepx){
-        for(y=topmargin;y<grain*stepy;y+=stepy){
-            stroke(mainhu,100,100)
-            cx=x+noise(xoff)*stepx;alea.push(cx);xoff+=xinc
-            cy=y+noise(xoff)*stepy;alea.push(cy);xoff+=xinc
-            grid.push(createVector(cx,cy))
-            ellipse(cx,cy,rad,rad)
-            stroke(rarehu,100,100)
-            cx=x+random(stepx)
-            cy=y+random(stepy)
+var grid = []
+var xoff = 0.0
+var xinc = 0.1
+function grille() {
+    grid = []
+    var x, y, i, j, stepx, stepy, cx, cy, rad, grain, p1, p2
+    grain = 42
+    rad = 5
+    stepx = floor(actualwidth / grain)
+    stepy = floor(actualheight / grain)
+    for (i = 0; i < grain; i++) {
+        for (j = 0; j < grain; j++) {
+            cx = leftmargin + (i * stepx) + noise(xoff) * stepx; alea.push(cx); xoff += xinc
+            cy = topmargin + (j * stepy) + noise(xoff) * stepy; alea.push(cy); xoff += xinc
+            grid.push(createVector(cx, cy))
+            //ellipse(cx, cy, rad, rad)
+            cx = x + random(stepx)
+            cy = y + random(stepy)
             //ellipse(cx,cy,rad,rad)
         }
     }
-    rad=3
-    for(i=0;i<grid.length;i++){
-        p1=grid[i]
-        p2=grid[floor(random(grid.length))]
-        //ellipse(p1.x,p1.y,rad,rad)
-        //line(p1.x,p1.y,p2.x,p2.y)
+    var red = true
+    for (i = 0; i < grid.length - 1; i++) {
+        //if (i % grain != grain - 1) { line(grid[i].x, grid[i].y, grid[i + 1].x, grid[i + 1].y) }
+        if (i < grid.length - grain && i % grain != grain - 1 && random()<0.42) {
+            if(random()>0.21){
+            quad(grid[i].x, grid[i].y,
+                grid[i + grain].x, grid[i + grain].y,
+                grid[i + grain + 1].x, grid[i + grain + 1].y,
+                grid[i + 1].x, grid[i + 1].y)
+            }
+            else{
+                colorie(grid[i].x, grid[i].y,
+                    grid[i + grain].x, grid[i + grain].y,
+                    grid[i + grain + 1].x, grid[i + grain + 1].y,
+                    grid[i + 1].x, grid[i + 1].y)    
+            }
+        }
     }
 }
 
@@ -47,7 +59,7 @@ function colorie(x1, y1, x2, y2, x3, y3, x4, y4) {
     push()
     var red = random(); alea.push(red)
     if (red < 0.42) { stroke(rarehu, 100, 100) }
-    for (var t1 = 0; t1 < 1; t1 += 0.05) {
+    for (var t1 = 0; t1 < 1; t1 += 0.25) {
         ox = (1 - t1) * x1 + (t1 * x2)
         oy = (1 - t1) * y1 + (t1 * y2)
         dx = (1 - t1) * x4 + (t1 * x3)
@@ -60,7 +72,7 @@ function colorie(x1, y1, x2, y2, x3, y3, x4, y4) {
 
 function signature() {
     var title = "~ été à montréal #2  ~"
-    var credits =  "al.my.re :: July 2024 (p5.js ~ CamBam Stick ~ vpype ~ juicy ~ gcode ~ " + alea.length + " random numbers)"
+    var credits = "al.my.re :: July 2024 (p5.js ~ CamBam Stick ~ vpype ~ juicy ~ gcode ~ " + alea.length + " random numbers)"
     textFont(font)
     textSize(fSize)
     push()
