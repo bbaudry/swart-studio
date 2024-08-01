c1a6126f85cad00679c33b52383b73d954b40ebb
bbaudry <baudry@kth.se>
2024-07-31 22:12:50 -0400

    ]}.*><*.{[

--- p5-experiments/plottable/etemontreal003-core.js
+++ p5-experiments/plottable/etemontreal003-core.js
@@ -13,72 +13,36 @@ function cartepostale(x, y) {
 }
 
 
-function pei() {
-    var x, y,cx,cy
-    cx=leftmargin+actualwidth*0.5
-    cy = topmargin + actualheight * 0.5
-
-/*     for (var i = 0; i < 90; i += 5) {
-        x = leftmargin + sin(radians(i)) * actualwidth * 0.5
-        for (var j = 0; j < 90; j += 1) {
-            y = topmargin + cos(radians(j)) * actualheight * 0.5
-            ellipse(x, y, 2, 2)
-            y = bottommargin - sin(radians(j+random(-2/(j+1),2/(j+1)))) * actualheight * 0.5
-            ellipse(x, y, 2, 2)
-        }
-        x = rightmargin - sin(radians(i)) * actualwidth * 0.5
-        for (var j = 0; j < 90; j += 10) {
-            y = topmargin + cos(radians(j)) * actualheight * 0.5
-            ellipse(x, y, 2, 2)
-            y = bottommargin - sin(radians(j)) * actualheight * 0.5
-            ellipse(x, y, 2, 2)
-        }
-    }
- */
-    textFont(font)
-    textSize(fSize)
-
-    ellipse(cx,cy,7,7)
-    ellipse(cx+actualwidth*0.5,cy,7,7)
-    for(var a=180;a<360;a+=3){
-        stroke(a,100,100)
-        x=cx+cos(radians(a))*actualwidth*0.5
-        line(x,cy-42,x,cy+42)
-    }
-}
-
 var lesBlocs = []
-var xoff = 0.0
-var xinc = 0.001
-function montreal() {
-    lesBlocs = []
-    var i, j, blocSud, blocOuest, cx, cy, grain
-    grain = 11
-    blocSud = floor((actualwidth * 0.5) / grain)
-    blocOuest = floor((actualheight * 0.5) / grain)
-    for (i = 0; i < grain; i++) {
-        for (j = 0; j < grain; j++) {
-            cx = leftmargin + i * (i / grain * blocSud)
-            cy = topmargin + j * (j / grain * blocOuest)
-            lesBlocs.push(createVector(cx, cy))
-            cx = rightmargin - (i * blocSud)
-            cy = topmargin + (j * blocOuest)
-            lesBlocs.push(createVector(cx, cy))
-            cx = rightmargin - (grain / i * blocSud)
-            cy = bottommargin - (grain / j * blocOuest)
-            //lesBlocs.push(createVector(cx, cy))
-            cx = leftmargin + (grain / i * blocSud)
-            cy = bottommargin - (grain / j * blocOuest)
-            //lesBlocs.push(createVector(cx, cy))
+function pei() {
+    var x, y, cx, cy, count, density
+    cx = leftmargin + actualwidth * 0.5
+    cy = topmargin + actualheight * 0.5
+    density=floor(random(3,9))
+    for (var a = 180; a < 360; a += density) {
+        x = cx + cos(radians(a + random(-a / 90, a / 90))) * actualwidth * 0.5
+        count = 0
+        for (var b = 270; b < 450; b += density) {
+            y = cy + sin(radians(b + random(-b / 360, b / 360))) * actualheight * 0.5
+            //ellipse(x, y, 3, 3)
+            lesBlocs.push(createVector(x, y))
+            count++
         }
     }
-    var offset = 2
-    for (i = 0; i < lesBlocs.length - offset; i++) {
-        if (i < lesBlocs.length - (grain * offset) && i % (grain * offset) != (grain - 1) * offset && i % (grain * offset) != grain * offset - 1) {
-            quad(lesBlocs[i].x, lesBlocs[i].y,
-                lesBlocs[i + offset * grain].x, lesBlocs[i + offset * grain].y,
-                lesBlocs[i + offset * (grain + 1)].x, lesBlocs[i + offset * (grain + 1)].y,
-                lesBlocs[i + offset].x, lesBlocs[i + offset].y)
+    for (i = 0; i < lesBlocs.length; i++) {
+        if (i < lesBlocs.length - count && i % count != count - 1 && random()<0.42) {
+            if (random() < 0.1) {
+                colorie(lesBlocs[i].x, lesBlocs[i].y,
+                    lesBlocs[i + count].x, lesBlocs[i + count].y,
+                    lesBlocs[i + count + 1].x, lesBlocs[i + count + 1].y,
+                    lesBlocs[i + 1].x, lesBlocs[i + 1].y)
+            }
+            else{
+                quad(lesBlocs[i].x, lesBlocs[i].y,
+                    lesBlocs[i + count].x, lesBlocs[i + count].y,
+                    lesBlocs[i + count + 1].x, lesBlocs[i + count + 1].y,
+                    lesBlocs[i + 1].x, lesBlocs[i + 1].y)    
+            }
         }
     }
 }
@@ -87,7 +51,7 @@ function colorie(x1, y1, x2, y2, x3, y3, x4, y4) {
     var ox, oy, dx, dy
     push()
     var red = random(); alea.push(red)
-    if (red < 0.1) { stroke(rarehu, 100, 100) }
+    if (red < 0.42) { stroke(rarehu, 100, 100) }
     quad(x1, y1, x2, y2, x3, y3, x4, y4)
     for (var t1 = 0; t1 < 1; t1 += 0.1) {
         ox = (1 - t1) * x1 + (t1 * x2)
