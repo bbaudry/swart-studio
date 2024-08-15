24708d1f003504c304d9f438de51787cb2d234fb
bbaudry <baudry@kth.se>
2024-08-15 11:00:24 -0400

    **-°><°-**

--- p5-experiments/plottable/etemontreal004-core.js
+++ p5-experiments/plottable/etemontreal004-core.js
@@ -1,16 +1,26 @@
 
 function cartepostale(x, y) {
     setmargins(x, y)
+    stroke(230,100,100)
+    vera()
+    stroke(30,100,100)
     vera()
     signature()
 }
 
-function vera(){
-    var cx = leftmargin + actualwidth*random(0.42,0.57)
-    var cy = topmargin + actualheight*random(0.42,0.57)
-    var a = random(360)
-    ellipse(cx,cy,42,42)
-    var x = cx+21*cos(a)
-    var y = cy+21*sin(a)
-    ellipse(x,y,12,12)
+function vera() {
+    var stepx = actualwidth * 0.1
+    var stepy = actualheight * 0.1
+    var diam = stepx*0.91
+    for (var x = leftmargin; x < rightmargin; x += stepx) {
+        for (var y = topmargin; y < bottommargin; y += stepy) {
+            var cx = x + stepx * random(0.42, 0.57)
+            var cy = y + stepy * random(0.42, 0.57)
+            var a = random(360)
+            ellipse(cx, cy, diam, diam)
+            var dx = cx + 21 * cos(a)
+            var dy = cy + 21 * sin(a)
+            ellipse(dx, dy, 12, 12)
+        }
+    }
 }
