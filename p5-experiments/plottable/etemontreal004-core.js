52e3e296ceae5c6f9061943ec8a67a650f0e89b9
bbaudry <baudry@kth.se>
2024-08-15 18:19:12 -0400

    {*{]><[}*}

--- p5-experiments/plottable/etemontreal004-core.js
+++ p5-experiments/plottable/etemontreal004-core.js
@@ -9,17 +9,22 @@ function cartepostale(x, y) {
 }
 
 function vera() {
-    var stepx = actualwidth * 0.1
-    var stepy = actualheight * 0.1
-    var diam = stepx*0.91
-    for (var x = leftmargin; x < rightmargin; x += stepx) {
-        for (var y = topmargin; y < bottommargin; y += stepy) {
-            var cx = x + stepx * random(0.42, 0.57)
-            var cy = y + stepy * random(0.42, 0.57)
+    var nbSteps = 17
+    var stepx = floor(actualwidth /nbSteps)
+    var stepy = stepx//floor(actualheight / nbSteps)
+    var diam = floor(stepx*0.91)
+    var rayon = diam/2
+    var x,y,cx,cy,dx,dy,i,j
+    for (i=0;i<nbSteps;i++) {
+        for (j=0;j<nbSteps;j++) {
+            x = leftmargin + i*stepx
+            y = topmargin + j*stepy
+            cx = x + stepx * random(0.42, 0.57)
+            cy = y + stepy * random(0.42, 0.57)
             var a = random(360)
             ellipse(cx, cy, diam, diam)
-            var dx = cx + 21 * cos(a)
-            var dy = cy + 21 * sin(a)
+            dx = cx + rayon * cos(a)
+            dy = cy + rayon * sin(a)
             ellipse(dx, dy, 12, 12)
         }
     }
