9185c0a2f9b3971c7afe07d8c9bfb5a0e1bf8211
bbaudry <baudry@kth.se>
2024-10-09 12:35:11 -0700

    ]+].><.[+[

--- p5-experiments/plottable/phoenix001.js
+++ p5-experiments/plottable/phoenix001.js
@@ -25,28 +25,34 @@ function draw() {
     xinc=0.005
     stroke(0,0,100)
     rect(leftmargin,topmargin,actualwidth,actualheight)
-    grid()
-    drawgrid(0)
-    drawgrid(1)
-    drawgrid(2)
-    drawgrid(3)
+    initgrid()
+    showgrid()
+    onesection(0)
+    onesection(1)
+    onesection(2)
+    onesection(3)
     noLoop()
 }
 
 var grille=[]
-function grid(){
+function initgrid(){
     var x,y
     for(i=0;i<steps;i++){
         x=leftmargin+i*xstep+noise(xoff)*xstep;xoff+=xinc
         for(j=0;j<steps;j++){
             y=topmargin+j*ystep+random(xoff)*ystep;xoff+=xinc
             grille.push({x:x,y:y})
-    //        ellipse(x,y,5,5)
         }
     }
 }
 
-function drawgrid(off){
+function showgrid(){
+    for(var i=0; i<grille.length;i++){
+        ellipse(grille[i].x,grille[i].y,5,5)
+    }
+}
+
+function onesection(off){
     var x1,y1,x2,y2,x3,y3,x4,y4
     var i1,i2,i3,i4,j1,j2,j3,j4
     var rad=17
@@ -55,7 +61,7 @@ function drawgrid(off){
     x1=grille[i1+j1*steps].x
     y1=grille[i1+j1*steps].y
     //fill(0,100,100);ellipse(x1,y1,rad,rad)
-    j2=Math.floor(random(j1+1,steps-1))
+    j2=Math.floor(random(j1+1,steps))
     i2=off    
     x2=grille[i2+j2*steps].x
     y2=grille[i2+j2*steps].y
@@ -71,30 +77,20 @@ function drawgrid(off){
     y4=grille[i4+j4*steps].y
     //fill(210,100,100);ellipse(x4,y4,rad,rad)
     noFill()
-    stroke(0,0,100);quad(x1,y1,x2,y2,x3,y3,x4,y4)
+    stroke(0,0,100)
+    innerquad(x1,y1,x2,y2,x3,y3,x4,y4)
 }
-/* 
-    i1=Math.floor(random(steps-2));console.log("i1: "+i1)
-    j1=0//Math.floor(random(steps-2))    
-    x1=grille[i1+j1*steps].x
-    y1=grille[i1+j1*steps].y
-    fill(0,100,100);ellipse(x1,y1,rad,rad)
-    i2=i1//Math.floor(random(i1+1,steps-1));console.log("i2: "+i2)
-    j2=Math.floor(random(steps-2))    
-    x2=grille[i2+j2*steps].x
-    y2=grille[i2+j2*steps].y
-    fill(50,100,100);ellipse(x2,y2,rad,rad)
-    i3=Math.floor(random(i1+1,steps-1));console.log(i3)
-    j3=j2//Math.floor(random(j1+1,steps-1))    
-    x3=grille[i3+j3*steps].x
-    y3=grille[i3+j3*steps].y
-    //fill(180,100,100);ellipse(x3,y3,rad,rad)
-    i4=i1//Math.floor(random(steps-2));console.log(i4)
-    j4=Math.floor(random(j2+1,steps-1))    
-    x4=grille[i4+j4*steps].x
-    y4=grille[i4+j4*steps].y
-    //fill(210,100,100);ellipse(x4,y4,rad,rad)
-*/
+
+function innerquad(x1,y1,x2,y2,x3,y3,x4,y4){
+    quad(x1,y1,x2,y2,x3,y3,x4,y4)
+    var offset = 7
+    quad(x1+offset,y1+offset,
+        x2-offset,y2+offset,
+        x3-offset,y3-offset,
+        x4+offset,y4-offset)
+
+}
+
 function showcredits(posx, posy) {
     textFont(bowie)
     textSize(fSize);
