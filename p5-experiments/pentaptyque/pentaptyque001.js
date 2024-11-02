84cdfbe289836fadd8d872381ef2d8fe33d3d3c6
bbaudry <baudry@kth.se>
2024-11-02 11:56:14 -0400

    }[-]><[-]{

---
+++ p5-experiments/pentaptyque/pentaptyque001.js
@@ -0,0 +1,21 @@
+function savesvg() {
+    save("pentaptyque001.svg");
+}
+
+function savepng() {
+    save("pentaptyque001.png");
+}
+
+function setup() {
+    getsvg()
+    //getpng()
+    centerCanvas();
+    colorMode(HSB, 360, 100, 100, 250);
+    strokeCap(SQUARE)
+    noFill()
+    maxcount = random(39, 45)
+}
+
+function draw(){
+    rect(0,0,w,totalheight)
+}
