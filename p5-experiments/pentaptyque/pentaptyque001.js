87c7e79c5f1adc25339e8b2d32cd7d08ab0550e3
bbaudry <baudry@kth.se>
2024-11-02 12:22:48 -0400

    .}°]><[°{.

--- p5-experiments/pentaptyque/pentaptyque001.js
+++ p5-experiments/pentaptyque/pentaptyque001.js
@@ -18,7 +18,10 @@ function setup() {
 
 function draw(){
     drawmask()
-    drawart()
+    for(var i=0;i<3;i++){
+        drawart()
+    }
+    noLoop()
 }
 
 function drawmask(){
@@ -33,6 +36,16 @@ function drawart(){
     for(var i=0; i<5; i++){
         push()
         translate(padding+leftmargin,(i+1)*padding+i*h+topmargin)
+        oneptyque()
         pop()
     }
+}
+
+function oneptyque(){
+    var cx=random(0.1*actualwidth,0.9*actualwidth)
+    var cy=random(0.1*actualheight,0.9*actualheight)
+    triangle(0,0,cx,cy,0,actualheight)
+    triangle(0,0,actualwidth,0,cx,cy)
+    triangle(actualwidth,0,actualwidth,actualheight,cx,cy)
+    triangle(actualwidth,actualheight,0,actualheight,cx,cy)
 }
