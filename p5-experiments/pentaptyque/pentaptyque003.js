cc5db46126a2cafb7e4f2bef31dafc574fd1c9c2
bbaudry <baudry@kth.se>
2024-11-10 17:32:32 -0500

    ]{-°><°-}[

--- p5-experiments/pentaptyque/pentaptyque003.js
+++ p5-experiments/pentaptyque/pentaptyque003.js
@@ -19,7 +19,7 @@ function setup() {
 
 }
 
-var res = 11 //knob: density of the field
+var res = 13 //knob: density of the field
 //knob: speed to navigate noise. smallest, smoother angle changes
 var steplength = 2*res//knob: length of each curve 
 var strw = 2 //knob: weight of each curve
@@ -68,19 +68,55 @@ function drawart(){
         initfield(noiseres)
         push()
         translate(padding+leftmargin,(i+1)*padding+i*h+topmargin)
-        onetyp()
+        //onetyp()
+        drawcurveinfield(Math.floor(nbrows/2),3, 11)
         pop()
         noiseres+=0.1
     }
 }
 
 function onetyp(){
+    var a,r,x1,y1,x2,y2
     for (let y = 0; y < nbrows; y++) {
         for (let x = 0; x < nbcols; x++) {
-            var r = field[y][x]
+            a = field[y][x]
+            r = res
             push()
-            rect((x*res),(y*res),4*r,4*r)
+            //rect((x*res),(y*res),4*r,4*r)
+            x1=x*res
+            y1=y*res
+            x2=x1+r*cos(a)
+            y2=y1+r*sin(a)
+            line(x1,y1,x2,y2)
             pop()
         }
     }
+}
+
+function drawcurveinfield(row, col, len) {
+    strokeWeight(strw)
+    stroke(0, 100, 100, 42)
+    noFill()
+    beginShape()
+    x1 = col * res
+    y1 = row * res
+    console.log("row: " + row + "; col: " + col + "; x1: "+x1+" ; y1: "+y1+"; nbrow: " + nbrows + "; nbcols: " + nbcols)
+    curveVertex(x1, y1)
+    curveVertex(x1, y1)
+    for (let i = 0; i < len; i++) {
+        console.log(i+"; row: " + row + "; col: " + col + "; x1: "+x1+" ; y1: "+y1)
+        angle = field[row][col]
+        x2 = x1 + steplength * cos(angle)
+        y2 = y1 + steplength * sin(angle)
+        curveVertex(x2, y2)
+        if (x2 > w || x2<0) { i = len }
+        if (y2 > h || y2<0) { i = len }
+        col = Math.floor(x2 / res)//if(x2==0){col=0}else{}
+        row = Math.floor(y2 / res)//if(y2==0){row=0}else{}
+        x1 = x2
+        y1 = y2
+    }
+    curveVertex(x2, y2)
+    curveVertex(x2, y2)
+    endShape()
 }
