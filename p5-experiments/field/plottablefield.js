7bcc1ee5930492575cdcf50e68562acf3f10d117
bbaudry <baudry@kth.se>
2024-11-21 15:04:56 -0500

    {++*><*++}

--- p5-experiments/field/plottablefield.js
+++ p5-experiments/field/plottablefield.js
@@ -8,11 +8,11 @@
 var echelle = 1
 var w = 1122 * echelle
 var h = 1587 * echelle
-var padding = 7
-var rightmargin = 0.97 * w
-var leftmargin = 0.03 * w
-var topmargin = 0.02 * h
-var bottommargin = 0.98 * h
+var padding = 0
+var rightmargin = 0.95 * w
+var leftmargin = 0.05 * w
+var topmargin = 0.05 * h
+var bottommargin = 0.95 * h
 var actualwidth = rightmargin - leftmargin
 var actualheight = bottommargin - topmargin
 var totalheight = padding * 2 + h 
