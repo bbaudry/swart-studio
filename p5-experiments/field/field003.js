c61d3f59abfee0a2e223f4070d8be45e5a4cbf77
bbaudry <baudry@kth.se>
2024-11-30 17:45:01 -0500

    ]}+*><*+{[

--- p5-experiments/field/field003.js
+++ p5-experiments/field/field003.js
@@ -1,9 +1,11 @@
-var font, posx, posy, knobs = []
-var fSize = 15
 function preload() {
     font = loadFont("../fonts/1CAMBam_Stick_9.ttf");
     sourcecode = loadStrings('field003.js');
 }
+
+var font
+var fSize = 15
+
 function savesvg() {
     save("field003.svg");
 }
@@ -26,7 +28,7 @@ function setup() {
 
 }
 
-var res = 7 //knob: density of the field
+var res = 3 //knob: density of the field
 //knob: speed to navigate noise. smallest, smoother angle changes
 var steplength = 3 * res//knob: length of each curve 
 var strw = 2 //knob: weight of each curve
@@ -47,7 +49,7 @@ function initfield(noiseres) {
         for (let x = 0; x < nbcols; x++) {
             xoff += noiseres
             let ikeda = noise(xoff, yoff)
-            angle = map(ikeda, 0.0, 1.0, 0, PI)
+            angle = map(ikeda, 0.0, 1.0, 0, 2*PI)
             row.push(angle)
         }
         field.push(row)
@@ -65,36 +67,37 @@ function draw() {
 }
 
 function drawart() {
-    for (var j = 0; j < 1; j++) {
-        initfield(0.05)
+    for (var j = 0; j < 52; j++) {
+        initfield(0.02)
         oneloop(j)
         //noiseres+=0.001
     }
 }
 
 function oneloop(j){
-    var initx, inity, x1, y1, x2, y2, len, angle, rad, iter
-    len = res
-    rad=1
+    var initx, inity, x1, y1, x2, y2, len, angle, rad, iter, hu
+    len = res*3
+    rad=random()
     iter=42
     initx=Math.floor(nbcols/2)//Math.floor(random(nbcols-1))
-    inity=nbrows-1//Math.floor(random(nbrows-1))
+    inity=Math.floor(nbrows/2)//Math.floor(random(nbrows-1))
     x1 = leftmargin + initx * res
     y1 = topmargin + inity * res
-    stroke(200,100,100)
+    hu=random([200,300,100])
+    stroke(hu,100,100)
     beginShape()
     for (var i = 1; i < iter; i++) {
         console.log("nbrows: "+nbrows+"; field rows: "+field.length)
         angle = field[nbrows - i][initx]
-        x2 = x1 + len * cos(angle+ PI)
-        y2 = y1 + len * sin(angle+ PI)
+        x2 = x1 + len * cos(angle+ PI*rad)
+        y2 = y1 + len * sin(angle+ PI*rad)
         vertex(x2, y2)
         x1 = x2; y1 = y2;
     }
     for (var i = 1; i < iter; i++) {
         angle = field[nbrows - i][initx]
-        x2 = x1 + len * cos(angle)
-        y2 = y1 + len * sin(angle)
+        x2 = x1 + len * cos(angle- PI*(1-rad))
+        y2 = y1 + len * sin(angle- PI*(1-rad))
         vertex(x2, y2)
         x1 = x2; y1 = y2;
     }
