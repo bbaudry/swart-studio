354a1ec215036f2e6907379bafac73b86adf0e9c
bbaudry <baudry@kth.se>
2024-12-01 16:09:42 -0500

    *.{.><.}.*

--- p5-experiments/field/field003.js
+++ p5-experiments/field/field003.js
@@ -20,28 +20,22 @@ function setup() {
     centerCanvas();
     colorMode(HSB, 360, 100, 100, 250);
     strokeCap(SQUARE)
+    background(0,0,0)
     noFill()
-    initfield()
-    maxcount = random(39, 45)
-    yoff = 0.0
-    noiseres = 0.007
-
 }
 
 var res = 3 //knob: density of the field
 //knob: speed to navigate noise. smallest, smoother angle changes
-var steplength = 3 * res//knob: length of each curve 
-var strw = 2 //knob: weight of each curve
 var field = []
 var nbcols = Math.floor(actualwidth / res) + 1
 var nbrows = Math.floor(actualheight / res) + 1
 var xoff, yoff
-var noiseres
 
 
 function initfield(noiseres) {
     let angle, row
     field = []
+    yoff = 0.0
     for (let y = 0; y < nbrows; y++) {
         row = []
         yoff += noiseres
@@ -57,33 +51,32 @@ function initfield(noiseres) {
 }
 
 function draw() {
-    //drawframe()
+    drawframe()
     drawart()
     stroke(0, 0, 0)
     //drawvecs()
-    fill(0, 0, 0); stroke(0, 0, 0)
+    fill(0, 0, 100); stroke(0, 0, 100)
     showcredits(leftmargin, bottommargin * 1.06, "al.my.re :: p5.js :: CamBam Stick [field 003). November 2024]")
     noLoop()
 }
 
 function drawart() {
+    initfield(0.02)
     for (var j = 0; j < 52; j++) {
-        initfield(0.02)
-        oneloop(j)
-        //noiseres+=0.001
+        oneloop()
     }
 }
 
-function oneloop(j){
+function oneloop(){
     var initx, inity, x1, y1, x2, y2, len, angle, rad, iter, hu
     len = res*3
-    rad=random()
+    rad=0.5
     iter=42
-    initx=Math.floor(nbcols/2)//Math.floor(random(nbcols-1))
-    inity=Math.floor(nbrows/2)//Math.floor(random(nbrows-1))
-    x1 = leftmargin + initx * res
+    initx=Math.floor(random(nbcols))
+    inity=Math.floor(nbrows*0.7)
+    x1 = leftmargin + Math.floor(nbcols/2) * res
     y1 = topmargin + inity * res
-    hu=random([200,300,100])
+    hu=random([200,300,100,50])
     stroke(hu,100,100)
     beginShape()
     for (var i = 1; i < iter; i++) {
