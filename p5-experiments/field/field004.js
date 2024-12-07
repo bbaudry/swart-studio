ced2f7535d298fb6d6ea684cff4cd246b6e6655f
bbaudry <baudry@kth.se>
2024-12-07 16:32:17 -0500

    +.}{><}{.+

--- p5-experiments/field/field004.js
+++ p5-experiments/field/field004.js
@@ -58,10 +58,10 @@ function initgrid(noiseres) {
 }
 
 function draw() {
-    drawframe()
+    background(0,0,100)
     initgrid(0.1)
     drawart()
-    fill(0, 0, 100); stroke(0, 0, 100)
+    fill(0, 0, 0); stroke(0, 0, 0)
     showcredits(leftmargin, bottommargin * 1.06, "al.my.re :: p5.js :: CamBam Stick [field 003). November 2024]")
     noLoop()
 }
@@ -74,6 +74,8 @@ function drawart() {
         if(random()<0.8){eye(s)}
         else{eye2(s)}
     }
+    strokeWeight(3)
+    drawflower()
 }
 
 function eye(s) {
@@ -116,4 +118,68 @@ function eye2(s){
         dy = (1 - t) * y3 + (t * y4);
         line(ox, oy, dx, dy)
     }
+}
+
+var res = 4 //knob: density of the field
+//knob: speed to navigate noise. smallest, smoother angle changes
+var fieldflower = []
+var nbcols = Math.floor(actualwidth / res) + 1
+var nbrows = Math.floor(actualheight / res) + 1
+var xoff, yoff
+
+
+function initfieldflower(noiseres) {
+    let angle, row
+    fieldflower = []
+    yoff = 0.0
+    for (let y = 0; y < nbrows; y++) {
+        row = []
+        yoff += noiseres
+        xoff = 0.0;
+        for (let x = 0; x < nbcols; x++) {
+            xoff += noiseres
+            let ikeda = noise(xoff, yoff)
+            angle = map(ikeda, 0.0, 1.0, 0, 2*PI)
+            row.push(angle)
+        }
+        fieldflower.push(row)
+    }
+}
+
+
+function drawflower() {
+    initfieldflower(0.02)
+    for (var j = 0; j < 7; j++) {
+        oneloop()
+    }
+}
+
+function oneloop(){
+    var initx, inity, x1, y1, x2, y2, len, angle, rad, iter, hu
+    len = res *3
+    rad=0.5
+    iter=37
+    initx=Math.floor(random(nbcols))
+    inity=Math.floor(nbrows*0.7)
+    x1 = leftmargin + Math.floor(nbcols/2) * res
+    y1 = topmargin + inity * res
+    hu=random([50])
+    stroke(hu,100,100)
+    beginShape()
+    for (var i = 1; i < iter; i++) {
+        console.log("nbrows: "+nbrows+"; field rows: "+field.length)
+        angle = fieldflower[nbrows - i][initx]
+        x2 = x1 + len * cos(angle+ PI*rad)
+        y2 = y1 + len * sin(angle+ PI*rad)
+        vertex(x2, y2)
+        x1 = x2; y1 = y2;
+    }
+    for (var i = 1; i < iter; i++) {
+        angle = fieldflower[nbrows - i][initx]
+        x2 = x1 + len * cos(angle- PI*(1-rad))
+        y2 = y1 + len * sin(angle- PI*(1-rad))
+        vertex(x2, y2)
+        x1 = x2; y1 = y2;
+    }
+    endShape(CLOSE)
 }
