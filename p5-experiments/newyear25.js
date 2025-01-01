4eecc7e176154cd805ce76e474070d18ffc3c6d4
bbaudry <baudry@kth.se>
2024-12-31 20:28:45 -0500

    -*}*><*{*-

--- p5-experiments/newyear25.js
+++ p5-experiments/newyear25.js
@@ -1,5 +1,5 @@
 function preload() {
-    font = loadFont("./fonts/FreeMonoBold.ttf");
+    font = loadFont("./fonts/ChunkFive-Regular.otf");
     fileName = "newyear25"
     sourcecode = loadStrings(fileName + '.js');
 }
@@ -12,28 +12,60 @@ function setup() {
     strokeCap(SQUARE)
     background(0, 0, 100)
     noFill()
+    setTypo()
 }
 
 var font
-var fSize = 111
+var fSize = 129
 
 function draw() {
-    background(0, 0, 100)
-    stroke(0,0,0)
+    //background(0, 0, 100)
+    stroke(0, 0, 0)
     noFill()
     //rect(leftmargin, topmargin, actualwidth, actualheight)
-    push()
-    translate(w,0)
+/*    push()
+    translate(w, 0)
     rotate(radians(90))
     stroke(0, 0, 0)
     rect(topmargin, leftmargin, actualheight, actualwidth)
 
     fill(0, 0, 0)
     textFont(font)
-    textSize(fSize)
-    text("11111101001", topmargin, leftmargin + fSize*2.5)
+    textSize(fSize)*/
+    //text("11111101001", topmargin, leftmargin + fSize * 2.5)
+    let ikeda = digits[Math.floor(random() * 3)]
+    let ryoji = ikeda[Math.floor(random() * ikeda.length)]
+    let vera = random() * 5
+    let molnar = exp(random() * 5)
+    let x1 = ryoji.x + molnar * cos(ryoji.alpha)
+    let y1 = ryoji.y + molnar * sin(ryoji.alpha)
+    ellipse(x1, y1, vera, vera)
 
-    pop()
-    noLoop()
+
+    //pop()
+    //noLoop()
 }
 
+var digits = []
+var onedigit, initialPixelDensity
+function setTypo() {
+    alpha = 10
+    initialPixelDensity = 0.42
+    fSize = 129
+    var y = topmargin + fSize
+    onedigit = font.textToPoints('1', leftmargin, y, fSize, {
+        sampleFactor: initialPixelDensity,
+        simplifyThreshold: 0
+    })
+    digits.push(onedigit)
+    onedigit1 = font.textToPoints('1', 100,y, fSize, {
+        sampleFactor: initialPixelDensity,
+        simplifyThreshold: 0
+    })
+    digits.push(onedigit1)
+    onedigit2 = font.textToPoints('1', 200, y, fSize, {
+        sampleFactor: initialPixelDensity,
+        simplifyThreshold: 0
+    })
+    digits.push(onedigit2)
+}
