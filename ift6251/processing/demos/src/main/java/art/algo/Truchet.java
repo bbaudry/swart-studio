62cfa656d8bf2907155d41fe829b08d8d2f62235
bbaudry <baudry@kth.se>
2025-01-12 11:09:23 -0500

    }°}[><]{°{

--- ift6251/processing/demos/src/main/java/art/algo/Truchet.java
+++ ift6251/processing/demos/src/main/java/art/algo/Truchet.java
@@ -7,7 +7,7 @@ public class Truchet extends PApplet {
     int w = 1000;
     int h = 1000;
     Random rand;
-    
+
     @Override
     public void settings() {
         size(w, h);
@@ -16,8 +16,8 @@ public class Truchet extends PApplet {
     @Override
     public void setup() {
         colorMode(HSB, 360, 100, 100);
-        colorMode(HSB,360,100,100);
-        background(0,0,0);
+        colorMode(HSB, 360, 100, 100);
+        background(0, 0, 0);
         strokeCap(SQUARE);
         rand = new Random();
     }
@@ -26,42 +26,44 @@ public class Truchet extends PApplet {
     public void draw() {
         int size = 100;
         noFill();
-        stroke(0,0,100);
-        strokeWeight((float)(size*0.1));
-        for(int x=size;x<w-size;x+=size){
-            for(int y=size;y<h-size;y+=size){
+        stroke(0, 0, 100);
+        strokeWeight((float) (size * 0.1));
+        for (int x = size; x < w - size; x += size) {
+            for (int y = size; y < h - size; y += size) {
                 int vera = rand.nextInt(0, 2);
                 switch (vera) {
                     case 0:
-                    tile2(x,y,size);
+                        tile2(x, y, size);
                         break;
                     case 1:
-                    tile1(x,y,size);
+                        tile1(x, y, size);
                         break;
-                
                     default:
                         break;
                 }
-                            
             }
         }
         noLoop();
     }
 
-    public void tile1(int x, int y, int size){
-        strokeWeight((float)(size*0.1));
-        arc(x,y,size,size,0,PI/2);
-        arc(x+size,y+size,size,size,PI,3*PI/2);
+    public void tile1(int x, int y, int size) {
+        strokeWeight((float) (size * 0.1));
+        arc(x, y, size, size, 0, PI / 2);
+        arc(x + size, y + size, size, size, PI, 3 * PI / 2);
     }
 
-    public void tile2(int x, int y, int size){
-        arc(x+size,y,size,size,PI/2,PI);
-        arc(x,y+size,size,size,3*PI/2,2*PI);
+    public void tile2(int x, int y, int size) {
+        arc(x + size, y, size, size, PI / 2, PI);
+        arc(x, y + size, size, size, 3 * PI / 2, 2 * PI);
     }
 
-        public static void main(String[] args) {
-            String[] processingArgs = { "Truchet" };
-            Truchet mySketch = new Truchet();
-            PApplet.runSketch(processingArgs, mySketch);
-        }
+    public void mouseClicked(){
+        exit();
+    }
+
+    public static void main(String[] args) {
+        String[] processingArgs = { "Truchet" };
+        Truchet mySketch = new Truchet();
+        PApplet.runSketch(processingArgs, mySketch);
+    }
 }
