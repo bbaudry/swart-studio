c518ca9eb1ebe9117e779483f4cf4aff0b610935
bbaudry <baudry@kth.se>
2025-01-27 22:20:25 -0500

    +[+°><°+]+

---
+++ p5-experiments/plein003.js
@@ -0,0 +1,34 @@
+
+var w, h, boites
+var cnv
+
+function setup() {
+    w = windowHeight
+    h = windowHeight
+    cnv = createCanvas(windowHeight, windowHeight);
+    centerCanvas();
+    colorMode(HSB, 360, 100, 100, 250);
+}
+
+
+function centerCanvas() {
+    var x = (windowWidth - windowHeight) / 2;
+    var y = (windowHeight - windowHeight) / 2;
+    cnv.position(x, y);
+}
+
+
+function draw() {
+    background(0, 0, 0)
+    noStroke()
+    ellipse(w*0.5,h*0.5,100,100)
+    beginShape();
+    vertex(ox1, oy1);
+    bezierVertex(
+        dx1 + ecartx * i * offx, dy1, 
+        dx2, dx2 * 1.2 + ecarty * i * offy, 
+        dx2, dy2);
+    endShape();
+
+    noLoop()
+}
