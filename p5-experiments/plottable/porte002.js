4958536aaee3be536a9afd509c94258353b10c20
bbaudry <baudry@kth.se>
2025-02-03 22:20:27 -0500

    }}*-><-*{{

--- p5-experiments/plottable/porte002.js
+++ p5-experiments/plottable/porte002.js
@@ -1,6 +1,6 @@
 var w, h, cnv, leftmargin, rightmargin, topmargin, bottommargin, actualwidth, actualheight, imgbtn, palette
 
-var palettes = [[0,90,180,270],[0,30,60,90,120,150,180],[180,210,240,270,300,330,30,60],[200,230,260,290,320,350]]
+var palettes = [[0, 90, 180, 270], [0, 30, 60, 90, 120, 150, 180], [180, 210, 240, 270, 300, 330, 30, 60], [200, 230, 260, 290, 320, 350]]
 function setup() {
     w = 800
     h = 800
@@ -16,7 +16,7 @@ function setup() {
     bottommargin = h - 16
     actualheight = bottommargin - topmargin
     colorMode(HSB, 360, 100, 100, 250);
-    palette=palettes[Math.floor(random(palettes.length))]
+    palette = palettes[Math.floor(random(palettes.length))]
     angleMode(DEGREES);
 }
 
@@ -28,51 +28,35 @@ function savepng() {
 }
 
 function draw() {
-    background(0, 50, 100)
+    background(0, 0, 100)
     stroke(300, 100, 100)
     noFill()
     rect(0, 0, w, h)
     stroke(300, 100, 100)
     sky()
-//    field()
+    field()
     noLoop()
 }
 
-function sky(){
-    var a1,a2,a3,a4,x1,y1,x2,y2,x3,y3,x4,y4,rad
-    rad=0.35*w
+function sky() {
+    var a1, a2, rad
+    rad = 0.35 * w
     push()
-    translate(w*0.5,h*0.5)
-    stroke(0,0,0);noFill()
-    ellipse(0,0,2*rad,2*rad)
-    a1=180*noise(xoff);xoff+=xinc
-    a2=181+178*noise(xoff);xoff+=xinc
-    x1=rad*cos(a1)
-    y1=rad*sin(a1)
-    x2=rad*cos(a2)
-    y2=rad*sin(a2)
-    line(x1,y1,x2,y2)
-    a3=random(a1,a1+(a2-a1))
-    a4=random(a2,a2+a1)
-    x3=rad*cos(a3)
-    y3=rad*sin(a3)
-    x4=rad*cos(a4)
-    y4=rad*sin(a4)
-    stroke(0,100,100)
-    line(x3,y3,x4,y4)
-    var p=intersect(x1,y1,x2,y2,x3,y3,x4,y4)
-    fill(200,100,100,100)
-    //triangle(x1,y1,p.x,p.y,x3,y3)
-    fill(270,100,100,100)
-    //triangle(x1,y1,p.x,p.y,x4,y4)
-    arc(0,0,rad*2,rad*2,a1,a3,CHORD)
-    arc(0,0,rad*2,rad*2,a3,a2,CHORD)
-    arc(0,0,rad*2,rad*2,a2,a4,CHORD)
-    arc(0,0,rad*2,rad*2,a3,a4,CHORD)
-    arc(0,0,rad*2,rad*2,a1,a2,CHORD)
-    fill(330,100,100,100)
-    //triangle(p.x,p.y,x1,y1,x4,y4)
-    //arc(0,0,rad*2,rad*2,a4,a1,PIE)
+    translate(w * 0.5, h * 0.5)
+    stroke(0, 0, 0); noFill()
+    a2 = 0
+    noStroke()
+    hu=palette[Math.floor(random(palette.length))]
+    fill(hu, 100, 100, 110)
+    ellipse(0,0,rad*2,rad*2)
+    for (var i = 0; i < 42; i++) {
+        a1 = a2 + 280 * noise(xoff); xoff += xinc
+        a2 = a1 + 278 * noise(xoff); xoff += xinc
+        hu=palette[Math.floor(random(palette.length))]
+        fill(hu, 100, 100, 110)
+        console.log(hu)
+        arc(0, 0, rad * 2, rad * 2, a1, a2, CHORD)
+    }
     pop()
 }
 
@@ -113,10 +97,10 @@ function quartier(x1, y1, x2, y2, x3, y3, x4, y4, a1, a2) {
         oy2 = (1 - t) * y1 + (t * y2);
         dx2 = (1 - t) * x3 + (t * x4);
         dy2 = (1 - t) * y3 + (t * y4);
-//        t += 0.085 * noise(xoff); xoff += xinc
-        hu=random(palette)
+        //        t += 0.085 * noise(xoff); xoff += xinc
+        hu = random(palette)
         noStroke()
-        fill(hu,100,100)
+        fill(hu, 100, 100)
         quad(ox1, oy1, dx1, dy1, dx2, dy2, ox2, oy2)
     }
     var cx, cy, outx1, outy1, outx2, outy2
@@ -164,16 +148,16 @@ function carte(x1, y1, x2, y2, x3, y3, x4, y4, d) {
         dy = (1 - t) * y3 + (t * y4);
         line(ox, oy, dx, dy)
         d++
-        carte(ox,oy,dx,dy,x4,y4,x1,y1,d)
-        carte(ox,oy,dx,dy,x3,y3,x2,y2,d)
+        carte(ox, oy, dx, dy, x4, y4, x1, y1, d)
+        carte(ox, oy, dx, dy, x3, y3, x2, y2, d)
     }
-    else{
+    else {
         noStroke()
-        hu=random(palette)
-        fill(hu,100,100,100)
+        hu = random(palette)
+        fill(hu, 100, 100, 100)
         quad(x1, y1, x2, y2, x3, y3, x4, y4)
-        hu=random(palette)
-        fill(hu,100,100,100)
+        hu = random(palette)
+        fill(hu, 100, 100, 100)
         quad(x1, y1, x2, y2, x3, y3, x4, y4)
     }
 }
