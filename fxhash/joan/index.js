23ea0faf9563c8b7e71b95b5832d3e5489be33d9
bbaudry <baudry@kth.se>
2025-02-12 19:52:09 -0500

    [+-.><.-+]

--- fxhash/joan/index.js
+++ fxhash/joan/index.js
@@ -16,7 +16,7 @@ function setup() {
     colorMode(HSB, 360, 100, 100, 250);
 }
 
-
+var hues=[30,50,90,230,330]
 function draw() {
     background(300, 20, 90)
     sky()
@@ -24,7 +24,30 @@ function draw() {
 }
 
 function sky(){
-    moons()
+    light()
+}
+
+function light(){
+    var x1,y1,x2,y2,x3,y3,x4,y4,minx,maxx,hu
+    x1=leftmargin+0.1*actualwidth
+    y1=topmargin
+    x2=x1+random(0.1,0.2)*actualwidth
+    y2=y1
+    minx=leftmargin
+    maxx=leftmargin
+    while(y1<bottommargin){
+        x3=x2+random(-0.03,0.03)*actualwidth
+        y3=y1+random(0.1,0.2)*actualwidth
+        x4=x1+random(-0.03,0.03)*actualwidth
+        y4=y3
+        hu=hues[Math.floor(random(hues.length))]
+        fill(hu,100,100)
+        quad(x1,y1,x2,y2,x3,y3,x4,y4)
+        y1=y4
+        y2=y3
+        x1=x4
+        x2=x3
+    }
 }
 
 function moons(){
