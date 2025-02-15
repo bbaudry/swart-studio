8d23959a10891ee78f2e72f2055985b3610cc3fe
bbaudry <baudry@kth.se>
2025-02-15 12:44:58 -0500

    +{]*><*[}+

--- fxhash/joan/index.js
+++ fxhash/joan/index.js
@@ -1,84 +1,124 @@
 
-var w, h, cnv, leftmargin, rightmargin, topmargin, bottommargin, actualwidth, actualheight 
+var w, h, cnv, leftmargin, rightmargin, topmargin, bottommargin, actualwidth, actualheight
 
 
 
 function setup() {
-    w=690
-    h=690
+    w = 690
+    h = 690
     createCanvas(w, h)
-    leftmargin = Math.floor(w*0.01)
-    rightmargin = Math.floor(w*0.99)
+    leftmargin = Math.floor(w * 0.01)
+    rightmargin = Math.floor(w * 0.99)
     actualwidth = rightmargin - leftmargin
-    topmargin = Math.floor(w*0.01)
-    bottommargin = Math.floor(w*0.99)
+    topmargin = Math.floor(w * 0.01)
+    bottommargin = Math.floor(w * 0.99)
     actualheight = bottommargin - topmargin
     colorMode(HSB, 360, 100, 100, 250);
     noStroke()
 }
 
-var hues=[30,90,230]
+var hues = [30, 90, 230]
 function draw() {
-    background(230, 100, 70)
+    background(230, 0, 100)
     sky()
     noLoop()
 }
 
-function sky(){
+function sky() {
     light()
 }
 
-xoff=$fx.rand()
-xinc=$fx.rand()*0.5
-stepx=0.05
-stepy=0.01+$fx.rand()*0.3
-
-function light(){
-    var x1,y1,x2,y2,x3,y3,x4,y4,cx,minx,maxx,hu
-    minx=leftmargin
-    maxx=leftmargin
-    for(var i=0;i<10;i++){
-        cx=leftmargin+(i*(stepx*1.4)+stepx)*actualwidth
-        y1=topmargin
-        y2=y1
-        x1=cx-noise(xoff)*stepx*actualwidth; xoff+=xinc
-        x2=cx+noise(xoff)*stepx*actualwidth; xoff+=xinc
-        while(y1<bottommargin){
-        x3=cx+noise(xoff)*stepx*actualwidth; xoff+=xinc
-        y3=y1+noise(xoff)*stepy*actualheight; xoff+=xinc
-        if(y3>=bottommargin){y3=bottommargin}
-        x4=cx-noise(xoff)*stepx*actualwidth; xoff+=xinc
-        y4=y3
-        hu=hues[Math.floor(random(hues.length))]
-        fill(random([30,120,210,300]),random([0,100,100]),random([0,100,100]))
-        quad(x1,y1,x2,y2,x3,y3,x4,y4)
-        y1=y4
-        y2=y3
-        x1=x4
-        x2=x3
-    }}
+var xoff = $fx.rand()
+var xinc = $fx.rand() * 0.5
+var stepx = 0.05
+var stepy = 0.01 + $fx.rand() * 0.3
+var hu,humin,humax,huup
+humin=180
+humax=330
+hu=humin
+huup=true
+function light() {
+    var x1, y1, x2, y2, x3, y3, x4, y4, cx, minx, maxx, hu
+    minx = leftmargin
+    maxx = leftmargin
+    for (var i = 0; i < 14; i++) {
+        cx = leftmargin + (i * (stepx * 1.4) + stepx) * actualwidth
+        y1 = topmargin
+        y2 = y1
+        x1 = cx - noise(xoff) * stepx * actualwidth; xoff += xinc
+        x2 = cx + noise(xoff) * stepx * actualwidth; xoff += xinc
+        while (y1 < bottommargin) {
+            x3 = cx + noise(xoff) * stepx * actualwidth; xoff += xinc
+            y3 = y1 + noise(xoff) * stepy * actualheight; xoff += xinc
+            if (y3 >= bottommargin) { y3 = bottommargin }
+            x4 = cx - noise(xoff) * stepx * actualwidth; xoff += xinc
+            y4 = y3
+            hu = hues[Math.floor(random(hues.length))]
+            //fill(random([30, 120, 210, 300]), random([0, 100, 100]), random([0, 100, 100]))
+            grad(x1, y1, x2, y2, x3, y3, x4, y4)
+            y1 = y4
+            y2 = y3
+            x1 = x4
+            x2 = x3
+        }
+    }
 }
 
-function moons(){
-    var cx = leftmargin+random(0.2,0.8)*actualwidth
-    var cy = topmargin+random(0.2,0.4)*actualwidth
+//hypothesis: y1=y2 and y4=y3 and y4>y1
+function grad(x1, y1, x2, y2, x3, y3, x4, y4){
+    var diffy=y4-y1
+    var ox,oy,dx,dy,inct
+    inct=1/diffy
+    oy=y1+1
+    dy=y2+1
+    for(var t=0;t<1;t+=inct){
+        ox = (1 - t) * x1 + (t * x4);
+        dx = (1 - t) * x2 + (t * x3);
+        stroke(hu,100,100)
+        line(ox,oy,dx,dy)
+        oy++
+        dy++
+        updatehu()
+    }
+}
+
+function updatehu(){
+    if(huup && hu<humax){
+        hu++
+    }
+    else{
+        huup=false
+        hu--
+    }
+    if(!huup && hu>humin){
+        hu--
+    }
+    else{
+        huup=true
+        hu++
+    }
+}
+
+function moons() {
+    var cx = leftmargin + random(0.2, 0.8) * actualwidth
+    var cy = topmargin + random(0.2, 0.4) * actualwidth
     noStroke()
-    
+
     push();
-    fill(220,40,100);
-    ellipse(150,200,200);
+    fill(220, 40, 100);
+    ellipse(150, 200, 200);
     canvas.getContext("2d").clip();
     fill(300, 20, 90)
-    ellipse(50,200,250);
+    ellipse(50, 200, 250);
     pop();
-    
+
     push();
-    fill(20,100,100);
-    ellipse(250,200,200);
+    fill(20, 100, 100);
+    ellipse(250, 200, 200);
     canvas.getContext("2d").clip();
-    fill(200,100,100)
-    ellipse(150,200,200);
+    fill(200, 100, 100)
+    ellipse(150, 200, 200);
     pop();
-  
+
 }
 
