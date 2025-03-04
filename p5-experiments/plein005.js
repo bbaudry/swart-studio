665aa0413553f64263999d2b1409cba19d9fb9a5
bbaudry <baudry@kth.se>
2025-03-04 12:13:20 -0500

    °-*[><]*-°

--- p5-experiments/plein005.js
+++ p5-experiments/plein005.js
@@ -36,19 +36,23 @@ function draw() {
 function nucleus(x1,y1,x2,y2,x3,y3) {
     noFill()
     stroke(0,0,100)
+    var cx= leftmargin+actualwidth*0.5
+    var cy= topmargin+actualheight*0.7
+    x1=cx+actualwidth*0.7*cos(radians(230))
+    y1=cy+actualwidth*0.7*sin(radians(230))
+    x2=cx+actualwidth*0.5*cos(radians(50))
+    y2=cy+actualwidth*0.5*sin(radians(50))
+    x3=cx+actualwidth*0.5*cos(radians(130))
+    y3=cy+actualwidth*0.5*sin(radians(130))
     triangle(x1,y1,x2,y2,x3,y3)
-    var t = 0.1
-    x1++//= (1 - t) * x1 + (t * x3 );
-    y1 = (1 - t) * y1 + (t * y3);
-    x2 = (1 - t) * x2 + (t * x3);
-    y3 = (1 - t) * y3 + (t * y1);
-    y2 = y3
+
+    x1=cx+actualwidth*0.68*cos(radians(228))
+    y1=cy+actualwidth*0.68*sin(radians(228))
+    x2=cx+actualwidth*0.48*cos(radians(48))
+    y2=cy+actualwidth*0.48*sin(radians(48))
+    x3=cx+actualwidth*0.48*cos(radians(130))
+    y3=cy+actualwidth*0.48*sin(radians(130))
     triangle(x1,y1,x2,y2,x3,y3)
-    x1++//= (1 - t) * x1 + (t * x3 );
-    y1 = (1 - t) * y1 + (t * y3);
-    x2 = (1 - t) * x2 + (t * x3);
-    y3 = (1 - t) * y3 + (t * y1);
-    y2 = y3
-    triangle(x1,y1,x2,y2,x3,y3)
+    ellipse(cx,cy,7,7)
 }
 
