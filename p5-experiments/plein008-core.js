a873be3702de09874b3407336a18e8b1a08456ac
bbaudry <baudry@kth.se>
2025-06-28 16:11:19 +0200

    }-]]><[[-{

--- p5-experiments/plein008-core.js
+++ p5-experiments/plein008-core.js
@@ -6,9 +6,9 @@ function hal() {
     cx = leftmargin + actualwidth * 0.5
     cy = topmargin + actualheight  * 0.5
     magics=[]
-    xoff = 0.0; xinc = 0.0001; magics.push({name:xinc,val:xinc})
-    angleinc = 11; magics.push({name:angleinc,val:angleinc})
-    diam = actualwidth * 0.01; diaminc = actualwidth * 0.003; magics.push({name:diaminc,val:diaminc})
+    xoff = 0.0; xinc = 0.0001; magics.push({name:"xinc",val:xinc})
+    angleinc = 11; magics.push({name:"angleinc",val:angleinc})
+    diam = actualwidth * 0.01; diaminc = actualwidth * 0.003; magics.push({name:"diaminc",val:diaminc})
     while(diam<w*0.9){
         angle2 = 0
         angle1 = Math.floor(noise(xoff) * angleinc); xoff += xinc
