a9463dbd51881170911cac4d3b046ee7ac176e22
bbaudry <baudry@kth.se>
2025-07-06 23:44:32 +0200

    -+.]><[.+-

--- p5-experiments/stervilin001.js
+++ p5-experiments/stervilin001.js
@@ -43,7 +43,8 @@ function draw() {
     noFill()
     stroke(300, 80, 100)
     let magie
-    kerou()
+    stroke(0, 100, 100);kerou()
+    stroke(180, 100, 100);kerou()
     //let magicnumbers=hal()
     //magie="Magic numbers :: "+magicnumbers[0].name+": "+magicnumbers[0].val+"; "+magicnumbers[1].name+": "+magicnumbers[1].val+"; "+magicnumbers[2].name+": "+magicnumbers[2].val+"; "+magicnumbers[3].name+": "+magicnumbers[3].val+"; "+magicnumbers[4].name+": "+magicnumbers[4].val
     magie += '\n' + "beautiful noise [almyre::2025]"
@@ -53,25 +54,20 @@ function draw() {
 
     noLoop()
 }
-
+var xoff=0.0
+var xinc=0.001
 function kerou() {
-    let xoff, xinc, x1, y1, ax1, ay1, ax2, ay2, x2, y2
-    xoff = 0.0
-    xinc = 0.01
-    stroke(0, 100, 100)
+    let x1, y1, ax1, ay1, ax2, ay2, x2, y2
     for (let i = 0; i < 150; i++) {
         beginShape()
         x1 = leftmargin
         if (random() < 0.1) { y1 += h * 0.1 }
         else { y1 = h * 0.1 + i * 3 }
         vertex(x1, y1)
-        ax1 = leftmargin + actualwidth * noise(xoff)
-        xoff += xinc
+        ax1 = leftmargin + actualwidth * noise(xoff); xoff += xinc
         ay1 = y1-actualheight*0.1
-        ax2 = leftmargin + actualwidth * (1 - noise(xoff))
-        xoff += xinc
-        ay2 = y1+actualheight*(noise(xoff)*1.3)
-        xoff += xinc
+        ax2 = leftmargin + actualwidth * (0.3 * noise(xoff)); xoff += xinc
+        ay2 = y1+actualheight*(noise(xoff)*1.3); xoff += xinc
         x2 = rightmargin
         y2 = y1
         bezierVertex(ax1, ay1, ax2, ay2, x2, y2)
