47a94c1bd94d79f845900b00e21c90b6d7c1bc63
bbaudry <baudry@kth.se>
2025-07-07 09:49:01 +0200

    +*-°><°-*+

--- p5-experiments/stervilin001.js
+++ p5-experiments/stervilin001.js
@@ -58,16 +58,17 @@ var xoff=0.0
 var xinc=0.001
 function kerou() {
     let x1, y1, ax1, ay1, ax2, ay2, x2, y2
-    for (let i = 0; i < 150; i++) {
+    y1 = h * 0.3 
+    for (let i = 0; i < 100; i++) {
         beginShape()
         x1 = leftmargin
-        if (random() < 0.1) { y1 += h * 0.1 }
-        else { y1 = h * 0.1 + i * 3 }
+        if (random() < 0.01) { y1 += h * 0.05 }
+        else { y1+=2 }
         vertex(x1, y1)
         ax1 = leftmargin + actualwidth * noise(xoff); xoff += xinc
-        ay1 = y1-actualheight*0.1
+        ay1 = y1+actualheight*0.1
         ax2 = leftmargin + actualwidth * (0.3 * noise(xoff)); xoff += xinc
-        ay2 = y1+actualheight*(noise(xoff)*1.3); xoff += xinc
+        ay2 = y1-actualheight*(noise(xoff)*1.2); xoff += xinc
         x2 = rightmargin
         y2 = y1
         bezierVertex(ax1, ay1, ax2, ay2, x2, y2)
