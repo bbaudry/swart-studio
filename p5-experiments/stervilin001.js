21ef344164b3fe4ef224c22b7d536fb95c7a5eca
bbaudry <baudry@kth.se>
2025-07-07 10:11:07 +0200

    *{]-><-[}*

--- p5-experiments/stervilin001.js
+++ p5-experiments/stervilin001.js
@@ -65,10 +65,10 @@ function kerou() {
         if (random() < 0.01) { y1 += h * 0.05 }
         else { y1+=2 }
         vertex(x1, y1)
-        ax1 = leftmargin + actualwidth * noise(xoff); xoff += xinc
-        ay1 = y1+actualheight*0.1
+        ax1 = leftmargin + actualwidth * noise(xoff);xoff += xinc
+        ay1 = y1+cos(radians(i))//actualheight*0.1
         ax2 = leftmargin + actualwidth * (0.3 * noise(xoff)); xoff += xinc
-        ay2 = y1-actualheight*(noise(xoff)*1.2); xoff += xinc
+        ay2 = y1-actualheight*(cos(radians(i))); xoff += xinc //noise(xoff)*1.2
         x2 = rightmargin
         y2 = y1
         bezierVertex(ax1, ay1, ax2, ay2, x2, y2)
