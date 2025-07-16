d1e953d706b6f54631596e3d0fbe900a12c5404e
bbaudry <baudry@kth.se>
2025-07-16 10:38:51 -0400

    {*{]><[}*}

--- p5-experiments/spark001-core.js
+++ p5-experiments/spark001-core.js
@@ -4,53 +4,68 @@ var sectionwidth
 // inspired by https://i.pinimg.com/originals/ce/67/ff/ce67ff060a0e13808a023b3d61389ee0.jpg
 // other option https://www.pinterest.com/pin/410109109798861399/
 function spark() {
-    let x
+    let x,y
     stroke(0,100,100)
     fill(0,100,100)
     sectionwidth = Math.floor(actualwidth * 0.2)
     x = leftmargin
-    s(x)
+    y = bottommargin
+    s(x,y); line(x,y)
     x+=sectionwidth
-    p(x)
+    p(x,y); line(x,y)
+    x+=sectionwidth
+    a(x,y); line(x,y)
 }
 
 
-function s(x) {
-    let cx, cy, a1, a2, a3, a4, rad, x1, y1, x2, y2, x3, y3, x4, y4, offsetx,offsety
-    cx = x + sectionwidth * 0.5
-    cy = topmargin + actualheight * 0.5
-    offsetx = sectionwidth*0.1
+function s(x,y) {
+    let cxtop, cytop, cxlow, cylow, a1, a2, a3, a4, rad, x1, y1, x2, y2, x3, y3, x4, y4, offsetx,offsety
+    offsetx = sectionwidth*0.05
     offsety = Math.floor(random(10,20))
     rad = Math.floor(sectionwidth * 0.38)
+    cxlow = x + sectionwidth * 0.5 - offsetx
+    cylow = y - rad
+    cxtop = x + sectionwidth * 0.5 + offsetx
+    cytop = cylow - offsety * 2
     a1 = Math.floor(random(210,230))
     a2 = Math.floor(random(340,360))
     a3 = Math.floor(random(30,50))
     a4 = Math.floor(random(160,180))
 
     // draw top arc
-    arc(cx+offsetx, cy-offsety, rad * 2, rad * 2, radians(a1), radians(a2))
+    arc(cxtop, cytop, rad * 2, rad * 2, radians(a1), radians(a2))
     // draw bottom arc
-    arc(cx-offsetx, cy+offsety, rad * 2, rad * 2, radians(a3), radians(a4))
+    arc(cxlow, cylow, rad * 2, rad * 2, radians(a3), radians(a4))
     // draw middle link
-    x1=(cx+offsetx)+rad*cos(radians(a1))
-    y1=(cy-offsety)+rad*sin(radians(a1))
-    x2=cx+offsetx
-    y2=cy-offsety
-    x3=(cx-offsetx)+rad*cos(radians(a3))
-    y3=(cy+offsety)+rad*sin(radians(a3))
-    x4=cx-offsetx
-    y4=cy+offsety
+    x1=cxtop+rad*cos(radians(a1))
+    y1=cytop+rad*sin(radians(a1))
+    x2=cxtop
+    y2=cytop
+    x3=cxlow+rad*cos(radians(a3))
+    y3=cylow+rad*sin(radians(a3))
+    x4=cxlow
+    y4=cylow
     quad(x1, y1, x2, y2, x3, y3, x4, y4,)
 }
 
-function p(x){
-    let y,cx,cy,rad,thick
+function p(x,y){
+    let cx,cy,rad,thick
     thick=sectionwidth*random(0.3,0.5)
-    y=topmargin+actualheight*0.5
-    y-=sectionwidth*0.5
+    y-=sectionwidth
     rect(x,y,thick,sectionwidth)
     rad=sectionwidth*random(0.2,0.3)
     cx=x+thick
     cy=y+rad
     arc(cx,cy,rad*2,rad*2,radians(270),radians(90))
+}
+
+function a(x,y){
+    let x1,y1,x2,y2,x3,y3
+    x1=x
+    y1=y
+    x2=x+sectionwidth*random(0.7,0.99)
+    y2=y
+    x3=x2
+    y3=y-sectionwidth*random(0.7,0.99)
+    triangle(x1,y1,x2,y2,x3,y3)
 }
