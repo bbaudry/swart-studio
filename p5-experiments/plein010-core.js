74a202517e781094b5d0c9cb11e2f0fe5f5a2529
bbaudry <baudry@kth.se>
2025-08-05 08:47:24 -0400

    +}]+><+[{+

--- p5-experiments/plein010-core.js
+++ p5-experiments/plein010-core.js
@@ -1,13 +1,94 @@
-
 function hal() {
-    let x1, y1, x2, y2, x3, y3, x4, y4, resolution, stepx, stepy, noisex, noisexinc, xoffset, yoffset
+    let x1, y1, x2, y2, x3, y3, x4, y4, resolution, stepx, stepy, noisex, noisexinc, noisey, noiseyinc, xoffset, yoffset, sections, nbsectionshorizontal, nbsectionsvertical
+
+    // Initialize some variables
+    sections = [];
+    id = 0;
     resolution = 100
+    nbsectionshorizontal = resolution
+    nbsectionsvertical = resolution
+    noisex = 0.0; noisey = 0.0
+    noisexinc = 0.01; noiseyinc = 0.01
     stepx = Math.floor(actualwidth / resolution)
     stepy = Math.floor(actualheight / resolution)
-    noisex = 0.0
-    noisexinc = 0.001
-    xoffset = stepx * 0.5
-    yoffset = stepy * 0.5
+    xoffset = stepx * 0.1
+    yoffset = stepy * 0.1
+
+    // Create all sections
+    for (let i = 0; i < nbsectionshorizontal; i++) {
+        for (let j = 0; j < nbsectionsvertical; j++) {
+            // Check if we are at the beginning of a row
+            if (i == 0) {
+                if (j == 0) {
+                    x1 = leftmargin + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
+                    x2 = x1 + stepx + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
+                    x3 = x1 + stepx + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
+                    x4 = leftmargin + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
+                    y1 = topmargin + (-yoffset + 2 * noise(noisex) * yoffset); noisex += noisexinc
+                    y2 = topmargin + (-yoffset + 2 * noise(noisex) * yoffset); noisex += noisexinc
+                    y3 = y2 + stepy + (-yoffset + 2 * noise(noisex) * yoffset); noisex += noisexinc
+                    y4 = y1 + stepy + (-yoffset + 2 * noise(noisex) * yoffset); noisex += noisexinc
+                }
+                else {
+                    x1 = sections[(j - 1)].x4
+                    x2 = sections[(j - 1)].x3
+                    x3 = x1 + stepx + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
+                    x4 = x1 + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
+                    y1 = sections[(j - 1)].y4
+                    y2 = sections[(j - 1)].y3
+                    y3 = y2 + stepy + (-yoffset + 2 * noise(noisex) * yoffset); noisex += noisexinc
+                    y4 = y1 + stepy + (-yoffset + 2 * noise(noisex) * yoffset); noisex += noisexinc
+                }
+            } else {
+                if (j == 0) {
+                    x1 = sections[((i - 1) * nbsectionsvertical) + j].x2
+                    x2 = x1 + stepx + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
+                    x3 = x1 + stepx + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
+                    x4 = sections[((i - 1) * nbsectionsvertical) + j].x3
+                    y1 = sections[((i - 1) * nbsectionsvertical) + j].y2
+                    y2 = y1 + (-yoffset + 2 * noise(noisex) * yoffset); noisex += noisexinc
+                    y3 = y2 + stepy + (-yoffset + 2 * noise(noisex) * yoffset); noisex += noisexinc
+                    y4 = sections[((i - 1) * nbsectionsvertical) + j].y3
+                }
+                else {
+                    x1 = sections[((i - 1) * nbsectionsvertical) + j].x2
+                    x2 = sections[i * nbsectionsvertical + j - 1].x3
+                    x3 = x1 + stepx + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
+                    x4 = sections[((i - 1) * nbsectionsvertical) + j].x3
+                    y1 = sections[i * nbsectionsvertical + j - 1].y4
+                    y2 = sections[i * nbsectionsvertical + j - 1].y3
+                    y3 = y2 + stepy + (-yoffset + 2 * noise(noisex) * yoffset); noisex += noisexinc
+                    y4 = sections[((i - 1) * nbsectionsvertical) + j].y3
+                }
+            }
+            // Create the section
+            let section = {
+                x1: x1,
+                y1: y1,
+                x2: x2,
+                y2: y2,
+                x3: x3,
+                y3: y3,
+                x4: x4,
+                y4: y4,
+            };
+
+            // Add the section to the list and increment the id
+            sections.push(section);
+            quad(x1, y1, x2, y2, x3, y3, x4, y4)
+        }
+    }
+}
+
+
+
+function hal_sav() {
+    let x1, y1, x2, y2, x3, y3, x4, y4, resolution, stepx, stepy, noisex, noisexinc, xoffset, yoffset
+    resolution = 15
+    stepx = Math.floor(actualwidth / resolution)
+    stepy = Math.floor(actualheight / resolution)
+    noisex = 0.0; noisexinc = 0.001
+    xoffset = stepx * 0.5; yoffset = stepy * 0.5
     x1 = leftmargin + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
     x2 = x1 + stepx + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
     x3 = x1 + stepx + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
@@ -19,14 +100,13 @@ function hal() {
         y4 = y1 + stepy + (-yoffset + 2 * noise(noisex) * yoffset); noisex += noisexinc
         while (y3 < bottommargin && y4 < bottommargin) {
             quad(x1, y1, x2, y2, x3, y3, x4, y4)
-            y1 = y4
-            y2 = y3
+            y1 = y4; y2 = y3
             y3 = y2 + stepy + (-yoffset + 2 * noise(noisex) * yoffset); noisex += noisexinc
             y4 = y1 + stepy + (-yoffset + 2 * noise(noisex) * yoffset); noisex += noisexinc
+            x1 = x4; x4 = x1 + (-xoffset + 2 * noise(noisex) * xoffset)
+            x2 = x3; x3 = x1 + stepx + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
         }
-        x1 = x2
-        x2 = x1 + stepx + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
-        x4 = x3
-        x3 = x4 + stepx + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
+        x1 = x2; x2 = x1 + stepx + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
+        x4 = x3; x3 = x4 + stepx + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
     }
 }
