c91ca1ab3b46ebb6fd8f26307f495f6ff04e5229
bbaudry <baudry@kth.se>
2025-08-05 21:00:20 -0400

    ][.{><}.][

--- p5-experiments/plein010-core.js
+++ p5-experiments/plein010-core.js
@@ -1,18 +1,18 @@
-var     sections;
+var sections;
 
 function hal() {
     let x1, y1, x2, y2, x3, y3, x4, y4, resolution, stepx, stepy, noisex, noisexinc, noisey, noiseyinc, xoffset, yoffset, nbsectionshorizontal, nbsectionsvertical
 
     // Initialize some variables
-    sections=[]
+    sections = []
     id = 0;
-    resolution = 142
-    nbsectionshorizontal = resolution
-    nbsectionsvertical = resolution
+    resolution = 31
+    nbsectionshorizontal = resolution*3
+    nbsectionsvertical = resolution*7
     noisex = 0.0; noisey = 0.0
-    noisexinc = 0.01; noiseyinc = 0.01
-    stepx = Math.floor(actualwidth / resolution)
-    stepy = Math.floor(actualheight / resolution)
+    noisexinc = 0.001; noiseyinc = 0.01
+    stepx = Math.floor(actualwidth / nbsectionshorizontal)
+    stepy = Math.floor(actualheight / nbsectionsvertical)
     xoffset = stepx * 0.1
     yoffset = stepy * 0.1
 
@@ -38,8 +38,8 @@ function hal() {
                     x4 = x1 + (-xoffset + 2 * noise(noisex) * xoffset); noisex += noisexinc
                     y1 = sections[(j - 1)].y4
                     y2 = sections[(j - 1)].y3
-                    y3 = y2 + stepy + (-yoffset + 2 * noise(noisey) * yoffset); //noisex += noisexinc
-                    y4 = y1 + stepy + (-yoffset + 2 * noise(noisey) * yoffset); //noisex += noisexinc
+                    y3 = y2 + stepy + (-yoffset + 2 * noise(noisey) * yoffset);
+                    y4 = y1 + stepy + (-yoffset + 2 * noise(noisey) * yoffset);
                 }
             } else {
                 if (j == 0) {
@@ -59,7 +59,7 @@ function hal() {
                     x4 = sections[((i - 1) * nbsectionsvertical) + j].x3
                     y1 = sections[i * nbsectionsvertical + j - 1].y4
                     y2 = sections[i * nbsectionsvertical + j - 1].y3
-                    y3 = y2 + stepy + (-yoffset + 2 * noise(noisey) * yoffset); //noisex += noisexinc
+                    y3 = y2 + stepy + (-yoffset + 2 * noise(noisey) * yoffset);
                     y4 = sections[((i - 1) * nbsectionsvertical) + j].y3
                 }
             }
@@ -77,11 +77,21 @@ function hal() {
 
             // Add the section to the list and increment the id
             sections.push(section);
-            quad(x1, y1, x2, y2, x3, y3, x4, y4)
         }
     }
+    showgrid()
 }
 
+function showgrid() {
+    let black = true
+    fill(0,0,0)
+    for (s in sections) {
+        if (black) {
+            quad(sections[s].x1, sections[s].y1, sections[s].x2, sections[s].y2, sections[s].x3, sections[s].y3, sections[s].x4, sections[s].y4)
+        }
+        black=!black
+    }
+}
 
 
 function hal_sav() {
