f18e2efb1e67c0c77dc8666f3a008e4a22b574a3
bbaudry <baudry@kth.se>
2025-09-06 18:05:01 -0400

    ]--.><.--[

--- p5-experiments/particles/walker.js
+++ p5-experiments/particles/walker.js
@@ -4,13 +4,16 @@ class Walker{
     }
 
     update(){
-        this.pos.x=this.pos.x+random(-1,1)
-        this.pos.y=this.pos.y+random(-1,1)
+        this.pos.x=this.pos.x+random(-5,5)
+        this.pos.y=this.pos.y+random(-5,5)
     }
 
     show(){
-        stroke(0,0,100)
-        strokeWeight(2)
+        stroke(0,0,100,10)
+        strokeWeight(7)
+        point(this.pos.x,this.pos.y)
+        stroke(0,100,100,200)
+        strokeWeight(1)
         point(this.pos.x,this.pos.y)
     }
 }
