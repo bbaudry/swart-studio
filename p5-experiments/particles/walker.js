b6e0970e73916bf59b02c169cd8f115def428759
bbaudry <baudry@kth.se>
2025-09-07 17:39:44 -0400

    {°°°><°°°}

--- p5-experiments/particles/walker.js
+++ p5-experiments/particles/walker.js
@@ -2,19 +2,25 @@ class Walker{
     constructor(x,y){
         this.pos=createVector(x,y)
         this.vel=p5.Vector.random2D()
-        this.vel=this.vel*random(3,21)
+        this.vel.mult(random(3,21))
+        this.xoff=0.0
+        this.xinx=0.001
     }
 
     update(){
         this.pos.add(this.vel)
-        this.vel=p5.Vector.random2D()
+        this.vel=p5.Vector.random2D().mult(21+noise(this.xoff)*42);this.xoff+=this.xinc
+        if(this.pos.x<0 || this.pos.x>w || this.pos.y<0 || this.pos.y>h ){
+            this.pos.x=200
+            this.pos.y=200
+        }
     }
 
     show(){
         stroke(0,0,100,10)
         strokeWeight(7)
-        point(this.pos.x,this.pos.y)
-        stroke(0,100,100,200)
+        //point(this.pos.x,this.pos.y)
+        stroke(0,100,100,20)
         strokeWeight(1)
         point(this.pos.x,this.pos.y)
     }
