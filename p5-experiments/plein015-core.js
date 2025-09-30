be21ed2e6e446272c7651d4711944b29f3a6de5f
bbaudry <baudry@kth.se>
2025-09-29 22:32:13 -0400

    *[]}><{[]*

---
+++ p5-experiments/plein015-core.js
@@ -0,0 +1,36 @@
+var xoff = 0.0
+var xinc = 1
+
+function hal() {
+    let resx, resy, stepx, stepy, x, y, x1, y1, coords, p1, p2, p3
+    circlewlines(leftmargin+actualwidth*0.5,topmargin+actualheight*0.5,actualwidth)
+    resx = 2 * Math.floor(random(2, 3)); resy = 2 * Math.floor(random(2, 3)); coords = []
+    stepx = Math.floor(actualwidth / resx); stepy = Math.floor(actualheight / resy)
+    x = leftmargin
+    for (let i = 0; i < resx; i++) {
+        y = topmargin
+        for (let j = 0; j < resy; j++) {
+            x1 = x + stepx * noise(xoff); xoff += xinc; y1 = y + stepy * noise(xoff)
+            coords.push({ x: x1, y: y1 })
+            y += stepy}
+        x += stepx}
+    for (let i = 0; i < resx - 1; i++) {
+        for (let j = 0; j < resy - 1; j++) {
+            p1 = coords[i * resy + j]; p2 = coords[i * resy + j + resy]; p3 = coords[i * resy + j + resy + 1]; p4 = coords[i * resy + j + 1]
+            trianglewlines(p1.x, p1.y, p3.x, p3.y, p4.x, p4.y);
+            trianglewlines(p2.x, p2.y, p3.x, p3.y, p1.x, p1.y);
+            trianglewlines(p2.x, p2.y, p3.x, p3.y, p4.x, p4.y);
+}}}
+
+function trianglewlines(x1, y1, x2, y2, x3, y3) {
+    // https://mathopenref.com/coordincenter.html
+    len1 = dist(x2, y2, x3, y3); len2 = dist(x1, y1, x3, y3); len3 = dist(x1, y1, x2, y2)
+    cx = (len1 * x1 + len2 * x2 + len3 * x3) / (len1 + len2 + len3); cy = (len1 * y1 + len2 * y2 + len3 * y3) / (len1 + len2 + len3)
+    for (let i = 0; i < 0.8; i += 0.03) {
+        triangle(lerp(cx, x1, i), lerp(cy, y1, i), lerp(cx, x2, i), lerp(cy, y2, i), lerp(cx, x3, i), lerp(cy, y3, i))
+}}
+
+function circlewlines(cx, cy, rad) {
+    for (let i = 0; i < 0.8; i += 0.005) {
+        ellipse(cx,cy,rad*i)
+}}
---
+++ p5-experiments/plein015.js
@@ -0,0 +1,89 @@
+
+var w, h
+var cnv
+var leftmargin, rightmargin, topmargin, bottommargin, actualheight, actualwidth, penwidth
+var sourcecode
+var font
+var fSize = 9
+var artname = "plein015"
+
+function preload() {
+    font = loadFont("./fonts/1CAMBam_Stick_9.ttf");
+        sourcecode = loadStrings(artname+'-core.js');
+}
+function setup() {
+    w = Math.floor(8.5 * 96)
+    h = Math.floor(11 * 96)
+    //cnv = createCanvas(w, h, SVG).mousePressed(savesvg);
+    cnv = createCanvas(w, h).mousePressed(savepng);
+    centerCanvas();
+    leftmargin = Math.floor(w * 0.05)
+    rightmargin = Math.floor(w * 0.95)
+    topmargin = Math.floor(h * 0.02)
+    bottommargin = Math.floor(h * 0.7)
+    actualwidth = rightmargin - leftmargin
+    actualheight = bottommargin - topmargin
+    colorMode(HSB, 360, 100, 100, 250);
+    //96*0.2/25.4 : 0.2mm is the width of a fineliner
+    //0.04 * 96 : 0.04 inch is 1 mm, the width of stabilo 68/32
+    penwidth =96*0.2/25.4
+    strokeWeight(penwidth)
+}
+
+function savesvg() {
+    save(artname+".svg");
+}
+
+
+function savepng() {
+    save(artname+".png");
+}
+
+
+function centerCanvas() {
+    var x = (windowWidth - windowHeight) / 2;
+    var y = (windowHeight - windowHeight) / 2;
+    cnv.position(x, y);
+}
+
+
+function draw() {
+    background(0, 0, 100)
+    noFill()
+    textFont(font)
+    textSize(fSize)
+    stroke(300,100,100)   
+    hal()
+    stroke(0, 0, 0); noFill(); strokeWeight(1)
+    let c = showcodeall(leftmargin * 4.2, bottommargin)
+    text("plein014[automne]", c[0], c[1] + fSize)
+    text("p5.js + axidraw [almyre::2025]", c[0], c[1] + 2*fSize)
+    noLoop()
+}
+
+
+function showcodeall(posx, posy) {
+    var x, y
+    x = posx
+    y = posy
+    for (b in sourcecode) {
+        text(sourcecode[b], x, y)
+        y += fSize
+    }
+    return ([x, y])
+}
+
+function clemence() {
+    fill(330, 50, 100)
+    noStroke()
+    let x, y, rayon
+    rayon = w * 0.3
+    x = w * 0.5
+    y = topmargin + rayon
+    arc(x, y, rayon * 2, rayon * 2, PI, 2 * PI)
+
+    fill(50, 80, 100)
+    x -= rayon
+    y += h * 0.1
+    rect(x, y, rayon, rayon)
+}
