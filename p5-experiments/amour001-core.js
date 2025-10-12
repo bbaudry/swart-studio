a1ee783990bcd06d716bab8134e5bdd504f817fa
bbaudry <baudry@kth.se>
2025-10-11 23:21:44 -0400

    {-.}><{.-}

--- p5-experiments/amour001-core.js
+++ p5-experiments/amour001-core.js
@@ -1,5 +1,5 @@
 var xoff = 0.0
-var xinc = 1
+var xinc = 0.0001
 
 function hal() {
     let x1,y1,x2,y2,x3,y3,x4,y4
@@ -13,16 +13,16 @@ function hal() {
         y1=cy+midy*sin(radians(angle))
         x2=cx-midx*sin(radians(angle))
         y2=cy-midy*sin(radians(angle))
-        angle+=noise(xoff)*5;xoff+=xinc
+        angle+=noise(xoff);xoff+=xinc
         x3=cx+midx*sin(radians(angle))
         y3=cy+midy*sin(radians(angle))
         x4=cx-midx*sin(radians(angle))
         y4=cy-midy*sin(radians(angle))
         //quad(x1,y1,x3,y3,x3,y4,x1,y2)
-        quad(x1,y1,x1,y2,x3,y4,x3,y3)
-        quad(x1,y1,x3,y3,x4,y3,x2,y1)
+        //quad(x1,y1,x1,y2,x3,y4,x3,y3)
+        //quad(x1,y1,x3,y3,x4,y3,x2,y1)
         quad(x1,y1,x3,y3,x4,y3,x2,y2)
-        quad(x2,y2,x4,y4,x3,y4,x1,y2)
+        quad(x2,y2,x4,y4,x3,y3,x1,y2)
     }
 }
 
