1923073bd2e972672b9f06e15ae9501f1d155973
bbaudry <baudry@kth.se>
2025-10-11 22:51:51 -0400

    }]}[><]{[{

---
+++ p5-experiments/amour001-core.js
@@ -0,0 +1,28 @@
+var xoff = 0.0
+var xinc = 1
+
+function hal() {
+    let x1,y1,x2,y2,x3,y3,x4,y4
+    let cx=leftmargin+actualwidth*0.5
+    let cy=topmargin+actualheight*0.5
+    let midx=actualwidth*0.5
+    let midy=actualheight*0.5
+    let angle=0
+    while(angle<85){
+        x1=cx+midx*sin(radians(angle))
+        y1=cy+midy*sin(radians(angle))
+        x2=cx-midx*sin(radians(angle))
+        y2=cy-midy*sin(radians(angle))
+        angle+=noise(xoff)*5;xoff+=xinc
+        x3=cx+midx*sin(radians(angle))
+        y3=cy+midy*sin(radians(angle))
+        x4=cx-midx*sin(radians(angle))
+        y4=cy-midy*sin(radians(angle))
+        //quad(x1,y1,x3,y3,x3,y4,x1,y2)
+        quad(x1,y1,x1,y2,x3,y4,x3,y3)
+        quad(x1,y1,x3,y3,x4,y3,x2,y1)
+        quad(x1,y1,x3,y3,x4,y3,x2,y2)
+        quad(x2,y2,x4,y4,x3,y4,x1,y2)
+    }
+}
+
