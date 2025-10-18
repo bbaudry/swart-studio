364b4622263263b4d763ef792c4a555febbcb2f6
bbaudry <baudry@kth.se>
2025-10-17 22:46:41 -0400

    +-..><..-+

--- p5-experiments/plein015.js
+++ p5-experiments/plein015.js
@@ -12,21 +12,21 @@ function preload() {
         sourcecode = loadStrings(artname+'-core.js');
 }
 function setup() {
-    w = Math.floor(8.5 * 96)
-    h = Math.floor(11 * 96)
-    //cnv = createCanvas(w, h, SVG).mousePressed(savesvg);
-    cnv = createCanvas(w, h).mousePressed(savepng);
+    w = Math.floor(96*210/25.4)
+    h = Math.floor(96*297/25.4)
+    cnv = createCanvas(w, h, SVG).mousePressed(savesvg);
+    //cnv = createCanvas(w, h).mousePressed(savepng);
     centerCanvas();
-    leftmargin = Math.floor(w * 0.03)
-    rightmargin = Math.floor(w * 0.97)
-    topmargin = Math.floor(h * 0.02)
-    bottommargin = Math.floor(h * 0.7)
+    leftmargin = Math.floor(w * 0.05)
+    rightmargin = Math.floor(w * 0.95)
+    topmargin = Math.floor(h * 0.05)
+    bottommargin = Math.floor(h * 0.95)
     actualwidth = rightmargin - leftmargin
     actualheight = bottommargin - topmargin
     colorMode(HSB, 360, 100, 100, 250);
     //96*0.2/25.4 : 0.2mm is the width of a fineliner
     //0.04 * 96 : 0.04 inch is 1 mm, the width of stabilo 68/32
-    penwidth =96*0.2/25.4
+    penwidth =96*0.8/25.4
     strokeWeight(penwidth)
 }
 
@@ -54,10 +54,10 @@ function draw() {
     textSize(fSize)
     stroke(300,100,100)   
     hal()
-    stroke(0, 0, 0); noFill(); strokeWeight(1)
-    let c = showcodeall(leftmargin * 4.2, bottommargin)
-    text("plein014[automne]", c[0], c[1] + fSize)
-    text("p5.js + axidraw [almyre::2025]", c[0], c[1] + 2*fSize)
+    // stroke(0, 0, 0); noFill(); strokeWeight(1)
+    // let c = showcodeall(leftmargin * 4.2, bottommargin)
+    // text("plein014[automne]", c[0], c[1] + fSize)
+    // text("p5.js + axidraw [almyre::2025]", c[0], c[1] + 2*fSize)
     noLoop()
 }
 
