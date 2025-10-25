ebf50c82c1ce4990f14f772cb1b6d40e3bd9895a
bbaudry <baudry@kth.se>
2025-10-25 18:34:17 -0400

    ]°[+><+]°[

--- p5-experiments/amour003-core.js
+++ p5-experiments/amour003-core.js
@@ -3,18 +3,20 @@ var noiseres = 0.001
 var resolutionx = 2
 
 function hal() {
-    let x,stepy,angle,xzone
+    let x1,x2,stepy,angle,cx
     resolutiony = Math.floor(random(7,37))
     stepy = Math.floor(actualheight / resolutiony)
     for (let j = 0; j < resolutiony - 1; j++){
         xoff = random(100.0)
-        x = leftmargin
-        angle=90
-        xzone=actualwidth*0.5
-        while (angle<180) {
-            line(x, topmargin + j * stepy, x, topmargin + (j + 1)*stepy)
-            x = x+(sin(radians(angle)))*(xzone-x)*0.5*noise(xoff);xoff+=noiseres
-            angle+=1
+        angle=0
+        cx=rightmargin//leftmargin+actualwidth*0.5
+        x1=cx;x2=cx
+        while (angle<90) {
+            line(x1, topmargin + j * stepy, x1, topmargin + (j + 1)*stepy)
+            line(x2, topmargin + j * stepy, x2, topmargin + (j + 1)*stepy)
+            //x1 = cx+(sin(radians(angle)))* actualwidth * 0.5//*noise(xoff);xoff+=noiseres
+            x2 = cx+(sin(radians(angle+270)))* actualwidth * 0.5*noise(xoff);xoff+=noiseres
+            angle+=3
         }
     }
 }
