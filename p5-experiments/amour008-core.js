cd935dec7aafcc2fa79c7dfa98614c079478506b
bbaudry <baudry@kth.se>
2025-11-05 22:01:58 -0500

    +*+.><.+*+

--- p5-experiments/amour008-core.js
+++ p5-experiments/amour008-core.js
@@ -15,62 +15,93 @@ function center() {
     east = []; south = []; west = []; north = []
     rowheight = 18
     columnwidth = 18
-    ampli = 7
-    stepx = 1; stepy = 4;
+    ampli = 5
+    stepx = penwidth*2; stepy = penwidth*5;
     x1 = columnwidth * 0.5; y1 = -rowheight * 0.5 // going east
     x2 = -columnwidth * 0.5; y2 = rowheight * 0.5 // going south
     x3 = -columnwidth * 0.5; y3 = -rowheight * 0.5 // going west
     x4 = -columnwidth * 0.5; y4 = -rowheight * 0.5 // going north
     noStroke()//strokeWeight(0.5); stroke(100, 100, 100)
-    
+
+    east.push(x1)
+    south.push(y2)
+    west.push(x3)    
+    north.push(y4)
+ 
     while (x1 + stepx < actualwidth * 0.5) {
-        east.push(x1)
-        west.push(x2)
-        south.push(y2)
-        north.push(y4)
 
         setfill(0.5)
-        rect(x1, y1, stepx, rowheight)
+//        rect(x1, y1, stepx, rowheight)
+//        quad(x1,y1,x1+stepx,y1,x1+stepx,y1+rowheight,x1,y1+rowheight)
+//        plottablequad(x1,y1,x1+stepx,y1,x1+stepx,y1+rowheight,x1,y1+rowheight)
         x1 += stepx
+        east.push(x1)
         stepx += ampli * noise(xoff); xoff += xinc
 
         setfill(0.5)
         rect(x2, y2, columnwidth, stepy)
         y2 += stepy
+        south.push(y2)
         stepy += ampli * noise(xoff); xoff += xinc; 
 
         setfill(0.5)
-        rect(x3, y3, stepx, rowheight); 
+        rect(x3-stepx, y3, stepx, rowheight)
         x3 -= stepx
+        west.push(x3)
         stepx += ampli * noise(xoff); xoff += xinc
 
         setfill(0.5)
-        rect(x4, y4, columnwidth, stepy)
+        rect(x4, y4-stepy, columnwidth, stepy)//; push(); stroke(300,100,100); noFill(); line(x4-10,y4-stepy,x4,y4-stepy); pop()
         y4 -= stepy
+        north.push(y4)
         stepy += ampli * noise(xoff); xoff += xinc; 
     }
 }
 
 function periphery(){
     let x1,x2,y1,y2
+    // south east
     for(let i=0; i< east.length-1; i++){
         x1=east[i]
         x2=east[i+1]
         for(let j=0;j<south.length-1; j++){
             y1=south[j]
             y2=south[j+1]
-            setfill(0.3)
-            quad(x1,y1,x2,y1,x2,y2,x1,y2)
+            setfill(0.5)
+            //noFill()
+            plottablequad(x1,y1,x2,y1,x2,y2,x1,y2)
         }
     }
+    //north west
     for(let i=0; i< west.length-1; i++){
         x1=west[i]
         x2=west[i+1]
         for(let j=0;j<north.length-1; j++){
             y1=north[j]
             y2=north[j+1]
-            setfill(0.3)
-            console.log("go west")
+            setfill(0.5)
+            quad(x1,y1,x2,y1,x2,y2,x1,y2)
+        }
+    }
+    //south west
+    for(let i=0; i< west.length-1; i++){
+        x1=west[i]
+        x2=west[i+1]
+        for(let j=0;j<north.length-1; j++){
+            y1=south[j]
+            y2=south[j+1]
+            setfill(0.5)
+            quad(x1,y1,x2,y1,x2,y2,x1,y2)
+        }
+    }
+    //north east
+    for(let i=0; i< west.length-1; i++){
+        x1=east[i]
+        x2=east[i+1]
+        for(let j=0;j<north.length-1; j++){
+            y1=north[j]
+            y2=north[j+1]
+            setfill(0.5)
             quad(x1,y1,x2,y1,x2,y2,x1,y2)
         }
     }
@@ -80,4 +111,15 @@ function setfill(proba){
     if(random()<proba){fill(0,0,100)}
     else{fill(0,0,0)}
     
+}
+
+function plottablequad(x1,y1,x2,y2,x3,y3,x4,y4){
+    let largeur=x2-x1
+    let hauteur=y4-y1
+    console.log('hi')
+    push()
+    stroke(0,0,100)
+    strokeWeight(penwidth)
+    rect(x1+penwidth*0.5,y1+penwidth*0.5,largeur,hauteur)
+    pop()
 }
