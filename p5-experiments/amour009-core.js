a298c233510ff2e48a550eef51b5f3e8bc6724af
bbaudry <baudry@kth.se>
2025-11-16 14:07:20 -0500

    .[*.><.*].

--- p5-experiments/amour009-core.js
+++ p5-experiments/amour009-core.js
@@ -4,7 +4,36 @@ xinc = 0.0001
 
 function hal() {
     flaque1()
+    //courbe()
 }
+
+function courbe(){
+    let cx, cy, r1, r2, a1, a2, x1, y1, cx1, cy1, cx2, cy2, x2, y2, rseed
+    cx=w*0.4
+    cy=h*0.3
+    r1=300
+    r2=320
+    a1=40
+    a2=130
+    x1=cx+r1*cos(a1)
+    y1=cy+r1*sin(a1)
+    ellipse(x1,y1,7,7)
+    cx1=x1//cx+(r1+100)*cos(a1)
+    cy1=cy+(r1+100)*sin(a1)
+    ellipse(cx1,cy1,7,7)
+    x2=cx+r2*cos(a2)
+    y2=cy+r2*sin(a2)
+    ellipse(x2,y2,7,7)
+    cx2=x2//cx+(r2+100)*cos(a2)
+    cy2=cy+(r2+100)*sin(a2)
+    ellipse(cx2,cy2,7,7)
+    beginShape()
+    vertex(x1,y1)
+    bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
+    endShape()
+}
+
+
 function flaque1(){
     let cx, cy, r1, r2, a1, a2, x1, y1, cx1, cy1, cx2, cy2, x2, y2, rseed
     cx=w*0.4
@@ -27,7 +56,7 @@ function flaque1(){
     y2=cy+r1*sin(a2)
     cx2=cx+(r1+100)*cos(a2)
     cy2=cy+(r1+100)*sin(a2)
-    bezierVertex(x1, y1, cx1, cy1, cx2, cy2, x2, y2)
+    bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
     
     a1=120
     a2=200
@@ -40,7 +69,7 @@ function flaque1(){
     y2=cy+r1*sin(a2)
     cx2=cx+(r1+100)*cos(a2)
     cy2=cy+(r1+100)*sin(a2)
-    bezierVertex(x1, y1, cx1, cy1, cx2, cy2, x2, y2)
+    bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
 
     a1=200
     a2=300
@@ -53,7 +82,7 @@ function flaque1(){
     y2=cy+r1*sin(a2)
     cx2=cx+(r1+100)*cos(a2)
     cy2=cy+(r1+100)*sin(a2)
-    bezierVertex(x1, y1, cx1, cy1, cx2, cy2, x2, y2)
+    bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
 
     a1=300
     a2=360
@@ -66,7 +95,7 @@ function flaque1(){
     y2=cy+r1*sin(a2)
     cx2=cx+(r1+100)*cos(a2)
     cy2=cy+(r1+100)*sin(a2)
-    bezierVertex(x1, y1, cx1, cy1, cx2, cy2, x2, y2)
+    bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
 
     endShape(CLOSE)
     rseed+=17
