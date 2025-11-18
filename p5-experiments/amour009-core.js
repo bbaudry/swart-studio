6754b102276b0c54d26f2ba5353e61ce63b4c9b2
bbaudry <baudry@kth.se>
2025-11-17 22:29:47 -0500

    .+{]><[}+.

--- p5-experiments/amour009-core.js
+++ p5-experiments/amour009-core.js
@@ -8,44 +8,54 @@ function hal() {
 }
 
 
-function flaque2(){
-    let xeast,yeast,xsouth,ysouth,xwest,ywest,xnorth,ynorth,x1, y1, cx1, cy1, cx2, cy2, x2, y2, offset, flaquewidth, flaqueheight
-    offset=6    
-    xeast=leftmargin+actualwidth*noise(xoff);  yeast=topmargin+actualheight*0.3;
-    xsouth=leftmargin+actualwidth*0.3;         ysouth=topmargin+actualheight*0.5*noise(xoff);
-    xwest=leftmargin+actualwidth*0.1;          ywest=topmargin+actualheight*0.36;
-    xnorth=leftmargin+actualwidth*0.5;         ynorth=topmargin+actualheight*0.1;
-    flaquewidth=xeast-xwest;                            flaqueheight=ysouth-ynorth
-    for(let i=0; i<42; i++){
-    xeast-=i*offset; ysouth-=i*offset;
-    xwest+=i*offset;   ynorth+=i*offset;
+function flaque2() {
+    let xe, ye, xs, ys, xw, yw, xn, yn, x1, y1, cx1, cy1, cx2, cy2, x2, y2, offset, pad, flaquewidth, flaqueheight
+    offset = 6
+    pad=0.2
+    xe = leftmargin + actualwidth * noise(xoff); ye = topmargin + actualheight * 0.3; xoff += xinc
+    xs = leftmargin + actualwidth * 0.3; ys = topmargin + actualheight * 0.5 * noise(xoff); xoff += xinc
+    xw = leftmargin + actualwidth * 0.1; yw = topmargin + actualheight * 0.36;
+    xn = leftmargin + actualwidth * 0.5; yn = topmargin + actualheight * 0.1;
+    flaquewidth = xe - xw; flaqueheight = ys - yn
+    let xeast, yeast, xsouth, ysouth, xwest, ywest, xnorth, ynorth
+    yeast=ye;xsouth=xs;ywest=yw;xnorth=xn
+    for (let i = 0; i < 2; i++) {
+        xeast = xe - i * offset; ysouth = ys - i * offset;
+        xwest = xw + i * offset; ynorth = yn + i * offset;
+        
+        beginShape()
+        x1 = xeast; y1 = yeast
+        cx1 = xeast; cy1 = yeast + flaqueheight * pad - i * offset
+        cx2 = xsouth + flaquewidth * pad - i * offset; cy2 = ysouth
+        x2 = xsouth; y2 = ysouth
+        vertex(x1, y1)
+        bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
 
-    beginShape()
-    x1=xeast;y1=yeast
-    cx1=xeast;cy1=yeast+flaqueheight*0.3-i*offset
-    cx2=xsouth+flaquewidth*0.3-i*offset;cy2=ysouth
-    x2=xsouth;y2=ysouth
-    vertex(x1,y1)
-    bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
+        x1 = xsouth; y1 = ysouth
+        cx1 = xsouth - flaquewidth * pad + i * offset; cy1 = ysouth
+        cx2 = xwest; cy2 = ywest + flaqueheight * pad - i * offset
+        x2 = xwest; y2 = ywest
+        bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
 
-    x1=xsouth;y1=ysouth
-    cx1=xsouth-flaquewidth*0.3+i*offset;cy1=ysouth
-    cx2=xwest;cy2=ywest+flaqueheight*0.3-i*offset
-    x2=xwest;y2=ywest
-    bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
+        x1 = xwest; y1 = ywest
+        cx1 = xwest; cy1 = ywest - flaqueheight * pad + i * offset
+        cx2 = xnorth - flaquewidth * pad + i * offset; cy2 = ynorth
+        x2 = xnorth; y2 = ynorth
+        bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
 
-    x1=xwest;y1=ywest
-    cx1=xwest;cy1=ywest-flaqueheight*0.3+i*offset
-    cx2=xnorth-flaquewidth*0.3+i*offset;cy2=ynorth
-    x2=xnorth;y2=ynorth
-    bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
-
-    x1=xnorth;y1=ynorth
-    cx1=xnorth+flaquewidth*0.3-i*offset;cy1=ynorth
-    cx2=xeast;cy2=yeast-flaqueheight*0.3+i*offset
-    x2=xeast;y2=yeast
-    bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
-    endShape()
+        x1 = xnorth; y1 = ynorth
+        cx1 = xnorth + flaquewidth * pad - i * offset; cy1 = ynorth
+        cx2 = xeast; cy2 = yeast - flaqueheight * pad + i * offset
+        x2 = xeast; y2 = yeast
+        bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
+        endShape()
     }
+        push()
+    stroke(50,100,100); ellipse(xs,ys,7,7)
+    stroke(150,100,100); ellipse(xn,yn,7,7)
+    stroke(200,100,100); ellipse(xe,ye,7,7)
+    stroke(250,100,100); ellipse(xw,yw,7,7)
+    pop()
+
 }
 
