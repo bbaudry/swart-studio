19e507c9afd13a7aa15ed6aea5ee61848ce5df70
bbaudry <baudry@kth.se>
2025-11-24 07:17:59 -0500

    [-.}><{.-]

--- p5-experiments/amour010-core.js
+++ p5-experiments/amour010-core.js
@@ -8,13 +8,13 @@ function hal() {
     test()
 }
 
-function test(){
-    cx=w*0.5
-    cy=h*0.5
-    a=random(0,360)
-    p=onedge(cx,cy,a)
-    ellipse(cx,cy,7,7)
-    ellipse(p.x,p.y,7,7)
+function test() {
+    cx = w * 0.5
+    cy = h * 0.5
+    a = 42//random(0,360)
+    p = onedge(cx, cy, a)
+    ellipse(cx, cy, 7, 7)
+    ellipse(p.x, p.y, 7, 7)
 }
 
 function fleur2(cx, cy) {
@@ -39,7 +39,7 @@ function fleur2(cx, cy) {
         if (high) {
             if (random() < 0.1) {
                 line(x1, y1, x2, y2)
-                blow(cx,cy,a1,a2,x2,y2,x4,y4)
+                blow(cx, cy, a1, a2, x2, y2, x4, y4)
                 line(x4, y4, x3, y3)
             }
             else {
@@ -79,35 +79,49 @@ function fleur2(cx, cy) {
     }
 }
 
-function blow(cx,cy,a1,a2,x2,y2,x4,y4){
-    let r1,r2,dx1,dy1,dx2,dy2
-    r1=dist(cx,cy,x2,y2)
-    r2=dist(cx,cy,x4,y4)
-    dx1=cx+actualheight*cos(a1); dx11=constrain(dx1,leftmargin,leftmargin+actualwidth)
-    dy1=cy+actualheight*sin(a1); dy11=constrain(dy1,topmargin,topmargin+actualheight)
-    line(x2,y2,dx1,dy1)
+function blow(cx, cy, a1, a2, x2, y2, x4, y4) {
+    let r1, r2, dx1, dy1, dx2, dy2
+    r1 = dist(cx, cy, x2, y2)
+    r2 = dist(cx, cy, x4, y4)
+    dx1 = cx + actualheight * cos(a1); dx11 = constrain(dx1, leftmargin, leftmargin + actualwidth)
+    dy1 = cy + actualheight * sin(a1); dy11 = constrain(dy1, topmargin, topmargin + actualheight)
+    line(x2, y2, dx1, dy1)
 }
 
-function onedge(cx,cy,a){
-    let b,r,x,y
-    if(a<=45 || a>315){
-        b=rightmargin-cx
+// determine coordinates of a point on the edge of the canvas
+// https://www.alloprof.qc.ca/fr/eleves/bv/mathematiques/les-rapports-trigonometriques-m1287
+function onedge(cx, cy, a) {
+    let b, r, x, y
+    a = 315.2
+    console.log("cx: " + cx + "; cy: " + cy + "; a: " + a + "; left:  " + leftmargin + "; right:  " + rightmargin + "; top:  " + topmargin + "; bottom:  " + bottommargin)
+    if (a <= 45 || a > 315) {
+        b = rightmargin - cx
+            r=b/cos(a)
+    } else {
+        if (a <= 135 || a > 45) {
+            b = bottommargin - cy
+                r=b/cos(Math.abs(90-a))
+
+        } else {
+            if (a <= 225 || a > 135) {
+                b = cx - leftmargin
+            }
+            else {
+                if (a <= 315 || a > 225) {
+                    b = cy - topmargin
+                }
+            }
+        }
     }
-    if(a<=135 || a>45){
-        b=bottommargin-cy
-    }
-    if(a<=225 || a>135){
-        b=cx-leftmargin
-    }
-    if(a<=315 || a>225){
-        b=cy-topmargin
-    }
-    r=b/cos(a)
+    x = cx + b * cos(a)
+    y = cy + b * sin(a)
+    //ellipse(x, y, 7, 7)
+    console.log("x: " + x + "; y: " + y + "; b: " + b)
     x=cx+r*cos(a)
     y=cy+r*sin(a)
-    console.log(a+" "+x+" "+y+" "+leftmargin+" "+rightmargin+" "+topmargin+" "+bottommargin)
-    ellipse(x,y,7,7)
-    return createVector(x,y)
+    console.log("x: "+x+"; y: "+y+"; r: "+r)
+    ellipse(x, y, 7, 7)
+    return createVector(x, y)
 }
 
 function fleur1() {
