7c8434fce33c6e0ac5cd3f67a6f0f35234647f6d
bbaudry <baudry@kth.se>
2025-12-16 22:08:17 -0500

    +]]°><°[[+

--- p5-experiments/plottable/vieenrose002-core.js
+++ p5-experiments/plottable/vieenrose002-core.js
@@ -9,15 +9,16 @@ function hal() {
 
 function ribbon() {
     let x1, y1, cx1, cy1, cx2, cy2, x2, y2
-    for (i = 0; i < 142; i += penwidth*2) {
+    for (i = 0; i < 142; i += penwidth) {
         x1 = topmargin
-        y1 = leftmargin + actualwidth * 0.32 + i * 3
+        y1 = leftmargin + actualwidth * 0.42 + i * 3
         cx1 = topmargin + actualheight * 0.3 * noise(xoff); xoff += xinc
-        cy1 = leftmargin + actualwidth * 0.1 * (1 - 2 * noise(xoff)); xoff += xinc
+        cy1 = y1-0.3*noise(xoff)//leftmargin + actualwidth * 0.1 * (1 - 2 * noise(xoff)); 
+        xoff += xinc
         cx2 = topmargin + actualheight * 0.6 * noise(xoff); xoff += xinc
-        cy2 = leftmargin + actualwidth * 0.6 - i//*(1-2*noise(xoff));xoff+=xinc
+        cy2 = leftmargin + actualwidth * 0.6 +i// * (1-2*noise(xoff));xoff+=xinc
         x2 = bottommargin
-        y2 = leftmargin + actualwidth * 0.2 + i * 7 * noise(xoff); xoff += xinc
+        y2 = leftmargin + actualwidth * 0.1 + i * 5// * noise(xoff); xoff += xinc
         //    line(x1,y1,cx1,cy1);line(cx1,cy1,cx2,cy2);line(cx2,cy2,x2,y2)
         beginShape()
         vertex(x1, y1)
