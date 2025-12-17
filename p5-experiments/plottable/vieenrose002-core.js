971e04fe6e7eac7d18156de5476ff467065c1ec4
bbaudry <baudry@kth.se>
2025-12-16 22:41:49 -0500

    *.°}><{°.*

--- p5-experiments/plottable/vieenrose002-core.js
+++ p5-experiments/plottable/vieenrose002-core.js
@@ -4,6 +4,7 @@ xinc = 0.01; yinc = 0.6
 
 function hal() {
     ribbon()
+    ribbon2()
     ribbon()
 }
 
@@ -18,11 +19,31 @@ function ribbon() {
         cx2 = topmargin + actualheight * 0.6 * noise(xoff); xoff += xinc
         cy2 = leftmargin + actualwidth * 0.6 +i// * (1-2*noise(xoff));xoff+=xinc
         x2 = bottommargin
-        y2 = leftmargin + actualwidth * 0.1 + i * 5// * noise(xoff); xoff += xinc
+        y2 = leftmargin + actualwidth * 0.42 + i * 3.1// * noise(xoff); xoff += xinc
         //    line(x1,y1,cx1,cy1);line(cx1,cy1,cx2,cy2);line(cx2,cy2,x2,y2)
         beginShape()
         vertex(x1, y1)
         bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
+        endShape()
+    }
+}
+
+function ribbon2() {
+    let x1, y1, cx1, cy1, cx2, cy2, x2, y2
+    for (i = 0; i < 162; i += penwidth) {
+        x1 = topmargin
+        y1 = leftmargin + i * 2
+        cx1 = topmargin + actualheight * 0.4 * noise(xoff); xoff += xinc
+        cy1 = y1+0.3*noise(xoff)//leftmargin + actualwidth * 0.1 * (1 - 2 * noise(xoff)); 
+        xoff += xinc
+        cx2 = topmargin + actualheight * 0.5 * noise(xoff); xoff += xinc
+        cy2 = cy1+1.7*noise(xoff)// * (1-2*noise(xoff));xoff+=xinc
+        x2 = bottommargin
+        y2 = leftmargin + actualwidth * 0.05 + i * 3// * noise(xoff); xoff += xinc
+            line(x1,y1,cx1,cy1);line(cx1,cy1,cx2,cy2);line(cx2,cy2,x2,y2)
+        beginShape()
+        vertex(x1, y1)
+        //bezierVertex(cx1, cy1, cx2, cy2, x2, y2)
         endShape()
     }
 }
