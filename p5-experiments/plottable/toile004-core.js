40a029354b392409c5cd5cdf1da297cdc26ec0c2
bbaudry <baudry@kth.se>
2025-12-27 22:34:19 -0500

    -[.{><}.]-

--- p5-experiments/plottable/toile004-core.js
+++ p5-experiments/plottable/toile004-core.js
@@ -1,6 +1,6 @@
 let xoff, xinc, yoff, yinc, innerradii, outerradii
 xoff = 0.0; yoff = 0.0
-xinc = 0.01; yinc = 0.1
+xinc = 0.001; yinc = 0.1
 
 function hal() {
     rect(leftmargin, topmargin, actualwidth, actualheight)
@@ -16,22 +16,24 @@ function multihelice(){
     inangle=0
     inrad=42
     inangleinc=random(1,3)
-    inradinc=0.5
+    inradinc=0.1
     outangle=0
     outrad=actualwidth*0.1
     outangleinc=0.6
     outradinc=0.1
-    diam=122
-    for(let i=0;i<3*360;i++){
+    diam=142*noise(xoff);xoff+=xinc
+    for(let i=0;i<2.5*360;i++){
         cx=inrad*cos(inangle)
         cy=inrad*sin(inangle)
         x=cx+outrad*cos(outangle)
         y=cy+outrad*sin(outangle)
+    //if(x-leftmargin<diam*0.5 || rightmargin-x<diam*0.5){text("x: "+x+"; l: "+leftmargin+"; r: "+rightmargin,0,actualheight*0.55);break;}
         ellipse(x,y,diam,diam)
         inangle+=inangleinc;
-        //inrad+=inradinc
+        inrad+=inradinc
         outangle+=outangleinc
         outrad+=0.3*noise(xoff);xoff+=xinc
+    diam=142*noise(xoff);xoff+=xinc
     }
 }
 
