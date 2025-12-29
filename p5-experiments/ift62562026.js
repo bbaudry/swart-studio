10ff281649da694e769413e4f9c94446fcd4f43f
bbaudry <baudry@kth.se>
2025-12-29 10:22:54 -0500

    .{-*><*-}.

---
+++ p5-experiments/ift62562026.js
@@ -0,0 +1,70 @@
+
+var w, h
+var cnv
+var leftmargin, rightmargin, topmargin, bottommargin, actualheight, actualwidth, penwidth
+var sourcecode
+var font
+var fSize = 99
+var artname = "amour001"
+
+function preload() {
+    font = loadFont("./fonts/ChunkFive-Regular.otf");
+}
+function setup() {
+    w = Math.floor(8.5 * 96)
+    h = Math.floor(11 * 96)
+    //cnv = createCanvas(w, h, SVG).mousePressed(savesvg);
+    cnv = createCanvas(w, h).mousePressed(savepng);
+    centerCanvas();
+    leftmargin = Math.floor(w * 0.03)
+    rightmargin = Math.floor(w * 0.97)
+    topmargin = Math.floor(h * 0.02)
+    bottommargin = Math.floor(h * 0.7)
+    actualwidth = rightmargin - leftmargin
+    actualheight = bottommargin - topmargin
+    colorMode(HSB, 360, 100, 100, 250);
+    //96*0.2/25.4 : 0.2mm is the width of a fineliner
+    //0.04 * 96 : 0.04 inch is 1 mm, the width of stabilo 68/32
+    penwidth =96*0.2/25.4
+    strokeWeight(penwidth)
+}
+
+function savesvg() {
+    save(artname+".svg");
+}
+
+
+function savepng() {
+    save(artname+".png");
+}
+
+
+function centerCanvas() {
+    var x = (windowWidth - windowHeight) / 2;
+    var y = (windowHeight - windowHeight) / 2;
+    cnv.position(x, y);
+}
+
+
+function draw() {
+    background(200, 100, 100)
+    noStroke()
+    fill(30,100,100)
+    fSize=actualheight*0.27
+    textFont(font)
+    textSize(fSize)
+    a="art"
+    x=(actualwidth-textWidth(a))*0.5
+    y=topmargin+fSize
+    text(a,x,y)
+    a="géné"
+    x=(actualwidth-textWidth(a))*0.5
+    y=topmargin+fSize*2
+    text(a,x,y)
+    a="ratif"
+    x=(actualwidth-textWidth(a))*0.5
+    y=topmargin+fSize*3
+    text(a,x,y)
+    noLoop()
+}
+
--- p5-experiments/plottable/toile004-core.js
+++ p5-experiments/plottable/toile004-core.js
@@ -1,58 +1,35 @@
-let xoff, xinc, yoff, yinc, innerradii, outerradii
-xoff = 0.0; yoff = 0.0
-xinc = 0.001; yinc = 0.1
+let xoff, xinc
+xoff = 0.0;
+xinc = 0.001;
 
 function hal() {
     rect(leftmargin, topmargin, actualwidth, actualheight)
     push()
     translate(leftmargin + actualwidth * 0.5, topmargin + actualheight * 0.5)
-    //ellipse(0,0,42,42)
     multihelice()
     pop()
 }
 
-function multihelice(){
-    let cx,cy,x,y,inrad,inangle,inangleinc,inradinc,outrad,outangle,outangleinc,outradinc,diam
-    inangle=0
-    inrad=42
-    inangleinc=random(1,3)
-    inradinc=0.1
-    outangle=0
-    outrad=actualwidth*0.1
-    outangleinc=0.6
-    outradinc=0.1
-    diam=142*noise(xoff);xoff+=xinc
-    for(let i=0;i<2.5*360;i++){
-        cx=inrad*cos(inangle)
-        cy=inrad*sin(inangle)
-        x=cx+outrad*cos(outangle)
-        y=cy+outrad*sin(outangle)
-    //if(x-leftmargin<diam*0.5 || rightmargin-x<diam*0.5){text("x: "+x+"; l: "+leftmargin+"; r: "+rightmargin,0,actualheight*0.55);break;}
-        ellipse(x,y,diam,diam)
-        inangle+=inangleinc;
-        inrad+=inradinc
-        outangle+=outangleinc
-        outrad+=0.3*noise(xoff);xoff+=xinc
-    diam=142*noise(xoff);xoff+=xinc
+function multihelice() {
+    let cx, cy, x, y, inrad, inangle, inangleinc, inradinc, outrad, outangle, outangleinc, outradinc, diam
+    inangle = 0
+    inrad = 42
+    inangleinc = random(1, 3)
+    inradinc = 0.1
+    outangle = 0
+    outrad = actualwidth * 0.1
+    outangleinc = 0.6
+    outradinc = 0.1
+    diam = 142
+    for (let i = 0; i < 2.5 * 360; i++) {
+        cx = inrad * cos(inangle)
+        cy = inrad * sin(inangle)
+        x = cx + outrad * cos(outangle)
+        y = cy + outrad * sin(outangle)
+        ellipse(x, y, diam, diam)
+        inangle += inangleinc;
+        inrad += inradinc
+        outangle += outangleinc
+        outrad += 0.3 * noise(xoff); xoff += xinc
     }
 }
-
-function helice() {
-    let angle, diameter, cx, cy, rayon
-    rayon = 0.5 * actualwidth * (1 - 2 * noise(xoff)); xoff += xinc
-    diameter = actualwidth * 0.3 * noise(xoff); xoff += xinc
-    angle = 360 * (1 - 2 * noise(yoff)); yoff += yinc
-    for (let i = 0; i < 365; i++) {
-        console.log(angle+" "+rayon)
-        cx = rayon * cos(angle)
-        cy = rayon * sin(angle)
-        ellipse(cx, cy, diameter, diameter)
-        //ellipse(cx,cy,3,3)
-        rayon =0.5 * actualwidth * (1 - 2 * noise(xoff)); xoff += xinc
-        diameter =Math.abs(0.2*actualwidth) * noise(xoff); xoff += xinc
-        angle = 720* (1 - 2 * noise(yoff)); yoff += yinc
-    }
-    noLoop()
-}
-
-
