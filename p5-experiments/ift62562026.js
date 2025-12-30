2812a86ee7e65e9dff0f88e5c8628c6dc0837a08
bbaudry <baudry@kth.se>
2025-12-30 17:23:23 -0500

    ]}]+><+[{[

--- p5-experiments/ift62562026.js
+++ p5-experiments/ift62562026.js
@@ -1,47 +1,56 @@
 
 var w, h
 var cnv
-var leftmargin, rightmargin, topmargin, bottommargin, actualheight, actualwidth, penwidth, count
+var leftmargin, rightmargin, topmargin, bottommargin, actualheight, actualwidth, penwidth, count, charsperline
 var sourcecode
 var font
-var fSize = 99
-var characters = ['#', '|', ':', '+', '/', '-', '=']
+var fSize
+var characters = [ '|', '+', '/', '-', '=', '\\']
 var beat = 84
 var lines = [
-    "_______________",
-    "__ART____GEN___",
-    "____ERATIF_____",
-    "_______________",
-    "_______ALGO____",
-    "___RITHMI______",
-    "_________QUE___",
-    "_______________",
-    "_______________"
+    "__________",
+    "__ART_____",
+    "_____GEN__",
+    "__ERATIF__",
+    "__________",
+    "___ALGO___",
+    "___RITHMI_",
+    "_____QUE__",
+    "__________",
+    "__________"
 ]
+//14
+var template
 
 function preload() {
     font = loadFont("./fonts/FreeMonoBold.otf");
 }
 function setup() {
-    w = 600
-    h = 600
+    fSize = 42//actualheight * 0.08
+    charsperline = 10
+    w = fSize * 10 + fSize * 2
+    h = fSize * 10 + fSize * 2
     //cnv = createCanvas(w, h, SVG).mousePressed(savesvg);
     cnv = createCanvas(w, h).mousePressed(savepng);
     centerCanvas();
-    leftmargin = Math.floor(w * 0.03)
-    rightmargin = Math.floor(w * 0.97)
-    topmargin = Math.floor(h * 0.02)
-    bottommargin = Math.floor(h * 0.7)
+    leftmargin = fSize
+    rightmargin = w - fSize
+    topmargin = fSize
+    bottommargin = h - fSize
     actualwidth = rightmargin - leftmargin
     actualheight = bottommargin - topmargin
     colorMode(HSB, 360, 100, 100, 250);
     //96*0.2/25.4 : 0.2mm is the width of a fineliner
     //0.04 * 96 : 0.04 inch is 1 mm, the width of stabilo 68/32
+    template = []
+    for (i in lines) {
+        for(j in lines[i]){
+        template.push({ c: lines[i][j], flip: true })}
+    }
     penwidth = 96 * 0.2 / 25.4
     strokeWeight(penwidth)
     count = 0
     noStroke()
-    fSize = actualheight * 0.155
     textFont(font)
     textSize(fSize)
 
@@ -65,35 +74,38 @@ function centerCanvas() {
 
 
 function draw() {
-    if (count % beat < 42) {
-        background(200, 100, 100)
-        fill(30, 100, 100)
-        for (let ind = 0; ind < lines.length; ind++) {
-            textlinerand(lines[ind], fSize * (1 + ind))
-        }
-    }
-    else {
-        background(0, 100, 100)
-        fill(180, 0, 0)
-        for (let ind = 0; ind < lines.length; ind++) {
-            textlinefix(lines[ind], fSize * (1 + ind))
-        }
-        //noLoop()
-    }
-    count++
+    flip()
 }
 
-function textlinerand(arg, posy) {
-    let a = ""
-    for (let i = 0; i < arg.length; i++) {
-        a += random(characters)
+function flip() {
+    background(0,0,0)
+    fill(0, 0, 100)
+    let x, y, t, i
+    i = Math.floor(random(template.length))
+    template[i].flip=false
+    x = leftmargin
+    y = topmargin
+
+
+    for (let ind=0;ind<template.length;ind++) {
+        if (ind % charsperline == 0) {
+            x = leftmargin
+            y += fSize
+        }
+        else {
+            x += fSize
+        }
+        if (template[ind].flip) {
+            t=random(characters)
+        }
+        else {
+            if(template[ind].c=='_'){
+                t=random(characters)
+                template[ind].c=t
+                template[ind].flip=false
+            }
+            else{t=template[ind].c}
+        }
+        text(t,x,y)
     }
-    text(a, leftmargin, posy)
-}
-function textlinefix(arg, posy) {
-    let a = ""
-    for (let i = 0; i < arg.length; i++) {
-        arg.charAt(i) == '_' ? a += random(characters) : a += arg.charAt(i)
-    }
-    text(a, leftmargin, posy)
 }
