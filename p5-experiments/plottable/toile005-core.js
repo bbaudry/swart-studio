da899a2ebb13453872860c81388c982f04be2249
bbaudry <benoit.baudry@umontreal.ca>
2026-01-21 22:13:03 -0500

    -*.[><].*-

--- p5-experiments/plottable/toile005-core.js
+++ p5-experiments/plottable/toile005-core.js
@@ -11,10 +11,11 @@ function hal(x,y,drawwidth,drawheight) {
 }
 
 function victor(drawwidth,drawheight){
-    let x1,y1,x2,y2,rad1,rad2,angle,angleinc
+    let x1,y1,x2,y2,rad1,rad2,angle,angleinc,anglerotate
     rotate(random(45))
     rad1=drawwidth*0.01
     angleinc=2
+    anglerotate=angleinc+Math.floor(random(-2,2))
     for(angle=0;angle<110;angle+=angleinc){
         rad2=drawwidth*0.3+drawwidth*0.5*noise(xoff);xoff+=xinc
         console.log(rad2)
@@ -22,7 +23,7 @@ function victor(drawwidth,drawheight){
         y1=rad1*sin(angle)
         x2=rad2*cos(angle)
         y2=rad2*sin(angle)
-        rotate(angleinc)
+        rotate(anglerotate)
         // ellipse(x1,y1,x2,y2)
         quad(x1-x2*0.5,y1-y2*0.5,x1+x2*0.5,y1-y2*0.5,x1+x2*0.5,y1+y2*0.5,x1-x2*0.5,y1+y2*0.5)
     }
