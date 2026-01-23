9cac188261c607b332a0cf633ca6ee2c2a754956
bbaudry <benoit.baudry@umontreal.ca>
2026-01-23 18:29:07 -0500

    {-][><][-}

--- p5-experiments/plottable/toile006-core.js
+++ p5-experiments/plottable/toile006-core.js
@@ -11,13 +11,14 @@ function hal(x,y,drawwidth,drawheight) {
 }
 
 function victor(drawwidth,drawheight){
-    let x1,y1,x2,y2,x3,y3,x4,y4,angle,angleinc,angleinit,rayon
+    let x1,y1,x2,y2,x3,y3,x4,y4,angle,angleinc,angleinit,rayon,ampli
     rayon=drawwidth*0.5
     angle=1//Math.floor(random(3,4))
     angleinit=angle
     angleinc=1
+    ampli=1
     ellipse(0,0,rayon*2,rayon*2)
-    while(angle<90-35){
+    for(let i=0;i<10;i++){
         x1=rayon*cos(angle)
         y1=rayon*sin(angle)
         x2=rayon*cos(180-angle)
@@ -27,8 +28,8 @@ function victor(drawwidth,drawheight){
         x4=rayon*cos(-angle)
         y4=rayon*sin(-angle)
         // quad(x1,y1,x2,y2,x3,y3,x4,y4)
-        bezier(x1,y1,lerp(x1,x2,0.2),y1+drawheight*0.1,lerp(x1,x2,0.8),y1+drawheight*0.1,x2,y2)
-        bezier(x3,y3,lerp(x3,x4,0.2),y3-drawheight*0.1,lerp(x3,x4,0.8),y3-drawheight*0.1,x4,y4)
+        bezier(x1,y1,lerp(x1,x2,0.2),y1+drawheight*0.1*ampli,lerp(x1,x2,0.8),y1+drawheight*0.1*ampli,x2,y2)
+        bezier(x3,y3,lerp(x3,x4,0.2),y3-drawheight*0.1*ampli,lerp(x3,x4,0.8),y3-drawheight*0.1*ampli,x4,y4)
         x1=rayon*cos(90-angle)
         y1=rayon*sin(90-angle)
         x2=rayon*cos(270+angle)
@@ -38,10 +39,11 @@ function victor(drawwidth,drawheight){
         x4=rayon*cos(90+angle)
         y4=rayon*sin(90+angle)
         // quad(x1,y1,x2,y2,x3,y3,x4,y4)
-        bezier(x1,y1,x1+drawwidth*0.1,lerp(y1,y2,0.2),x1+drawwidth*0.1,lerp(y1,y2,0.8),x2,y2)
-        bezier(x3,y3,x3-drawwidth*0.1,lerp(y3,y4,0.2),x3-drawwidth*0.1,lerp(y3,y4,0.8),x4,y4)
-        angle+=5//angleinc
-        angleinc+=1//noise(xoff);xoff+=xinc
+        bezier(x1,y1,x1+drawwidth*0.1*ampli,lerp(y1,y2,0.2),x1+drawwidth*0.1*ampli,lerp(y1,y2,0.8),x2,y2)
+        bezier(x3,y3,x3-drawwidth*0.1*ampli,lerp(y3,y4,0.2),x3-drawwidth*0.1*ampli,lerp(y3,y4,0.8),x4,y4)
+        angle+=angleinc
+        angleinc+=0//noise(xoff);xoff+=xinc
+        ampli+=0.5
     }
 
 
