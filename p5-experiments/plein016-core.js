dad7648f2ccf6f08a3f422fd09931fabe82b08c9
bbaudry <benoit.baudry@umontreal.ca>
2026-01-29 21:35:39 -0500

    [°-{><}-°]

--- p5-experiments/plein016-core.js
+++ p5-experiments/plein016-core.js
@@ -9,21 +9,33 @@ function hal() {
 }
 
 function star(){
-    let x1,y1,x2,y2,x3,y3,x4,y4,rad,radmax,angle,anglestep
+    let x1,y1,x2,y2,x3,y3,x4,y4,rad,radmax,angle,a,anglestep
     radmax=w*0.49
-    anglestep=Math.floor(random(10,20))
-    rad=radmax
-    ellipse(0,0,radmax*2,radmax*2)
-    for(angle=5;angle<175;angle+=anglestep){
-        x1=rad*cos(angle)
-        y1=rad*sin(angle)
-        x2=rad*cos(-angle)
-        y2=rad*sin(-angle)
-        x3=rad*cos(-angle-anglestep)
+    anglestep=Math.floor(random(5,17))
+    for(angle=5;angle<180-anglestep;angle+=anglestep){
+        rad=random(radmax*0.5,radmax)
+        a=angle
+        x1=rad*cos(a)
+        y1=rad*sin(a)
+        x2=rad*cos(-a)
+        y2=rad*sin(-a)
+        x3=rad*cos(-a-anglestep)
         y3=y2
-        x4=rad*cos(angle+anglestep)
+        x4=rad*cos(a+anglestep)
         y4=y1
-        quad(x1,y1,x2,y2,x3,y3,x4,y4)
+        hu=random([30,330,180])
+        fill(hu,100,100)
+        
+        if(random()<0.5){
+            quad(x1,y1,x2,y2,x3,y3,x4,y4)
+            console.log("ok")
+        }
+        else{
+            push()
+            rotate(90)
+            quad(x1,y1,x2,y2,x3,y3,x4,y4)
+            pop()
+        }
     }
 
 }
