6a5dc54c58b523c808f3df6609c53ba127e5f67a
bbaudry <benoit.baudry@umontreal.ca>
2026-02-01 11:52:52 -0500

    [{{°><°}}]

--- p5-experiments/plein012-core.js
+++ p5-experiments/plein012-core.js
@@ -1,25 +1,35 @@
 var xoff = 0.0
-var xinc = 0.001
+var xinc = 0.0005
+var noisevalues = []
 
 function hal() {
     let ax1, ay1, px1, py1, px2, py2, ax2, ay2, rayon, hinc
-    rayon=4; hinc=0.05
+    rayon=4; hinc=0.09
     for (let t = 0; t < 290; t+=1) {
         ax1 = leftmargin; ay1 = lerp(bottommargin, topmargin, hinc)-t
         ax2 = rightmargin; ay2 = lerp(bottommargin, topmargin, hinc)-t
         px1 = lerp(ax1,ax2,0.25)
-        py1 = lerp(ay1,ay2,0.25)+rayon*sin(radians(rayon*noise(xoff)));xoff+=xinc 
+        py1 = lerp(ay1,ay2,0.25)+rayon*sin(radians(rayon*getNoise()))
         px2 = lerp(ax1,ax2,0.75)
-        py2 = lerp(ay1,ay2,0.75)-rayon*sin(radians(rayon*noise(xoff)));xoff+=xinc 
+        py2 = lerp(ay1,ay2,0.75)-rayon*sin(radians(rayon*getNoise())) 
         bezier(ax1, ay1, px1, py1, px2, py2, ax2, ay2)
 
         ax1 = leftmargin; ay1 = lerp(topmargin, bottommargin, hinc)+t
         ax2 = rightmargin; ay2 = lerp(topmargin, bottommargin, hinc)+t
         px1 = lerp(ax1,ax2,0.25)
-        py1 = lerp(ay1,ay2,0.25)+rayon*sin(radians(rayon*noise(xoff)));xoff+=xinc 
+        py1 = lerp(ay1,ay2,0.25)+rayon*sin(radians(rayon*getNoise())) 
         px2 = lerp(ax1,ax2,0.75)
-        py2 = lerp(ay1,ay2,0.75)-rayon*sin(radians(rayon*noise(xoff)));xoff+=xinc 
+        py2 = lerp(ay1,ay2,0.75)-rayon*sin(radians(rayon*getNoise()))
         bezier(ax1, ay1, px1, py1, px2, py2, ax2, ay2)
         rayon+=4.2
     }
+    console.log("hi hal")
+    return noisevalues
+}
+
+function getNoise(){
+    let r=noise(xoff)
+    noisevalues.push(r)
+    xoff+=xinc
+    return r
 }
--- p5-experiments/plein016-core.js
+++ p5-experiments/plein016-core.js
@@ -12,9 +12,9 @@ function star(){
     let x1,y1,x2,y2,x3,y3,x4,y4,rad,radmax,angle,a,anglestep
     radmax=w*0.49
     ellipse(0,0,radmax*2,radmax*2)
-    anglestep=Math.floor(random(5,17))
+    anglestep=Math.floor(random(11,17))
     for(angle=5;angle<180-anglestep;angle+=anglestep){
-        rad=random(radmax*0.5,radmax)
+        rad=random(radmax*0.6,radmax)
         a=angle
         x1=rad*cos(a)
         y1=rad*sin(a)
@@ -25,18 +25,35 @@ function star(){
         x4=rad*cos(a+anglestep)
         y4=y1
         hu=random([30,330,180])
-        fill(hu,100,100)
+        
+        random()<0.1?stroke(0,0,0):stroke(hu,100,100)
+        noFill()
 
-        if(random()<0.5){
-            quad(x1,y1,x2,y2,x3,y3,x4,y4)
-            console.log("ok")
+        if(random()<0.6){
+            quadwlines(x1,y1,x2,y2,x3,y3,x4,y4)
         }
-        else{
+        if(random()<0.6){
             push()
             rotate(90)
-            quad(x1,y1,x2,y2,x3,y3,x4,y4)
+            quadwlines(x1,y1,x2,y2,x3,y3,x4,y4)
             pop()
         }
     }
 
+}
+
+function quadwlines(x1,y1,x2,y2,x3,y3,x4,y4){
+    let ox,oy,dx,dy
+    //quad(x1,y1,x2,y2,x3,y3,x4,y4)
+    ox=x1
+    oy=y2
+    dx=x3
+    dy=oy
+        console.log("line x1: "+x1+"; x2: "+x2+"; x3: "+x3+"; x4: "+x4)
+        console.log("line y1: "+y1+"; y2: "+y2+"; y3: "+y3+"; y4: "+y4)
+    while(oy<y4){
+        line(ox,oy,dx,dy)
+        oy+=penwidth
+        dy=oy
+    }
 }
