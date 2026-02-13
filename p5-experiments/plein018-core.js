0cf16397fb46ee89e736dc742b59990bc56d3b8e
bbaudry <benoit.baudry@umontreal.ca>
2026-02-13 10:44:01 -0500

    °°[.><.]°°

--- p5-experiments/plein018-core.js
+++ p5-experiments/plein018-core.js
@@ -8,21 +8,18 @@ function hal() {
 }
 
 function grille() {
-    let x, y, stepx, stepy, hauteur
-    stepx = Math.floor(actualwidth / 15)
-    stepy = Math.floor(actualheight / resolution)
-    x = leftmargin
-    largeur = Math.floor(random(1, 5)) * stepx
-    while (x + largeur < rightmargin) {
-        y = topmargin
-        hauteur = Math.floor(random(1, 5)) * stepy
-        while (y + hauteur < bottommargin) {
-            rect(x, y, largeur, hauteur)
-            y += hauteur
-            hauteur = Math.floor(random(1, 5)) * stepy
-        }
-        rect(x, y, largeur, bottommargin - y)
-        x += largeur
-        largeur = Math.floor(random(1, 5)) * stepx
-    }
+    let x1,y1,x2,y2,x3,y3,x4,y4,gap
+    x1=Math.floor(leftmargin+random(0.42,0.52)*actualwidth)
+    y1=topmargin
+    x2=rightmargin
+    y2=Math.floor(topmargin+random(0.42,0.52)*actualheight)
+    x3=Math.floor(leftmargin+random(0.42,0.52)*actualwidth)
+    y3=bottommargin
+    x4=leftmargin
+    y4=Math.floor(topmargin+random(0.42,0.52)*actualheight)
+    gap=7
+    quad(leftmargin,topmargin,x1-gap,topmargin,x1-gap,y2-gap,x4,y4-gap)
+    quad(x1+gap,topmargin,rightmargin,topmargin,rightmargin,y2-gap,x1+gap,y2-gap)
+    quad(rightmargin,y2+gap,rightmargin,bottommargin,x3+gap,bottommargin,x1+gap,y2+gap)
+    quad(x4,y4+gap,x1-gap,y2+gap,x3-gap,y3,x4,bottommargin)
 }
