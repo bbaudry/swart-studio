8040461f7cc1d9a454053df7c626965008d296c9
bbaudry <benoit.baudry@umontreal.ca>
2026-02-16 19:47:08 -0500

    .{*[><]*}.

--- p5-experiments/plein018-core.js
+++ p5-experiments/plein018-core.js
@@ -21,23 +21,33 @@ function grille() {
     gap=5
     stroke(330,100,100)
     quad(x4,y1,x1-gap,y1,x1-gap,y2-gap,x4,y4-gap)
-    random()<0.5?splitquad(x4,y1,x1-gap,y1,x1-gap,y2-gap,x4,y4-gap):splittri(x1-gap,y1,x1-gap,y2-gap,x4,y4-gap)
+    random()<0.9?splitquad(x4,y1,x1-gap,y1,x1-gap,y2-gap,x4,y4-gap):splittri(x1-gap,y1,x1-gap,y2-gap,x4,y4-gap)
     quad(x1+gap,y1,x2,y1,x2,y2-gap,x1+gap,y2-gap)
     quad(x1+gap,y2+gap,x2,y2+gap,x2,y3,x3+gap,y3)
     quad(x4,y4+gap,x1-gap,y2+gap,x3-gap,y3,x4,y3)
 }
 function splitquad(x1,y1,x2,y2,x3,y3,x4,y4){
-    let resx,resy,i,j,x,y,largeur,hauteur
-    resx=Math.floor(random(2,6))
-    resy=Math.floor(random(2,6))
+    let resx,resy,i,j,ix1,iy1,ix2,iy2,ix3,iy3,ix4,iy4,largeur,hauteur
+    resx=Math.floor(random(4,6))
+    resy=Math.floor(random(2,4))
     largeur=(x2-x1)/resx
     hauteur=(y3-y1)/resy
     for(i=0;i<resx;i++){
-        x=lerp(x1,x2,i/resx)
+        ix1=lerp(x1,x2,i/resx)
+        ix2=lerp(x1,x2,(i+1)/resx)
+        ix3=lerp(x4,x3,(i+1)/resx)
+        ix4=lerp(x4,x3,i/resx)
+        oy1=lerp(y1,y2,i/resx)
+        oy2=lerp(y1,y2,(i+1)/resx)
+        dy1=lerp(y4,y3,i/resx)
+        dy2=lerp(y4,y3,(i+1)/resx)
         for(j=0;j<resy;j++){
             console.log("une part; resx: "+resx)
-            y=lerp(y2,y3,j/resy)
-            rect(x,y,largeur,hauteur)
+            iy1=lerp(oy1,dy1,j/resy)
+            iy2=lerp(oy2,dy2,j/resy)
+            iy3=lerp(oy2,dy2,(j+1)/resy)
+            iy4=lerp(oy1,dy1,(j+1)/resy)
+            quad(ix1,iy1,ix2,iy2,ix3,iy3,ix4,iy4)
         }
     }
 }
