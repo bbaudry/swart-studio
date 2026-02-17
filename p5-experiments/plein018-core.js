0280e0fb002f41fb1ab63ea1fca8c463e690327e
bbaudry <benoit.baudry@umontreal.ca>
2026-02-16 21:48:18 -0500

    *{{{><}}}*

--- p5-experiments/plein018-core.js
+++ p5-experiments/plein018-core.js
@@ -1,5 +1,5 @@
 var xoff = 0.0
-var xinc = 1
+var xinc = 0.01
 var grid = []
 var resolution = 8
 
@@ -20,35 +20,45 @@ function grille() {
     y4=Math.floor(topmargin+random(0.42,0.52)*actualheight)
     gap=5
     stroke(330,100,100)
-    quad(x4,y1,x1-gap,y1,x1-gap,y2-gap,x4,y4-gap)
+//    quad(x4,y1,x1-gap,y1,x1-gap,y2-gap,x4,y4-gap)
     random()<0.9?splitquad(x4,y1,x1-gap,y1,x1-gap,y2-gap,x4,y4-gap):splittri(x1-gap,y1,x1-gap,y2-gap,x4,y4-gap)
-    quad(x1+gap,y1,x2,y1,x2,y2-gap,x1+gap,y2-gap)
+//    quad(x1+gap,y1,x2,y1,x2,y2-gap,x1+gap,y2-gap)
+    random()<0.9?splitquad(x1+gap,y1,x2,y1,x2,y2-gap,x1+gap,y2-gap):splittri(x1+gap,y1,x2,y1,x2,y2-gap,x1+gap,y2-gap)
     quad(x1+gap,y2+gap,x2,y2+gap,x2,y3,x3+gap,y3)
     quad(x4,y4+gap,x1-gap,y2+gap,x3-gap,y3,x4,y3)
 }
 function splitquad(x1,y1,x2,y2,x3,y3,x4,y4){
-    let resx,resy,i,j,ix1,iy1,ix2,iy2,ix3,iy3,ix4,iy4,largeur,hauteur
-    resx=Math.floor(random(4,6))
-    resy=Math.floor(random(2,4))
+    let resx,resy,i,j,stepx,stepy,ix1,iy1,ix2,iy2,ix3,iy3,ix4,iy4,largeur,hauteur
+    resx=1//Math.floor(random(2,4))
+    resy=1//Math.floor(random(2,4))
     largeur=(x2-x1)/resx
     hauteur=(y3-y1)/resy
-    for(i=0;i<resx;i++){
+    i=0
+    stepx=noise(xoff);xoff+=xinc
+    while(i+stepx<resx){
         ix1=lerp(x1,x2,i/resx)
-        ix2=lerp(x1,x2,(i+1)/resx)
-        ix3=lerp(x4,x3,(i+1)/resx)
+        ix2=lerp(x1,x2,(i+stepx)/resx)
+        ix3=lerp(x4,x3,(i+stepx)/resx)
         ix4=lerp(x4,x3,i/resx)
         oy1=lerp(y1,y2,i/resx)
-        oy2=lerp(y1,y2,(i+1)/resx)
+        oy2=lerp(y1,y2,(i+stepx)/resx)
         dy1=lerp(y4,y3,i/resx)
-        dy2=lerp(y4,y3,(i+1)/resx)
-        for(j=0;j<resy;j++){
-            console.log("une part; resx: "+resx)
+        dy2=lerp(y4,y3,(i+stepx)/resx)
+        stepy=0
+        j=0
+        stepy=noise(xoff);xoff+=xinc
+        while(j+stepy<resy){
+            j+=stepy
             iy1=lerp(oy1,dy1,j/resy)
             iy2=lerp(oy2,dy2,j/resy)
-            iy3=lerp(oy2,dy2,(j+1)/resy)
-            iy4=lerp(oy1,dy1,(j+1)/resy)
-            quad(ix1,iy1,ix2,iy2,ix3,iy3,ix4,iy4)
+            iy3=lerp(oy2,dy2,(j+stepy)/resy)
+            iy4=lerp(oy1,dy1,(j+stepy)/resy)
+            quadwlines(ix1,iy1,ix2,iy2,ix3,iy3,ix4,iy4)
+            stepy=noise(xoff);xoff+=xinc
         }
+                i+=stepx
+
+        stepx=noise(xoff);xoff+=xinc
     }
 }
 function splittri(x1,y1,x2,y2,x3,y3){
@@ -56,7 +66,7 @@ function splittri(x1,y1,x2,y2,x3,y3){
 }
 function quadwlines(x1,y1,x2,y2,x3,y3,x4,y4){
     let d,t,tinc,ox,oy,dx,dy,amp
-    amp=random(1,2)
+    amp=1//random(1,2)
     dist(x1,y1,x4,y4)>dist(x2,y2,x3,y3)?d=dist(x1,y1,x4,y4):d=dist(x2,y2,x3,y3)
     //penwidth<1?tinc=d*penwidth:
     tinc=1/(d/penwidth)*amp
