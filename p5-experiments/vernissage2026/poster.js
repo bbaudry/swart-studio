48f80ff4ef419e831afed61364588147a57957a4
bbaudry <benoit.baudry@umontreal.ca>
2026-04-06 15:26:04 -0400

    {]+}><{+[}

--- p5-experiments/vernissage2026/poster.js
+++ p5-experiments/vernissage2026/poster.js
@@ -21,9 +21,9 @@ function setup() {
 }
 
 function getsvg() {
-    //letter paper
-    w = Math.floor(96 * 8.5)
-    h = Math.floor(96 * 11)
+    //A3
+    w=Math.floor(96*297/25.4)
+    h=Math.floor(96*420/25.4)
     console.log("w: " + w + "; h: " + h + "; window: " + windowWidth)
 
     cnv = createCanvas(w, h, SVG)//.mousePressed(savesvg);
@@ -55,8 +55,8 @@ function draw() {
     console.log("w: " + w + "; h: " + h)
     noFill()
     stroke(0, 100, 100)
-    strokeWeight(11)
-    
+    strokeWeight(1)
+
     writecode()
     noLoop()
 }
@@ -64,15 +64,67 @@ function draw() {
 
 function writecode() {
     var x, y, t, stepy, postertext
+    fSize=101
     textSize(fSize)
     textFont(font)
     stepy = fSize * 1.2
-    y = 0
-    postertext = ["ART", "ALGO", "RITH", "MI", "QUE"]
+    y = h*0.1
+    postertext = ["ART.ALGO", "RITHMIQUE"]
     for (i in postertext) {
         t = postertext[i]
         x = w * 0.5 - textWidth(t) * 0.5
         y += stepy
         text(t, x, y)
     }
+    y += stepy
+    fSize=42
+    textSize(fSize)
+    stepy = fSize * 1.5
+    postertext = ["vernissage du cours IFT::6258", "10 oeuvres algorithmiques originales"]
+    for (i in postertext) {
+        t = postertext[i]
+        x = w * 0.5 - textWidth(t) * 0.5
+        y += stepy
+        text(t, x, y)
+    }
+    y += stepy*2
+    fSize=31
+    textSize(fSize)
+    stepy = fSize *1.5
+    postertext2 = [
+    "Bouncing Discoveries  ", "  Supernova",
+    "The Art of Motion  ", "  RGB Trio",
+    "Cascading Echoes  ","  Just Jim",
+    "Brave New World  ","  Banana Mousquetaire",
+    "Mind's Maze  ","  Nat-Art",
+    "Decoherence  ","  ant",
+    "interaction  ","  montrealhci",
+    "what the fract!  ","  the tumbling cubes",
+    "Plotted shadow  ","  DS9",
+    "Echoes in motion  ","  Les galactiques"]
+    let links=[]
+    for (let i=0;i<postertext2.length-1;i+=2) {
+        t = postertext2[i]+postertext2[i+1]
+        x = w * 0.5 - textWidth(t) * 0.5
+        y += stepy
+        links.push(createVector(x+textWidth(postertext2[i]),y))
+        text(t, x, y)
+    }
+    for(let p=0;p<links.length;p++){
+        triangle(links[p].x,links[p].y-fSize,links[p].x-fSize*0.5,links[p].y,links[p].x+fSize*0.5,links[p].y)
+    }
+    y += stepy*5
+    fSize=21
+    textSize(fSize)
+    stepy = fSize *1.5
+    postertext = ["https://github.com/rethread-studio/algorithmic-art-course","https://software-performing.art/vernissage2026/"]
+    for (i in postertext) {
+        t = postertext[i]
+        x = w * 0.5 - textWidth(t) * 0.5
+        y += stepy
+        text(t, x, y)
+    }
+
 }
+
+
