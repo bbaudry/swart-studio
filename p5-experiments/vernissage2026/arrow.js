cb4a2e73b361bea0686ac53de46ef2d65ac315bd
bbaudry <benoit.baudry@umontreal.ca>
2026-04-06 14:45:05 -0400

    [*+{><}+*]

--- p5-experiments/vernissage2026/arrow.js
+++ p5-experiments/vernissage2026/arrow.js
@@ -4,7 +4,7 @@ var cnv
 var leftmargin, rightmargin, topmargin, bottommargin, actualheight, actualwidth, penwidth
 var sourcecode
 var font
-var fSize = 151
+var fSize = 64
 var res = 42
 var xoff = 0.0
 var xinc = 0.0001
@@ -56,38 +56,82 @@ function draw() {
     noFill()
     stroke(0, 100, 100)
     strokeWeight(11)
-    arrow()    
-    noLoop()
+    negativearrow()  
+    strokeWeight(1)
+    art()
 }
 
+function art(){
+        push()
+    translate(w*0.5,h*0.5)
+    textFont(font)
+    textSize(fSize)
+    t="art algorithmique"
+    rotate(270)
+    text(t,-textWidth(t)*0.5,fSize*0.5)
+    pop()
+    noLoop()
+
+}
 
 function arrow() {
-    let x1,y1,x2,y2,x3,y3,ox,oy,dx,dy
-    //triangle(w*0.5,42,w*0.9,h*0.4,w*0.1,h*0.4)
+    let x1,y1,x2,y2,x3,y3,x4,y4
+    // arrow head (x1,y1)=(x3,y3) to have a triangle
     x1=w*0.5;y1=42
     x2=w*0.1;y2=h*0.4
     x3=w*0.5;y3=42
     x4=w*0.9;y4=h*0.4
-    for(let t=0;t<1;t+=0.01){
-        ox=lerp(x1,x2,t)
-        oy=lerp(y1,y2,t)
-        dx=lerp(x3,x4,t)
-        dy=lerp(y3,y4,t)
-        if(random()<0.42){
-            line(ox,oy,dx,dy)
-        }
-    }
-    //quad(w*0.32,h*0.4,w*0.68,h*0.4,w*0.63,h*0.9,w*0.37,h*0.9)
+    hatchquad(x1,y1,x2,y2,x3,y3,x4,y4)
+
+    // arrow tail
     x1=w*0.32;y1=h*0.4
     x2=w*0.37;y2=h*0.9
     x3=w*0.68;y3=h*0.4
     x4=w*0.63;y4=h*0.9
+    hatchquad(x1,y1,x2,y2,x3,y3,x4,y4)
+}
+
+// draws 'around' the shape of an arrow
+// the arrow appears in the absence of lines
+function negativearrow() {
+    let x1,y1,x2,y2,x3,y3,x4,y4
+    // left of arrow head
+    x1=w*0.05;y1=42
+    x2=w*0.5;y2=42
+    x3=w*0.05;y3=h*0.4
+    x4=w*0.5;y4=42
+    hatchquad(x1,y1,x2,y2,x3,y3,x4,y4)
+
+    // right of arrow head
+    x1=w*0.95;y1=42
+    x2=w*0.5;y2=42
+    x3=w*0.95;y3=h*0.4
+    x4=w*0.5;y4=42
+    hatchquad(x1,y1,x2,y2,x3,y3,x4,y4)
+
+    //left of arrow tail
+    x1=w*0.05;y1=h*0.4
+    x2=w*0.32;y2=h*0.4
+    x3=w*0.05;y3=h*0.95
+    x4=w*0.37;y4=h*0.95
+    hatchquad(x1,y1,x2,y2,x3,y3,x4,y4)
+
+    //right of arrow tail
+    x1=w*0.95;y1=h*0.4
+    x2=w*0.68;y2=h*0.4
+    x3=w*0.95;y3=h*0.95
+    x4=w*0.63;y4=h*0.95
+    hatchquad(x1,y1,x2,y2,x3,y3,x4,y4)
+}
+
+function hatchquad(x1,y1,x2,y2,x3,y3,x4,y4){
+    let ox,oy,dx,dy
     for(let t=0;t<1;t+=0.01){
         ox=lerp(x1,x2,t)
         oy=lerp(y1,y2,t)
         dx=lerp(x3,x4,t)
         dy=lerp(y3,y4,t)
-        if(random()<0.42){
+        if(random()<0.21){
             line(ox,oy,dx,dy)
         }
     }
