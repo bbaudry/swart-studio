f15781b0b08e6a5298c8e38b24959c9d0204ca17
bbaudry <benoit.baudry@umontreal.ca>
2026-05-01 10:50:20 -0400

    °{*-><-*}°

--- rust-fragments/wind/src/main.rs
+++ rust-fragments/wind/src/main.rs
@@ -21,14 +21,15 @@ fn main() {
 struct Model {
     // .size(1000,1000)
     hashes: Vec<String>,
-    resolution:i32,
     grid:Vec<Cell>
 }
 
 struct Cell{
-    x:f64,
-    y:f64,
+    x:f32,
+    y:f32,
+    w:f32,
     h:f32,
+    hu:f32,
     s:f32,
     l:f32
 }
@@ -41,31 +42,35 @@ fn model(app: &App) -> Model {
         .fullscreen()
         .build()
         .unwrap();
-    let mut h=Vec::new();
-    h.push(String::from("48d5ee7bfb46ee177d865ace846b1cb6695b3cd7"));
+    let mut ha=Vec::new();
+    ha.push(String::from("48d5ee7bfb46ee177d865ace846b1cb6695b3cd7"));
     let res=8;
-    let w: f64 = app.window_rect().w() as f64;
-    let grid=initgrid(8,w);
+    let w: f32 = app.window_rect().w();
+    let h: f32 = app.window_rect().h();
+    let grid=initgrid(res,w,h);
     Model {
-        hashes:h,
-        resolution:res,
+        hashes:ha,
         grid:grid
     }
 }
 
-fn initgrid(res:i32,w:f64) -> Vec<Cell>{
-    let step=w/res as f64;
+fn initgrid(res:i32,w:f32,h:f32) -> Vec<Cell>{
+    let step=h*1.8 as f32/res as f32;
+    let initx=-step*res as f32*0.5+step*0.5;
+    let inity=-step*res as f32*0.5+step*0.5;
     let mut g:Vec<Cell>=Vec::new();
     println!("{}", step);
     for x in 0..res{
         for y in 0..res{
             let c: Cell;
             c = Cell {
-                x:x as f64 *step,
-                y:(y+x*y) as f64 *step,
-                h:0.0,
-                s:1.0,
-                l:0.5
+                x:initx + x as f32 *step,
+                y:inity + y as f32 *step,
+                w:step,
+                h:step,
+                hu:0.0,
+                s:0.0,
+                l:random_range(0.0,1.0)
             };
             g.push(c);
         }
@@ -74,7 +79,7 @@ fn initgrid(res:i32,w:f64) -> Vec<Cell>{
     return g;
 }
 
-fn event(_app: &App, model: &mut Model, event: WindowEvent) {
+fn event(_app: &App, _model: &mut Model, event: WindowEvent) {
     // Print events as they occur to the console
     println!("{:?}", event);
 
@@ -110,17 +115,15 @@ fn event(_app: &App, model: &mut Model, event: WindowEvent) {
     }
 }
 
-fn update(app: &App, model: &mut Model, _update: Update) {}
+fn update(app: &App, model: &mut Model, _update: Update) {
+
+}
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(WHITE);
     // let h = app.window_rect().h();
     // let w = app.window_rect().w();
-    draw.rect()
-        .color(hsl(330.0 / 360.0, 1.0, 0.5))
-        .x_y(0.0,0.0)
-        .w_h(111.0, 111.0);
     let s = model.hashes.get(0).unwrap().to_string();
     let char_vec: Vec<char> = s.chars().collect();   
     for c in char_vec {
@@ -128,9 +131,17 @@ fn view(app: &App, model: &Model, frame: Frame) {
     }
     for cell in model.grid.iter() {
         draw.rect()
-        .color(hsl(cell.h / 360.0, cell.s, cell.l))
+        .color(hsl(cell.hu / 360.0, cell.s, cell.l))
         .x_y(cell.x as f32,cell.y as f32)
-        .w_h(100.0, 100.0);
+        .w_h(cell.w,cell.h);
     }   
+    draw.rect()
+        .color(hsl(330.0 / 360.0, 1.0, 0.5))
+        .x_y(0.0,0.0)
+        .w_h(110.0, 110.0);
+    draw.ellipse()
+        .color(hsl(230.0 / 360.0, 1.0, 0.5))
+        .x_y(-0.0,-0.0)
+        .w_h(10.0, 10.0);
     draw.to_frame(app, &frame).unwrap();
 }
