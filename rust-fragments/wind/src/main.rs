04b501fcb3426d4cf494d4d65ec6f789db5fbf75
bbaudry <benoit.baudry@umontreal.ca>
2026-05-16 13:17:00 -0400

    *}].><.[{*

--- rust-fragments/wind/src/main.rs
+++ rust-fragments/wind/src/main.rs
@@ -22,31 +22,32 @@ fn main() {
 struct Model {
     // .size(1000,1000)
     hashes: Vec<String>,
-    grid:Vec<Cell>,
-    files:Vec<Particle>,
-    connect:Connect
+    grid: Vec<Cell>,
+    files: Vec<Particle>,
+    connect: Connect,
+    count:u32,
 }
 
-struct Cell{
-    x:f32,
-    y:f32,
-    w:f32,
-    h:f32,
-    hu:f32,
-    s:f32,
-    l:f32
+struct Cell {
+    x: f32,
+    y: f32,
+    w: f32,
+    h: f32,
+    hu: f32,
+    s: f32,
+    l: f32,
 }
 
-struct Particle{
-    cx:f32,
-    cy:f32,
-    rad:f32
+struct Particle {
+    cx: f32,
+    cy: f32,
+    rad: f32,
 }
 
-struct Connect{
-    p1:Point2,
-    p2:Point2,
-    p3:Point2
+struct Connect {
+    p1: Point2,
+    p2: Point2,
+    p3: Point2,
 }
 
 fn model(app: &App) -> Model {
@@ -57,44 +58,45 @@ fn model(app: &App) -> Model {
         .fullscreen()
         .build()
         .unwrap();
-    let mut ha:Vec<String>=Vec::new();
+    let mut ha: Vec<String> = Vec::new();
     ha.push(String::from("48d5ee7bfb46ee177d865ace846b1cb6695b3cd7"));
-    let res=8;
+    let res = 8;
     let w: u32 = app.main_window().inner_size_pixels().0;
     let h: u32 = app.main_window().inner_size_pixels().1;
-//    let h: f32 = app.window_rect().h();
-    let grid=initgrid(res,w,h);
-    let files=initFile(7, w);
-    let connect = Connect{
-        p1:pt2(0.0,0.0),
-        p2:pt2(40.0,0.0),
-         p3:pt2(40.0,0.0)
+    //    let h: f32 = app.window_rect().h();
+    let grid = initgrid(res, w, h);
+    let files = initFile(7, w);
+    let connect = Connect {
+        p1: pt2(0.0, 0.0),
+        p2: pt2(40.0, 0.0),
+        p3: pt2(40.0, 0.0),
     };
     Model {
-        hashes:ha,
-        grid:grid,
-        files:files,
-        connect:connect
-    }   
+        hashes: ha,
+        grid: grid,
+        files: files,
+        connect: connect,
+        count:0,
+    }
 }
 
-fn initgrid(res:i32,w:u32,h:u32) -> Vec<Cell>{
-    let step=(h as i32/res) as f32;
-    let initx=-step*res as f32*0.5+step*0.5;
-    let inity=-step*res as f32*0.5+step*0.5;
-    let mut g:Vec<Cell>=Vec::new();
+fn initgrid(res: i32, w: u32, h: u32) -> Vec<Cell> {
+    let step = (h as i32 / res) as f32;
+    let initx = -step * res as f32 * 0.5 + step * 0.5;
+    let inity = -step * res as f32 * 0.5 + step * 0.5;
+    let mut g: Vec<Cell> = Vec::new();
     println!("{}", step);
-    for x in 0..res{
-        for y in 0..res{
+    for x in 0..res {
+        for y in 0..res {
             let c: Cell;
             c = Cell {
-                x:initx + x as f32 *step,
-                y:inity + y as f32 *step,
-                w:step,
-                h:step,
-                hu:0.0,
-                s:0.0,
-                l:random_range(0.0,1.0)
+                x: initx + x as f32 * step,
+                y: inity + y as f32 * step,
+                w: step,
+                h: step,
+                hu: 0.0,
+                s: 0.0,
+                l: random_range(0.0, 1.0),
             };
             g.push(c);
         }
@@ -102,131 +104,129 @@ fn initgrid(res:i32,w:u32,h:u32) -> Vec<Cell>{
     return g;
 }
 
-fn initFile(nbfiles: i32, w:u32) -> Vec<Particle>{
+fn initFile(nbfiles: i32, w: u32) -> Vec<Particle> {
     // blast determines the range inside which we place the particles, the larger nbfiles, the larger the blast
     let blast = map_range(nbfiles, 1, 300, 10, (w as f32 * 0.1).floor() as i32) as f32;
-    let mut res:Vec<Particle>=Vec::new();
-    for i in 0..nbfiles{
-        let angle:f32 = random_range(0.0,2.0*PI);
-        let rayon= random_range(-blast,blast);
-        let x=rayon*angle.cos();
-        let y=rayon*angle.sin();
-        let p=Particle{
-            cx:x,
-            cy:y,
-            rad:3.0
+    let mut res: Vec<Particle> = Vec::new();
+    for i in 0..nbfiles {
+        let angle: f32 = random_range(0.0, 2.0 * PI);
+        let rayon = random_range(-blast, blast);
+        let x = rayon * angle.cos();
+        let y = rayon * angle.sin();
+        let p = Particle {
+            cx: x,
+            cy: y,
+            rad: 3.0,
         };
         res.push(p);
     }
     return res;
 }
 
-
 fn update(app: &App, model: &mut Model, _update: Update) {
-    let index = random_range(0.0,model.hashes.len() as f64).floor() as i64;
+    let index = random_range(0.0, model.hashes.len() as f64).floor() as i64;
     let hash = model.hashes.get(0);
     for mut cell in model.grid.iter_mut() {
-        cell.l=random_range(0.0,1.0)
-    }   
-    let w =app.main_window().inner_size_pixels().0;
-    let h =app.main_window().inner_size_pixels().1;
-    model.files=initFile(random_range(1,300), w);
-    let (x1,y1,x2,y2,x3,y3):(f32,f32,f32,f32,f32,f32);
-    x1=model.files.get(0).unwrap().cx;
-    y1=model.files.get(0).unwrap().cy;
-    if x1>0.0 {
-        x2=(w as f32)*0.25;
-        x3=w as f32;
+        cell.l = random_range(0.0, 1.0)
     }
-    else{
-        x2=-(w as f32)*0.25;
-        x3=-1.0*(w as f32);
+    let w = app.main_window().inner_size_pixels().0;
+    let h = app.main_window().inner_size_pixels().1;
+    model.files = initFile(random_range(1, 300), w);
+    let (x1, y1, x2, y2, x3, y3): (f32, f32, f32, f32, f32, f32);
+    x1 = model.files.get(0).unwrap().cx;
+    y1 = model.files.get(0).unwrap().cy;
+    if x1 > 0.0 {
+        x2 = (w as f32) * 0.25;
+        x3 = (w as f32) * 0.5;
+    } else {
+        x2 = -(w as f32) * 0.25;
+        x3 = -1.0 * (w as f32) * 0.5;
     }
-    if y1>0.0 {
-        y2=(h as f32) * random_range(0.0,0.5);
+    if y1 > 0.0 {
+        y2 = (h as f32) * random_range(0.0, 0.5);
+    } else {
+        y2 = (h as f32) * random_range(-0.5, 0.0);
     }
-    else{
-        y2=(h as f32) * random_range(-0.5,0.0);
-    }
-    model.connect = Connect { 
-        p1: pt2(x1,y1), 
-        p2: pt2(x2, y2), 
-        p3: pt2(x3, y2) 
-    }
-
+    model.connect = Connect {
+        p1: pt2(x1, y1),
+        p2: pt2(x2, y2),
+        p3: pt2(x3, y2),
+    };
+    model.count=model.count+1;
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    draw.background().color(hsla(330.0 / 360.0, 1.0, 0.0,0.1));
+    draw.background().color(hsla(330.0 / 360.0, 1.0, 0.0, 0.1));
     let s = model.hashes.get(0).unwrap().to_string();
-    let char_vec: Vec<char> = s.chars().collect();   
+    let char_vec: Vec<char> = s.chars().collect();
     let w: u32 = app.main_window().inner_size_pixels().0;
     let h: u32 = app.main_window().inner_size_pixels().1;
     //drawcells(model,draw.clone());
     //markcenter(model,draw.clone());
-    drawfiles(model,draw.clone());
-    drawconnect(model,draw.clone());
+    if model.count == 1 {
+        drawfiles(model, draw.clone());
+        drawconnect(model, draw.clone());
         // Create a text primitive with styling
-    draw.text("Styled Text")
-        .font_size(8)
-//        .font("Courier") // Specify a font
-        .color(BLUE)
-        .x_y(model.connect.p2.x-400.0, model.connect.p3.y); // Position at center
- 
-    draw.to_frame(app, &frame).unwrap();
+        draw.text("48d5ee7bfb46ee177d865ace846b1cb6695b3cd7")
+            .font_size(10)
+            //        .font("Courier") // Specify a font
+            .color(WHITE)
+            .x_y(model.connect.p3.x * 0.85, model.connect.p3.y);
+
+        draw.to_frame(app, &frame).unwrap();
+    }
 }
 
-fn drawconnect(model: &Model, draw:Draw){
+fn drawconnect(model: &Model, draw: Draw) {
     draw.line()
-//        .start(app.window_rect().bottom_left())
+        //        .start(app.window_rect().bottom_left())
         .start(model.connect.p1)
         .end(model.connect.p2)
         .weight(1.0)
         .color(RED);
     draw.line()
-//        .start(app.window_rect().bottom_left())
+        //        .start(app.window_rect().bottom_left())
         .start(model.connect.p2)
         .end(model.connect.p3)
         .weight(1.0)
         .color(RED);
 }
 
-fn drawfiles(model: &Model, draw:Draw){
-    for f in model.files.iter(){
+fn drawfiles(model: &Model, draw: Draw) {
+    for f in model.files.iter() {
         draw.ellipse()
             .color(hsl(0.0, 0.0, 0.5))
-            .x_y(f.cx,f.cy)
-            .w_h(f.rad,f.rad);
+            .x_y(f.cx, f.cy)
+            .w_h(f.rad, f.rad);
         println!("one file at x {}, y {}", f.cx, f.cy);
-    } 
+    }
 }
 
 /* helper method to mark the center of the window
  */
-fn markcenter(model: &Model, draw:Draw){
+fn markcenter(model: &Model, draw: Draw) {
     draw.rect()
         .color(hsl(330.0 / 360.0, 1.0, 0.5))
-        .x_y(0.0,0.0)
+        .x_y(0.0, 0.0)
         .w_h(110.0, 110.0);
     draw.ellipse()
         .color(hsl(230.0 / 360.0, 1.0, 0.5))
-        .x_y(-0.0,-0.0)
+        .x_y(-0.0, -0.0)
         .w_h(10.0, 10.0);
-
 }
 
-fn drawcells(model: &Model, draw:Draw){
+fn drawcells(model: &Model, draw: Draw) {
     for cell in model.grid.iter() {
         draw.rect()
-        .color(hsl(cell.hu / 360.0, cell.s, cell.l))
-        .x_y(cell.x as f32,cell.y as f32)
-        .w_h(cell.w,cell.h);
+            .color(hsl(cell.hu / 360.0, cell.s, cell.l))
+            .x_y(cell.x as f32, cell.y as f32)
+            .w_h(cell.w, cell.h);
         draw.ellipse()
             .color(hsl(230.0 / 360.0, 1.0, 0.5))
-            .x_y(cell.x,cell.y)
+            .x_y(cell.x, cell.y)
             .w_h(10.0, 10.0);
-    }   
+    }
 }
 
 fn event(_app: &App, _model: &mut Model, event: WindowEvent) {
