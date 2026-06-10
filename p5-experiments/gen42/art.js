20aff9ce067da083173e6fcc193e282d71b4feb5
bbaudry <benoit.baudry@umontreal.ca>
2026-06-09 22:42:49 -0400

    .][°><°][.

--- p5-experiments/gen42/art.js
+++ p5-experiments/gen42/art.js
@@ -1,6 +1,17 @@
 
+let palettes = [
+    [30, 220],
+    [330, 180]
+]
+let palette_index
+
 function hal() {
-    fill(220,random(100),100)
+    let res = 20
+    let stepx = actualwidth / res
+    let stepy = actualheight / res
+    palette_index=Math.floor(random(palettes.length))
+    background(palettes[palette_index][0],100,100)
+    fill(palettes[palette_index][1],100,100)
     noStroke()
-    rect(leftmargin,topmargin,actualwidth,actualheight)
+    rect(leftmargin,topmargin+actualheight*random(0.8),actualwidth,actualheight*0.1)
 }
