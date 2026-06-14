7321156afc2e6c372f1626814aed1a77f33396ec
bbaudry <benoit.baudry@umontreal.ca>
2026-06-13 21:10:20 -0400

    *°+.><.+°*

--- p5-experiments/gen42/art.js
+++ p5-experiments/gen42/art.js
@@ -1,17 +1,30 @@
 
 let palettes = [
     [30, 220],
-    [330, 180]
+    [330, 180],
+    [220,30],
+    [180,30]
 ]
 let palette_index
 
 function hal() {
-    let res = 20
+    let res = 3
     let stepx = actualwidth / res
     let stepy = actualheight / res
+    let xoff=0
+    let xinc=0.001
     palette_index=Math.floor(random(palettes.length))
     background(palettes[palette_index][0],100,100)
     fill(palettes[palette_index][1],100,100)
     noStroke()
-    rect(leftmargin,topmargin+actualheight*random(0.8),actualwidth,actualheight*0.1)
+    for(let i=0;i<Math.floor(random(2,5));i++){
+    let x=leftmargin
+    while(x<rightmargin){
+        let large=noise(xoff)*res; 
+        let top=noise(xoff)*actualheight
+        let bottom=noise(xoff)*(bottommargin-top)
+        xoff+=xinc
+        rect(x,top,large,bottom)
+        x+=large*2
+    }}
 }
