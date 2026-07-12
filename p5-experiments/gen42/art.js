38377db276a5c56f29cc7fae6ad96770ce22f25b
bbaudry <benoit.baudry@umontreal.ca>
2026-07-12 05:21:28 -0400

    {{.°><°.}}

--- p5-experiments/gen42/art.js
+++ p5-experiments/gen42/art.js
@@ -17,12 +17,12 @@ function hal() {
 function paint() {
     let res = random(3, 5)
     let iter = Math.floor(random(2, 5))
-    let widthratio = random(0.8,1.0)
     let xoff = 0
     let xinc = random(0.001, 0.005)
-    let x,y,top,stringheight
+    let x,y,top,stringheight,widthratio
     for (let i = 0; i < iter; i++) {
         x = leftmargin
+        widthratio = random(0.5,1.0)
         while (x < rightmargin * widthratio) {
             let large = noise(xoff) * res;
             top = noise(xoff) * actualheight
