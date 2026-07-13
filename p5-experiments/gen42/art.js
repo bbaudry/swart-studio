fbc601d8c4ccbfeaf5ef9d6390fa043b67a46e25
bbaudry <benoit.baudry@umontreal.ca>
2026-07-13 10:02:05 -0400

    }++{><}++{

--- p5-experiments/gen42/art.js
+++ p5-experiments/gen42/art.js
@@ -11,33 +11,69 @@ let palette_index
 
 function hal() {
     setcolors()
-    paint()
+    paint2()
+}
+
+function paint2() {
+    let res,iter,x,y,top,boxwidth,boxheight,widthratio,dice
+    res = random(3, 5)
+    iter = Math.floor(random(2, 5))
+    let xoff = 0
+    let xinc = random(0.001, 0.005)
+    for (let i = 0; i < iter; i++) {
+        x = leftmargin
+        while(x<rightmargin){
+            dice=noise(xoff)
+            xoff+=xinc
+            top = noise(xoff) * actualheight
+            boxheight = noise(xoff) * (bottommargin - top)
+            if (dice < 0.5) {
+                boxwidth = noise(xoff) * res;
+                xoff += xinc
+                rect(x, top, boxwidth, boxheight)
+                x += boxwidth * 2
+            }
+            else{
+                y = top
+                boxwidth = noise(xoff) * (rightmargin-x) * 0.5;
+                xoff += xinc
+                while (y<top+boxheight) {
+                    let large = noise(xoff) * res;
+                    xoff += xinc
+                    rect(x, y, rightmargin-x, large)
+                    y += large * 2
+                }
+                x+=boxwidth
+            }
+        }
+    }
 }
 
 function paint() {
-    let res = random(3, 5)
-    let iter = Math.floor(random(2, 5))
+    let res,iter,x,y,top,boxwidth,boxheight,widthratio
+    res = random(3, 5)
+    iter = Math.floor(random(2, 5))
     let xoff = 0
     let xinc = random(0.001, 0.005)
-    let x,y,top,stringheight,widthratio
     for (let i = 0; i < iter; i++) {
         x = leftmargin
         widthratio = random(0.5,1.0)
-        while (x < rightmargin * widthratio) {
-            let large = noise(xoff) * res;
-            top = noise(xoff) * actualheight
-            stringheight = noise(xoff) * (bottommargin - top)
-            xoff += xinc
-            rect(x, top, large, stringheight)
-            x += large * 2
-        }
-        y = top
-        while (y<top+stringheight) {
-            let large = noise(xoff) * res;
-            xoff += xinc
-            rect(x, y, rightmargin-x, large)
-            y += large * 2
-        }
+            while (x < rightmargin * widthratio) {
+                boxwidth = noise(xoff) * res;
+                top = noise(xoff) * actualheight
+                boxheight = noise(xoff) * (bottommargin - top)
+                xoff += xinc
+                rect(x, top, boxwidth, boxheight)
+                x += boxwidth * 2
+            }
+            y = top
+            while (y<top+boxheight) {
+                let large = noise(xoff) * res;
+                xoff += xinc
+                rect(x, y, rightmargin-x, large)
+                y += large * 2
+            }
+        
     }
 }
 
