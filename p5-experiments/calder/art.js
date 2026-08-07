0849f39de818d3399f699d5e0523cf3bfffed433
bbaudry <benoit.baudry@umontreal.ca>
2026-08-07 09:56:15 -0400

    *[}[><]{]*

--- p5-experiments/calder/art.js
+++ p5-experiments/calder/art.js
@@ -1,17 +1,23 @@
 let seuil=0.42*2
-let maxdepth=4
+let maxdepth=8
 function hal() {
     dig(0,leftmargin,topmargin,actualwidth,actualheight)
 }
 
 function dig(depth,x,y,large,haut){
-    noFill()
-    stroke(0,0,100)
+    noStroke()
+    random()<0.5?fill(0,0,0):fill(0,0,100)
     rect(x,y,large,haut)
     if(depth<maxdepth){
         let d=depth+1
-        if(random()<seuil){dig(d,x,y,large*0.5,haut*0.5);dig(d,x+large*0.5,y,large*0.5,haut*0.5)}
-        if(random()<seuil){dig(d,x+large*0.5,y+haut*0.5,large*0.5,haut*0.5);dig(d,x,y+haut*0.5,large*0.5,haut*0.5)}
+        let rl=noise(xoff);xoff+=xinc;//random()
+        let rh=noise(xoff);xoff+=xinc;//random()
+        if(random()<seuil){
+            dig(d,x,y,large*rl,haut*rh);dig(d,x+large*rl,y,large*(1-rl),haut*rh)
+        }
+        if(random()<seuil){
+            dig(d,x+large*rl,y+haut*rh,large*(1-rl),haut*(1-rh));dig(d,x,y+haut*rh,large*rl,haut*(1-rh))
+        }
     }
 }
 
